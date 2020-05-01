b3010ae04f0c42145b2f27cfe042da2bcc2b9f07
Chris Adams <chris@cadams.com.au>
2020-05-02 09:47:51 +1000

    Another attempt at fixing Safari 11

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const MAP_HEIGHT_PERCENT = 56.25;
-export const MAP_URL = 'https://raw.githubusercontent.com/thecadams/co-reality-map/master/public/bodyssey.jpg';
+export const MAP_URL = '/bodyssey.jpg';
 export const MAP_ALT = 'Clickable Map';
 export const SCHEDULE_URL = '/bodyssey_schedule.pdf';
