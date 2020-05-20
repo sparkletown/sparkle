2c6553b3c9cf013c7bca8f0df98a3d2cbb9a83b5
Chris Adams <chris@cadams.com.au>
2020-05-19 20:10:05 -0700

    Import only part of Firebase to fix warning

--- src/ChatForm.js
+++ src/ChatForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useFirestore } from 'react-redux-firebase';
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 
 // One message per second
 const TIME_BETWEEN_SENDS_MILLIS = 2000;
