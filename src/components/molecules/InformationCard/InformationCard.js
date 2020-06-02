ca87d791e2005e69ea133b77f73dc8144d728b72
Antoine Eudes <antoine.eudes@gmail.com>
2020-06-02 18:28:14 +0200

    :sparkles: adaptation for the bat party

--- src/components/molecules/InformationCard/InformationCard.js
+++ src/components/molecules/InformationCard/InformationCard.js
@@ -4,7 +4,7 @@ import "./InformationCard.scss";
 const InformationCard = ({ title, children }) => (
   <div className="information-card-container">
     <h4>{title}</h4>
-    <p className="grey-color-text">{children}</p>
+    <p className="information-card-text">{children}</p>
   </div>
 );
 
