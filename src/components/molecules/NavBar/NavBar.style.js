0787002434ee216938323eca73818d085f415c19
Antoine Eudes <antoine.eudes@gmail.com>
2020-06-02 14:05:04 +0200

    :sparkles: add navbar to party landing page

---
+++ src/components/molecules/NavBar/NavBar.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.nav`
+  display: flex;
+  justify-content: center;
+`;
+
+export const NavbarBrandName = styled.span`
+  font-style: italic;
+  font-weight: 700;
+`;
