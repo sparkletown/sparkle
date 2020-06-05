fdd7ba8a92832344c6837ed311c803448b9f0d2f
Chris Adams <chris@cadams.com.au>
2020-06-05 02:08:42 -0700

    Authenticate

--- scripts/config-upload.js
+++ scripts/config-upload.js
@@ -1,7 +1,11 @@
+"use strict";
+
 var fs = require("fs");
 var firebase = require("firebase/app");
+require("firebase/auth");
 require("firebase/firestore");
 var validate = require("jsonschema").validate;
+var read = require("read");
 
 var schema = require("./schema.json");
 
@@ -12,21 +16,20 @@ ${process.argv[1]}: Upload event config
 Config will be validated using schema.json in the script directory,
 then uploaded to the config collection in the co-reality-map firestore.
 
-Usage: node ${process.argv[1]} EVENT_NAME CONFIG_PATH
+Usage: node ${process.argv[1]} API_KEY EVENT_NAME CONFIG_PATH
 
-Example: node ${process.argv[1]} example example.js
+Example: node ${process.argv[1]} aaazzz111222333 example example.js
 `);
   process.exit(1);
 }
 
 const argv = process.argv.slice(2);
-if (argv.length < 2) {
+if (argv.length < 3) {
   usage();
 }
-
-config = argv[0];
-path = argv[1];
-
+var apiKey = argv[0];
+var config = argv[1];
+var path = argv[2];
 var doc = require(path);
 console.log("Loaded document:", doc);
 
@@ -36,10 +39,42 @@ console.log("Validation result:", validateResult);
 if (!validateResult.valid) {
   console.error("Invalid document, skipping upload");
 } else {
-  const firebaseConfig = {
-    projectId: "co-reality-map",
-  };
-  firebase.initializeApp(firebaseConfig);
-  const firestore = firebase.firestore();
-  firestore.doc(`config/${config}`).set(doc);
+  read({ prompt: "Username:" }, function (err, username) {
+    if (err) {
+      console.error("Error obtaining username:", err);
+      process.exit(1);
+    }
+    read({ prompt: "Password:", silent: true }, function (err, password) {
+      if (err) {
+        console.error("Error obtaining password:", err);
+        process.exit(1);
+      }
+
+      const firebaseConfig = {
+        apiKey: apiKey,
+        projectId: "co-reality-map",
+      };
+      firebase.initializeApp(firebaseConfig);
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(username, password)
+        .then(function () {
+          firebase
+            .firestore()
+            .doc(`config/${config}`)
+            .set(doc)
+            .then(function () {
+              console.log("Document successfully written!");
+              process.exit(0);
+            })
+            .catch(function (err) {
+              console.error("Error writing document: ", err);
+              process.exit(1);
+            });
+        })
+        .catch(function (err) {
+          console.error("Login error:", err);
+        });
+    });
+  });
 }
--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,3 +1,5 @@
 EVENT=example
 CONFIG=example.js
-node $(dirname $0)/config-upload.js ${EVENT} ../configs/${CONFIG}
+API_KEY=$(grep API_KEY $(dirname $0)/../src/secrets.js|head -n1|cut -d\" -f2)
+
+node $(dirname $0)/config-upload.js ${API_KEY} ${EVENT} ../configs/${CONFIG}
