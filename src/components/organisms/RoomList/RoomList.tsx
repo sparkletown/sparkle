3f7ae5ed6036babe378562ec688e28a9b9d0b388
Chris Adams <chris@cadams.com.au>
2020-06-06 02:04:58 -0700

    Hide easter egg rooms

--- src/components/organisms/RoomList/RoomList.tsx
+++ src/components/organisms/RoomList/RoomList.tsx
@@ -14,22 +14,26 @@ const RoomList: React.FunctionComponent<PropsType> = ({
   startUtcSeconds,
   rooms,
   attendances,
-}) => (
-  <>
-    <div className="room-list-title">
-      What's on now: {rooms.length} rooms open
-    </div>
-    <div className="rooms-container">
-      {rooms.map((room) => (
-        <RoomCard
-          key={room.title}
-          startUtcSeconds={startUtcSeconds}
-          room={room}
-          attendance={attendances[room.title]}
-        />
-      ))}
-    </div>
-  </>
-);
+}) => {
+  rooms = rooms.filter((room) => room.on_list);
+
+  return (
+    <>
+      <div className="room-list-title">
+        What's on now: {rooms.length} rooms open
+      </div>
+      <div className="rooms-container">
+        {rooms.map((room) => (
+          <RoomCard
+            key={room.title}
+            startUtcSeconds={startUtcSeconds}
+            room={room}
+            attendance={attendances[room.title]}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default RoomList;
