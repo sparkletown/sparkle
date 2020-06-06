37d64caceff92151d5cb128c24db8a7c19eb2aa9
Chris Adams <chris@cadams.com.au>
2020-06-05 17:19:35 -0700

    Fix password checks

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,30 @@
 const functions = require("firebase-functions");
-const { PASSWORD } = require("./secrets");
+const { PASSWORD, ADMIN_PASSWORD } = require("./secrets");
 
 // Case-insensitive first character for iDevices
 function lowercaseFirstChar(password) {
   return password.charAt(0).toLowerCase() + password.substring(1);
 }
 
+function passwordsMatch(submittedPassword, actualPassword) {
+  return (
+    submittedPassword === actualPassword ||
+    lowercaseFirstChar(submittedPassword) === lowercaseFirstChar(actualPassword)
+  );
+}
+
 exports.checkPassword = functions.https.onCall((data, context) => {
-  if (
-    data.password === undefined ||
-    (data.password.length > 0 &&
-      lowercaseFirstChar(data.password) !== lowercaseFirstChar(PASSWORD))
-  ) {
-    throw new functions.https.HttpsError(
-      "unauthenticated",
-      "Password incorrect"
-    );
-  } else {
+  if (data && data.password && !passwordsMatch(data.password, PASSWORD)) {
     return "OK";
   }
+
+  throw new functions.https.HttpsError("unauthenticated", "Password incorrect");
+});
+
+exports.checkAdminPassword = functions.https.onCall((data, context) => {
+  if (data && data.password && !passwordsMatch(data.password, ADMIN_PASSWORD)) {
+    return "OK";
+  }
+
+  throw new functions.https.HttpsError("unauthenticated", "Password incorrect");
 });
