c538a7137b3bfa45ecdf06f43c909b1e87d4e9d3
Antoine Eudes <antoine.eudes@gmail.com>
2020-06-15 16:54:39 +0200

    :sparkles: add JazzBar in map

---
+++ configs/co-reality-6.js
@@ -0,0 +1,26 @@
+module.exports = {
+  start_utc_seconds: 1592683200,
+  duration_hours: 8,
+  entrance_hosted_hours: 2,
+  party_name: "Island Party",
+  unhosted_entry_video_url: "https://www.youtube.com/embed/U4GsKONeHnM",
+  map_url: "/maps/island-party.jpg",
+  map_viewbox: "0 0 1500 1500",
+  rooms: [
+    {
+      title: "Jazz Mountain",
+      subtitle: "Enter a virtual Jazz bar",
+      about: "Live the experience of a Jazz bar",
+      url: "/kansassmittys",
+      on_map: true,
+      on_list: true,
+      path:
+        "M 135.423 414.34 C 129.92 416.788 71.603 449.271 94.627 464.937 C 97.468 466.87 112.622 488.205 107.574 493.396 C 96.597 504.682 56.097 488.739 43.088 504.152 C 9.669 543.748 67.99 534.962 51.461 567.682 C 35.16 599.951 26.064 656.522 43.906 676.873 C 54.1 688.501 99.604 670.197 115.088 678.731 C 125.117 684.258 136.075 700.216 146.041 702.698 C 168.944 708.402 197.263 696.005 219.884 702.698 C 232.887 706.545 255.279 730.161 274.93 720.234 C 297.208 708.98 327.724 693.95 352.391 681.489 C 370.196 672.495 342.056 639.386 336.727 630.629 C 324.542 610.607 320.759 570.941 303.001 559.698 C 290.331 551.675 264.763 548.074 255.427 540 C 242.956 529.215 229.052 480.684 223.184 459.905 C 220.239 449.478 228.419 423.485 218.457 427.745 C 202.275 434.664 134.716 369.82 134.716 419.026",
+      template: "jazzBar",
+      attendance_x: "17%",
+      attendance_y: "25%",
+      image: "/rooms/jazzmountain.jpg",
+      events: [],
+    },
+  ],
+};
