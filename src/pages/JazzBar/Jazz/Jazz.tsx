3efd02f38437f4fdf156e2f3f96b80a187b6b57e
Antoine Eudes <antoine.eudes@gmail.com>
2020-06-25 15:49:21 +0200

    :sparkles: add Chatmodal

--- src/pages/JazzBar/Jazz/Jazz.tsx
+++ src/pages/JazzBar/Jazz/Jazz.tsx
@@ -59,55 +59,58 @@ const Jazz: React.FunctionComponent<PropsType> = ({
             customTables={JAZZBAR_TABLES}
           />
         )}
+
         <div
           className={`jazz-container ${
-            !seatedAtTable
-              ? "container-in-grid"
-              : `container-in-row ${
-                  isVideoFocused ? "video-focused col-11" : "col-5"
-                }`
+            !seatedAtTable ? "container-in-grid" : "container-in-row "
           }`}
         >
-          <iframe
-            title="main event"
-            width="100%"
-            height="100%"
-            className={"youtube-video"}
-            src="https://www.youtube.com/embed/b44P11vLiY8?autoplay=1"
-            frameBorder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;"
-          />
+          <div
+            className={`video ${
+              seatedAtTable
+                ? isVideoFocused
+                  ? "video-focused col-11"
+                  : "col-5 video-not-focused"
+                : ""
+            }`}
+          >
+            <iframe
+              title="main event"
+              width="100%"
+              height="100%"
+              className={"youtube-video"}
+              src="https://www.youtube.com/embed/b44P11vLiY8?autoplay=1"
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;"
+            />
+          </div>
         </div>
         {seatedAtTable && (
-          <div
-            className={`${isVideoFocused ? "col-5" : "col-12"} table-container`}
-          >
-            <div className="jazz-wrapper">
-              <Room roomName={seatedAtTable} setUserList={setUserList} />
-            </div>
-            <div>
-              <div className="full-screen-checkbox">
-                <div className="focus">
-                  Focus on:<div className="focus-option">Jazz</div>
-                </div>
-                <label className="switch">
-                  <input
-                    type="checkbox"
-                    checked={!isVideoFocused}
-                    onChange={() => setIsVideoFocused(!isVideoFocused)}
-                  />
-                  <span className="slider" />
-                </label>
-                <div className="focus-option">Friends</div>
+          <div className="container-in-row">
+            <div
+              className={`${
+                isVideoFocused ? "col-5" : "col-12"
+              } table-container`}
+            >
+              <div className="jazz-wrapper">
+                <Room roomName={seatedAtTable} setUserList={setUserList} />
               </div>
-              {/* <TablesUserList
-                setSeatedAtTable={setSeatedAtTable}
-                seatedAtTable={seatedAtTable}
-                experienceName={experience.associatedRoom}
-                TableComponent={TableComponent}
-                joinMessage={false}
-                customTables={JAZZBAR_TABLES}
-              /> */}
+              <div>
+                <div className="full-screen-checkbox">
+                  <div className="focus">
+                    Focus on:<div className="focus-option">Jazz</div>
+                  </div>
+                  <label className="switch">
+                    <input
+                      type="checkbox"
+                      checked={!isVideoFocused}
+                      onChange={() => setIsVideoFocused(!isVideoFocused)}
+                    />
+                    <span className="slider" />
+                  </label>
+                  <div className="focus-option">Friends</div>
+                </div>
+              </div>
             </div>
           </div>
         )}
