a39db8428aecba47dfeba3e583e85e91d7a05a6f
Mathieu Dähne <mathieud@theodo.co.uk>
2020-07-02 17:27:28 +0100

    Scope chats per venue

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,77 +1,9 @@
-import firebase from "firebase/app";
-
 import { leaveRoom } from "utils/useLocationUpdateEffect";
 
 export const PREVIEW_ROOM = "PREVIEW_ROOM";
 export const EXIT_PREVIEW_ROOM = "EXIT_PREVIEW_ROOM";
 export const SET_USER = "SET_USER";
 export const TOGGLE_MUTE_REACTIONS = "TOGGLE_MUTE_REACTIONS";
-
-export function sendGlobalChat(from, text) {
-  return (dispatch) => {
-    const firestore = firebase.firestore();
-    firestore.collection("chatsv3").add({
-      type: "global",
-      ts_utc: firebase.firestore.Timestamp.fromDate(new Date()),
-      text,
-      from,
-    });
-  };
-}
-
-export const sendRoomChat = (from, to, text) => {
-  return (dispatch) => {
-    const firestore = firebase.firestore();
-    firestore.collection("chatsv3").add({
-      type: "room",
-      ts_utc: firebase.firestore.Timestamp.fromDate(new Date()),
-      text,
-      from,
-      to,
-    });
-  };
-};
-
-export function sendPrivateChat(from, to, text) {
-  return (dispatch) => {
-    const firestore = firebase.firestore();
-    firestore
-      .collection("privatechats")
-      .doc(from)
-      .collection("chats")
-      .add({
-        type: "private",
-        ts_utc: firebase.firestore.Timestamp.fromDate(new Date()),
-        text,
-        from,
-        to,
-      });
-    firestore
-      .collection("privatechats")
-      .doc(to)
-      .collection("chats")
-      .add({
-        type: "private",
-        ts_utc: firebase.firestore.Timestamp.fromDate(new Date()),
-        text,
-        from,
-        to,
-      });
-  };
-}
-
-export function sendTableChat(from, to, text) {
-  return (dispatch) => {
-    const firestore = firebase.firestore();
-    firestore.collection("chatsv3").add({
-      type: "table",
-      ts_utc: firebase.firestore.Timestamp.fromDate(new Date()),
-      text,
-      from,
-      to,
-    });
-  };
-}
 
 export function previewRoom(room) {
   return { type: PREVIEW_ROOM, room };
