06d3e3edebcc5ace570bfde6349ff8e68bc6f31a
Mathieu Dähne <mathieud@theodo.co.uk>
2020-07-02 15:20:59 +0100

    Use same interface for all venues

--- src/components/venues/Jazzbar/JazzbarRouter/JazzbarRouter.tsx
+++ src/components/venues/Jazzbar/JazzbarRouter/JazzbarRouter.tsx
@@ -1,21 +1,24 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { useRouteMatch, Switch, Route } from "react-router-dom";
 import JazzBar from "../Jazzbar";
 import ReactionPage from "pages/ReactionPage";
 import useConnectPartyGoers from "hooks/useConnectPartyGoers";
 import ExperienceContextProvider from "components/context/ExperienceContext";
 import { useFirestoreConnect } from "react-redux-firebase";
+import { Venue } from "pages/VenuePage/VenuePage";
 
-interface PropsType {
-  venueName: string;
-}
-
-const JazzbarRouter: React.FunctionComponent<PropsType> = ({ venueName }) => {
+const JazzbarRouter: React.FunctionComponent = () => {
   const match = useRouteMatch();
   useConnectPartyGoers();
   useFirestoreConnect("users");
+
+  const { venue } = useSelector((state: any) => ({
+    venue: state.firestore.data.currentVenue,
+  })) as { venue: Venue };
+
   return (
-    <ExperienceContextProvider venueName={venueName}>
+    <ExperienceContextProvider venueName={venue.name}>
       <Switch>
         <Route path={`${match.url}/band`} component={ReactionPage} />
         <Route path={`${match.url}/`} component={JazzBar} />
