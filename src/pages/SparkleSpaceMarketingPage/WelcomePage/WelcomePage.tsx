79389259040a635fda5122b5afd0620b856b35d9
Antoine Eudes <antoine.eudes@gmail.com>
2020-07-02 19:44:45 +0200

    :sparkles: email form on marketing page works

--- src/pages/SparkleSpaceMarketingPage/WelcomePage/WelcomePage.tsx
+++ src/pages/SparkleSpaceMarketingPage/WelcomePage/WelcomePage.tsx
@@ -1,102 +1,126 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useFirebase } from "react-redux-firebase";
 
-const WelcomePage: React.FunctionComponent = () => (
-  <>
-    <div className="hero">
-      <p className="hero-text">
-        Welcome to the SparkleVerse{" "}
-        <span role="img" aria-label="sparkle emoji">
-          ✨
-        </span>
-      </p>
-      <div className="subtitle">
-        Help co-create a decentralised experimental city in the Multiverse with
-        fellow burners from around the world
-      </div>
-    </div>
-    <div className="info-wrapper">
-      <h3 className="card-wrapper-title">How the Sparkleverse works:</h3>
-      <div className="row card-container">
-        <div className="col-md-4">
-          <div className="info-card">
-            <h3 className="card-title">1. Digital Playa Map</h3>
-            <img
-              className="image-in-card"
-              src="/marketing-page/digital-playa-map.jpg"
-              title="Digital Playa Map image"
-            />
-            <p>
-              A 2-D browser-based map for roaming in shared adventures through
-              an integrated patchwork quilt of places and experiences
-            </p>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="info-card">
-            <h3 className="card-title">
-              2. Easy, powerful tools to create magic
-            </h3>
-            <img
-              className="image-in-card"
-              src="/marketing-page/powerful-tools.jpg"
-              title="Powerful tools image"
-            />
-            <p>
-              Populate the playa with your camp, art, host performances (DJs,
-              talks) etc using simple yet powerful space-creation tools and
-              templates
-            </p>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="info-card">
-            <h3 className="card-title">3. Community education + support</h3>
-            <img
-              className="image-in-card"
-              src="/marketing-page/community-education.jpg"
-              title="Community education image"
-            />
-            <p>
-              Forums, examples, templates and community support to help you add
-              your magic to the Sparkleverse for others to enjoy
-            </p>
-          </div>
+interface InterestedPersonEmail {
+  email: string;
+}
+
+const WelcomePage: React.FunctionComponent = () => {
+  const { register, handleSubmit, setValue } = useForm<InterestedPersonEmail>();
+  const firebase = useFirebase();
+
+  const onSubmit = async (data: InterestedPersonEmail) => {
+    await firebase.firestore().collection("interestedPerson").add(data);
+    setValue("email", "");
+  };
+  return (
+    <>
+      <div className="hero">
+        <p className="hero-text">
+          Welcome to the SparkleVerse{" "}
+          <span role="img" aria-label="sparkle emoji">
+            ✨
+          </span>
+        </p>
+        <div className="subtitle">
+          Help co-create a decentralised experimental city in the Multiverse
+          with fellow burners from around the world
         </div>
       </div>
-    </div>
-
-    <div className="interested-contributors">
-      <div className="interested-contribtors-text">
-        Interested to connect, learn more or participate:
+      <div className="info-wrapper">
+        <h3 className="card-wrapper-title">How the Sparkleverse works:</h3>
+        <div className="row card-container">
+          <div className="col-md-4">
+            <div className="info-card">
+              <h3 className="card-title">1. Digital Playa Map</h3>
+              <img
+                className="image-in-card"
+                src="/marketing-page/digital-playa-map.jpg"
+                title="Digital Playa Map image"
+              />
+              <p>
+                A 2-D browser-based map for roaming in shared adventures through
+                an integrated patchwork quilt of places and experiences
+              </p>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="info-card">
+              <h3 className="card-title">
+                2. Easy, powerful tools to create magic
+              </h3>
+              <img
+                className="image-in-card"
+                src="/marketing-page/powerful-tools.jpg"
+                title="Powerful tools image"
+              />
+              <p>
+                Populate the playa with your camp, art, host performances (DJs,
+                talks) etc using simple yet powerful space-creation tools and
+                templates
+              </p>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="info-card">
+              <h3 className="card-title">3. Community education + support</h3>
+              <img
+                className="image-in-card"
+                src="/marketing-page/community-education.jpg"
+                title="Community education image"
+              />
+              <p>
+                Forums, examples, templates and community support to help you
+                add your magic to the Sparkleverse for others to enjoy
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <input type="text" placeholder="Enter your email" />
-      <input className="btn btn-primary submit-btn" type="submit" />
-    </div>
 
-    <div className="info-wrapper">
-      <h3>Watch the video for a quick introduction to our approach</h3>
-      <div className="video-container">
-        <iframe
-          title="Our approach to Burning Man"
-          className="video"
-          src="https://www.youtube.com/embed/4Ku4E2MXp-k"
-          frameBorder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
+      <div className="interested-contributors">
+        <div className="interested-contribtors-text">
+          Interested to connect, learn more or participate:
+        </div>
+        <input
+          type="text"
+          name="email"
+          ref={register({ required: true })}
+          placeholder="Enter your email"
+        />
+        <input
+          className="btn btn-primary submit-btn"
+          type="submit"
+          onClick={handleSubmit(onSubmit)}
+        />
       </div>
-      <button
-        className="btn btn-primary btn-block btn-centered help-build-button"
-        onClick={() =>
-          window.open(
-            "https://docs.google.com/forms/d/e/1FAIpQLSeGGsafBOnO63GOiPjBhIdFaEqoM0xBSERdkTEqh3DrPteQvw/viewform"
-          )
-        }
-      >
-        Help build the SparkleVerse
-      </button>
-    </div>
-  </>
-);
+
+      <div className="info-wrapper">
+        <h3>Watch the video for a quick introduction to our approach</h3>
+        <div className="video-container">
+          <iframe
+            title="Our approach to Burning Man"
+            className="video"
+            src="https://www.youtube.com/embed/4Ku4E2MXp-k"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <button
+          className="btn btn-primary btn-block btn-centered help-build-button"
+          onClick={() =>
+            window.open(
+              "https://docs.google.com/forms/d/e/1FAIpQLSeGGsafBOnO63GOiPjBhIdFaEqoM0xBSERdkTEqh3DrPteQvw/viewform"
+            )
+          }
+        >
+          Help build the SparkleVerse
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default WelcomePage;
