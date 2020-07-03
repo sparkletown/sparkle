fa1fd90d0b8e3d2b02ec8e19b13b00105c09bea5
Chris Adams <chris@cadams.com.au>
2020-07-03 08:56:17 -0700

    Upcoming tickets overlay

--- src/components/molecules/NavBar/NavBar.tsx
+++ src/components/molecules/NavBar/NavBar.tsx
@@ -1,17 +1,30 @@
 import React from "react";
+import firebase from "firebase/app";
 import "./NavBar.scss";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { UpcomingEvent } from "types/UpcomingEvent";
+import UpcomingTickets from "components/molecules/UpcomingTickets";
 
 interface PropsType {
   redirectionUrl?: string;
 }
 
 const NavBar: React.FunctionComponent<PropsType> = ({ redirectionUrl }) => {
-  const { user, users } = useSelector((state: any) => ({
+  const { user, users, venue } = useSelector((state: any) => ({
     user: state.user,
     users: state.firestore.data.users,
+    venue: state.firestore.data.currentVenue,
   }));
+
+  const hasUser = user && users && users[user.uid];
+
+  const now = firebase.firestore.Timestamp.fromDate(new Date());
+  const futureUpcoming = venue?.events?.filter(
+    (e: UpcomingEvent) => e.ts_utc.valueOf() > now.valueOf()
+  );
+
+  const hasUpcomingEvents = futureUpcoming && futureUpcoming.length > 0;
 
   return (
     <header>
@@ -25,17 +38,22 @@ const NavBar: React.FunctionComponent<PropsType> = ({ redirectionUrl }) => {
             />
           </span>
         </Link>
-        {user && users && users[user.uid] && (
-          <div>
-            <Link to="/account/edit">
-              <img
-                src={users[user.uid].pictureUrl}
-                className="profile-icon"
-                alt="avatar"
-                width="40"
-                height="40"
-              />
-            </Link>
+        {(hasUser || hasUpcomingEvents) && (
+          <div className="icons-container">
+            {hasUpcomingEvents && <UpcomingTickets events={futureUpcoming} />}
+            {hasUser && (
+              <div>
+                <Link to="/account/edit">
+                  <img
+                    src={users[user.uid].pictureUrl}
+                    className="profile-icon"
+                    alt="avatar"
+                    width="40"
+                    height="40"
+                  />
+                </Link>
+              </div>
+            )}
           </div>
         )}
       </nav>
