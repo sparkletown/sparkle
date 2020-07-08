9249e0bc1a455abe96f7e30e177b3f9313e3bbc2
Mathieu Dähne <mathieud@theodo.co.uk>
2020-07-08 15:27:42 +0100

    Big boxes roughtly defined

--- src/components/venues/Jazzbar/JazzTab/JazzTab.tsx
+++ src/components/venues/Jazzbar/JazzTab/JazzTab.tsx
@@ -43,21 +43,16 @@ export interface JazzbarVenue extends Venue {
 
 const Jazz: React.FunctionComponent<PropsType> = ({ setUserList }) => {
   const dispatch = useDispatch();
-  const { experience, user, users, muteReactions, venue } = useSelector(
-    (state: any) => ({
-      experience:
-        state.firestore.data.config?.[PARTY_NAME]?.experiences.jazzbar,
-      user: state.user,
-      users: state.firestore.ordered.partygoers,
-      muteReactions: state.muteReactions,
-      venue: state.firestore.data.currentVenue,
-    })
-  ) as {
+  const { user, users, muteReactions, venue } = useSelector((state: any) => ({
+    user: state.user,
+    users: state.firestore.ordered.partygoers,
+    muteReactions: state.muteReactions,
+    venue: state.firestore.data.currentVenue,
+  })) as {
     users: User[];
     user: FUser;
     venue: JazzbarVenue;
     muteReactions: boolean;
-    experience: any;
   };
 
   const [isMessageToTheBandSent, setIsMessageToTheBandSent] = useState(false);
@@ -77,37 +72,8 @@ const Jazz: React.FunctionComponent<PropsType> = ({ setUserList }) => {
 
   const usersInJazzBar =
     users &&
-    experience &&
+    venue &&
     users.filter((user: User) => user.lastSeenIn === venue.name);
-
-  const usersAtSameTable =
-    users &&
-    users.filter(
-      (user: User) =>
-        user.data?.[venue.name] && user.data[venue.name].table === seatedAtTable
-    );
-
-  const usersInJazzbarWithoutPeopleAtTable =
-    users &&
-    experience &&
-    usersAtSameTable &&
-    users.filter(
-      (user: User) =>
-        usersInJazzBar.includes(user) && !usersAtSameTable.includes(user)
-    );
-
-  const usersSeated =
-    users &&
-    users.filter(
-      (user: User) => user.data?.[venue.name] && user.data[venue.name].table
-    );
-
-  const usersStanding =
-    usersSeated &&
-    users.filter(
-      (user: User) =>
-        user.lastSeenIn === venue.name && !usersSeated.includes(user)
-    );
 
   function createReaction(
     reaction: { reaction: EmojiReactionType },
@@ -148,82 +114,81 @@ const Jazz: React.FunctionComponent<PropsType> = ({ setUserList }) => {
   };
 
   return (
-    <div className="scrollable-area">
-      <div className="user-interaction-container">
-        {users && (
-          <UserList
-            users={
-              seatedAtTable
-                ? usersInJazzbarWithoutPeopleAtTable
-                : usersInJazzBar
-            }
-            limit={26}
-            activity="in the jazz bar"
+    <div style={{ display: "flex", width: "90vw", flexWrap: "wrap" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          flexGrow: 3,
+          flexBasis: 0,
+        }}
+      >
+        <div style={{ border: "1px solid white", height: "500px" }}>
+          <iframe
+            key="main-event"
+            title="main event"
+            width="100%"
+            height="100%"
+            className="youtube-video"
+            src={`${venue.iframeUrl}?autoplay=1`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;"
           />
-        )}
+        </div>
+        <div
+          style={{
+            border: "1px solid white",
+            display: "flex",
+            flexWrap: "wrap",
+          }}
+        >
+          <TablesUserList
+            setSeatedAtTable={setSeatedAtTable}
+            seatedAtTable={seatedAtTable}
+            venueName={venue.name}
+            TableComponent={TableComponent}
+            joinMessage={true}
+            customTables={JAZZBAR_TABLES}
+          />
+          {seatedAtTable && (
+            <div className="container-in-row">
+              <div
+                className={`${
+                  isVideoFocused ? "col-5" : "col-12"
+                } table-container`}
+              >
+                <TableHeader
+                  seatedAtTable={seatedAtTable}
+                  setSeatedAtTable={setSeatedAtTable}
+                  venueName={venue.name}
+                />
+                <div className="jazz-wrapper">
+                  <Room roomName={seatedAtTable} setUserList={setUserList} />
+                </div>
+                <TableFooter
+                  isVideoFocused={isVideoFocused}
+                  setIsVideoFocused={setIsVideoFocused}
+                />
+              </div>
+            </div>
+          )}
+        </div>
       </div>
       <div
-        className={`content ${
-          !seatedAtTable ? "jazz-bar-grid" : "jazz-bar-table"
-        }`}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          flexGrow: 1,
+          flexBasis: 0,
+        }}
       >
-        <TablesUserList
-          setSeatedAtTable={setSeatedAtTable}
-          seatedAtTable={seatedAtTable}
-          venueName={venue.name}
-          TableComponent={TableComponent}
-          joinMessage={true}
-          customTables={JAZZBAR_TABLES}
-        />
-        <div
-          className={`jazz-container ${
-            !seatedAtTable ? "container-in-grid" : "container-in-row "
-          }`}
-        >
-          <div
-            key="main-event-container"
-            className={`video ${
-              seatedAtTable
-                ? isVideoFocused
-                  ? "video-focused col-11"
-                  : "col-5 video-not-focused"
-                : ""
-            }`}
-          >
-            <iframe
-              key="main-event"
-              title="main event"
-              width="100%"
-              height="100%"
-              className="youtube-video"
-              src={`${venue.iframeUrl}?autoplay=1`}
-              frameBorder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;"
+        <div style={{ border: "1px solid white" }}>
+          <div className="call-out-band-container-at-table">
+            <CallOutMessageForm
+              onSubmit={handleSubmit(onSubmit)}
+              isMessageToTheBandSent={isMessageToTheBandSent}
+              register={register}
             />
-            {seatedAtTable && (
-              <div className="call-out-band-container-at-table">
-                <CallOutMessageForm
-                  onSubmit={handleSubmit(onSubmit)}
-                  isMessageToTheBandSent={isMessageToTheBandSent}
-                  register={register}
-                />
-              </div>
-            )}
-          </div>
-          <div
-            className={`reaction-bar ${
-              isVideoFocused ? "video-focused" : "video-not-focused"
-            }`}
-          >
-            {!seatedAtTable && (
-              <div className="call-out-band-container">
-                <CallOutMessageForm
-                  onSubmit={handleSubmit(onSubmit)}
-                  isMessageToTheBandSent={isMessageToTheBandSent}
-                  register={register}
-                />
-              </div>
-            )}
             <div className="emoji-container">
               {Reactions.map((reaction) => (
                 <div className="reaction-container">
@@ -239,51 +204,143 @@ const Jazz: React.FunctionComponent<PropsType> = ({ setUserList }) => {
                 </div>
               ))}
             </div>
-            <div
-              className="reaction-mute"
-              onClick={() => dispatch({ type: TOGGLE_MUTE_REACTIONS })}
-              id="toggle-mute-reactions"
-            >
-              <div className="reaction-mute-text">Reactions:</div>
-              <FontAwesomeIcon
-                size="lg"
-                icon={muteReactions ? faVolumeMute : faVolumeUp}
-                color={muteReactions ? "red" : undefined}
-              />
-            </div>
+            Messages to the Band
           </div>
         </div>
-        {seatedAtTable && (
-          <div className="container-in-row">
-            <div
-              className={`${
-                isVideoFocused ? "col-5" : "col-12"
-              } table-container`}
-            >
-              <TableHeader
-                seatedAtTable={seatedAtTable}
-                setSeatedAtTable={setSeatedAtTable}
-                venueName={venue.name}
-              />
-              <div className="jazz-wrapper">
-                <Room roomName={seatedAtTable} setUserList={setUserList} />
-              </div>
-              <TableFooter
-                isVideoFocused={isVideoFocused}
-                setIsVideoFocused={setIsVideoFocused}
-              />
-            </div>
-          </div>
-        )}
-      </div>
-      {!seatedAtTable && (
-        <div className="user-interaction-container">
-          {usersStanding && (
-            <UserList users={usersStanding} limit={26} activity="standing" />
+        <div style={{ border: "1px solid white" }}>
+          {users && (
+            <UserList
+              users={usersInJazzBar}
+              limit={26}
+              activity="listening to jazz"
+            />
           )}
         </div>
-      )}
+      </div>
     </div>
+    //   <div className="scrollable-area">
+    //     <div
+    //       className={`content ${
+    //         !seatedAtTable ? "jazz-bar-grid" : "jazz-bar-table"
+    //       }`}
+    //     >
+    //       <TablesUserList
+    //         setSeatedAtTable={setSeatedAtTable}
+    //         seatedAtTable={seatedAtTable}
+    //         venueName={venue.name}
+    //         TableComponent={TableComponent}
+    //         joinMessage={true}
+    //         customTables={JAZZBAR_TABLES}
+    //       />
+    //       <div
+    //         className={`jazz-container ${
+    //           !seatedAtTable ? "container-in-grid" : "container-in-row "
+    //         }`}
+    //       >
+    //         <div
+    //           key="main-event-container"
+    //           className={`video ${
+    //             seatedAtTable
+    //               ? isVideoFocused
+    //                 ? "video-focused col-11"
+    //                 : "col-5 video-not-focused"
+    //               : ""
+    //           }`}
+    //         >
+    //           <iframe
+    //             key="main-event"
+    //             title="main event"
+    //             width="100%"
+    //             height="100%"
+    //             className="youtube-video"
+    //             src={`${venue.iframeUrl}?autoplay=1`}
+    //             frameBorder="0"
+    //             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;"
+    //           />
+    //           {seatedAtTable && (
+    //             <div className="call-out-band-container-at-table">
+    //               <CallOutMessageForm
+    //                 onSubmit={handleSubmit(onSubmit)}
+    //                 isMessageToTheBandSent={isMessageToTheBandSent}
+    //                 register={register}
+    //               />
+    //             </div>
+    //           )}
+    //         </div>
+    //         <div
+    //           className={`reaction-bar ${
+    //             isVideoFocused ? "video-focused" : "video-not-focused"
+    //           }`}
+    //         >
+    //           {!seatedAtTable && (
+    //             <div className="call-out-band-container">
+    //               <CallOutMessageForm
+    //                 onSubmit={handleSubmit(onSubmit)}
+    //                 isMessageToTheBandSent={isMessageToTheBandSent}
+    //                 register={register}
+    //               />
+    //             </div>
+    //           )}
+    //           <div className="emoji-container">
+    //             {Reactions.map((reaction) => (
+    //               <div className="reaction-container">
+    //                 <button
+    //                   className="reaction"
+    //                   onClick={() => reactionClicked(user, reaction.type)}
+    //                   id={`send-reaction-${reaction.type}`}
+    //                 >
+    //                   <span role="img" aria-label={reaction.ariaLabel}>
+    //                     {reaction.text}
+    //                   </span>
+    //                 </button>
+    //               </div>
+    //             ))}
+    //           </div>
+    //           <div
+    //             className="reaction-mute"
+    //             onClick={() => dispatch({ type: TOGGLE_MUTE_REACTIONS })}
+    //             id="toggle-mute-reactions"
+    //           >
+    //             <div className="reaction-mute-text">Reactions:</div>
+    //             <FontAwesomeIcon
+    //               size="lg"
+    //               icon={muteReactions ? faVolumeMute : faVolumeUp}
+    //               color={muteReactions ? "red" : undefined}
+    //             />
+    //           </div>
+    //         </div>
+    //       </div>
+    //       {seatedAtTable && (
+    //         <div className="container-in-row">
+    //           <div
+    //             className={`${
+    //               isVideoFocused ? "col-5" : "col-12"
+    //             } table-container`}
+    //           >
+    //             <TableHeader
+    //               seatedAtTable={seatedAtTable}
+    //               setSeatedAtTable={setSeatedAtTable}
+    //               venueName={venue.name}
+    //             />
+    //             <div className="jazz-wrapper">
+    //               <Room roomName={seatedAtTable} setUserList={setUserList} />
+    //             </div>
+    //             <TableFooter
+    //               isVideoFocused={isVideoFocused}
+    //               setIsVideoFocused={setIsVideoFocused}
+    //             />
+    //           </div>
+    //         </div>
+    //       )}
+    //     </div>
+    //     {!seatedAtTable && (
+    //       <div className="user-interaction-container">
+    //         {usersStanding && (
+    //           <UserList users={usersStanding} limit={26} activity="standing" />
+    //         )}
+    //       </div>
+    //     )}
+    //   </div>
   );
 };
 
