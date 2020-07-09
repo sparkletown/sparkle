e7511998b2d0945e5702e51805cc220f2757215e
Antoine Eudes <antoine.eudes@gmail.com>
2020-07-09 17:37:28 +0200

    :rotating-light: removed warnings

--- src/components/venues/Jazzbar/Jazzbar.tsx
+++ src/components/venues/Jazzbar/Jazzbar.tsx
@@ -1,18 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import JazzBarSkeletonPage from "./JazzBarSkeletonPage";
 import useProfileInformationCheck from "hooks/useProfileInformationCheck";
 
 import JazzTab from "./JazzTab";
-import { User } from "types/User";
 
 const JazzBar = () => {
   useProfileInformationCheck();
 
-  const [userList, setUserList] = useState<User[]>([]);
-
   return (
     <JazzBarSkeletonPage>
-      <JazzTab setUserList={setUserList} />
+      <JazzTab setUserList={() => null} />
     </JazzBarSkeletonPage>
   );
 };
