ac6ddb0057c20b9926700b0b1d4705a4dce3f2ee
Antoine Eudes <antoine.eudes@gmail.com>
2020-07-21 13:48:23 +0200

    :sparkles: redirection between / and /venue/kansassmittys

--- src/components/venues/VenueRouter/VenueRouter.tsx
+++ src/components/venues/VenueRouter/VenueRouter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import EntranceExperience from "components/venues/Jazzbar/EntranceExperience";
 import EventPage from "components/venues/EventPage";
 import VenuePage from "pages/VenuePage";
@@ -9,6 +9,7 @@ const VenueRouter = () => (
     <Route exact path="/venue/:venueId" component={EntranceExperience} />
     <Route path="/venue/:venueId/entrance/:eventId" component={EventPage} />
     <Route path="/venue/:venueId/event/:eventId" component={VenuePage} />
+    <Route path="/" component={() => <Redirect to="/venue/kansassmittys" />} />
   </Switch>
 );
 
