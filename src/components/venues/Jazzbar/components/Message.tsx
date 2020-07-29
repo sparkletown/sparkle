954e3f3ad0c8596e00553f56989d941e1d448f13
maxbull <maxbull@live.co.uk>
2020-07-29 18:37:20 +0100

    Refactor message component

---
+++ src/components/venues/Jazzbar/components/Message.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { User } from "types/User";
+import "./ReactionList.scss";
+import { RestrictedChatMessage } from "components/context/ChatContext";
+import { DEFAULT_PROFILE_IMAGE } from "settings";
+
+interface MessageProps {
+  sender: User;
+  message: RestrictedChatMessage;
+  onClick: () => void;
+}
+
+export const Message: React.FC<MessageProps> = ({
+  sender,
+  message,
+  onClick,
+}) => {
+  const profileImageSize = 40;
+  return (
+    <div className="message" key={`${message.from}-${message.ts_utc}`}>
+      <img
+        onClick={onClick}
+        key={`${message.from}-messaging-the-band`}
+        className="profile-icon"
+        src={sender.pictureUrl || DEFAULT_PROFILE_IMAGE}
+        title={sender.partyName}
+        alt={`${sender.partyName} profile`}
+        width={profileImageSize}
+        height={profileImageSize}
+      />
+      <div className="message-bubble">{message.text}</div>
+    </div>
+  );
+};
