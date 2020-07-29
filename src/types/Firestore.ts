5a220b84228e3db51b57df2872e412054dbde656
maxbull <maxbull@live.co.uk>
2020-07-29 10:09:24 +0100

    371: Type firestore

---
+++ src/types/Firestore.ts
@@ -0,0 +1,48 @@
+import { Venue } from "./Venue";
+import { Purchase } from "./Purchase";
+
+interface VenueStatus {
+  currentVenue: boolean;
+  venueChats: boolean;
+  venueEvents: boolean;
+  userPurchaseHistory: boolean;
+}
+
+type VenueTimestamps = Record<keyof VenueStatus, number>;
+
+interface VenueChat {
+  ts_utc: { seconds: number; nanoseconds: number };
+  text: string;
+  from: string;
+  to: string;
+  type: string;
+}
+
+interface VenueEvent {
+  name: string;
+  price: number;
+  descriptions: Array<string>;
+  collective_price: number;
+  start_utc_seconds: number;
+  duration_minutes: number;
+}
+
+export interface Firestore {
+  status: {
+    requesting: VenueStatus;
+    requested: VenueStatus;
+    timestamps: VenueTimestamps;
+  };
+  data: {
+    currentVenue: Venue;
+    venueChats: Record<string, VenueChat> | null;
+    venueEvents: Record<string, VenueEvent>;
+    userPurchaseHistory: Record<string, Purchase>;
+  };
+  ordered: {
+    currentVenue: Array<Venue>;
+    venueChats: Array<VenueChat>;
+    venueEvents: Array<VenueEvent>;
+    userPurchaseHistory: Array<Purchase>;
+  };
+}
