264c3dc9d029a8289c919ef14252f941601a5d8e
maxbull <maxbull@live.co.uk>
2020-07-30 12:45:52 +0100

    349: Add create event button

--- src/pages/Account/Admin.tsx
+++ src/pages/Account/Admin.tsx
@@ -3,15 +3,21 @@ import "firebase/storage";
 import "./Account.scss";
 import { useUser } from "hooks/useUser";
 import AuthenticationModal from "components/organisms/AuthenticationModal";
+import WithNavigationBar from "components/organisms/WithNavigationBar";
 
 const Admin: React.FC = () => {
   const { user } = useUser();
 
   return (
-    <div className="page-container">
-      Admin
-      <AuthenticationModal show={!user} onHide={() => {}} />
-    </div>
+    <WithNavigationBar>
+      <div className="container admin-container">
+        <div className="title">Admin</div>
+        <AuthenticationModal show={!user} onHide={() => {}} />
+        <div className="centered-flex">
+          <button className="btn btn-primary">Create a venue</button>
+        </div>
+      </div>
+    </WithNavigationBar>
   );
 };
 
