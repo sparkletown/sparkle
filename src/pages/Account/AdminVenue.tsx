d713b39eb94a677b18785a28aa87c0503202d8d7
maxbull <maxbull@live.co.uk>
2020-07-30 18:18:44 +0100

    351: Add admin venue validation logic

--- src/pages/Account/AdminVenue.tsx
+++ src/pages/Account/AdminVenue.tsx
@@ -1,16 +1,53 @@
-import React, { useCallback, useState } from "react";
+import React, { useMemo } from "react";
 import WithNavigationBar from "components/organisms/WithNavigationBar";
+import { useForm, FieldError } from "react-hook-form";
+import * as Yup from "yup";
 
 const LONG_DESCRIPTION_PLACEHOLDER =
   "Describe what is unique andwonderful and sparkling about your venue";
 
+interface FormInputs {
+  name: string;
+  bannerImageUrl: FileList;
+  logoImageUrl: FileList;
+  tagline: string;
+  longDescription: string;
+}
+
+const validationSchema = Yup.object().shape<FormInputs>({
+  name: Yup.string()
+    .required("Display name required")
+    .test(
+      "dfsd",
+      "Must have alphanumeric characters",
+      (val: string) => val.replace(/\W/g, "").length > 0
+    ),
+  bannerImageUrl: Yup.mixed<FileList>()
+    .required("Required")
+    .test(
+      "bannerImageUrl",
+      "Image required",
+      (val: FileList) => val.length > 0
+    ),
+  logoImageUrl: Yup.mixed<FileList>()
+    .required("Required")
+    .test("logoImageUrl", "Image required", (val: FileList) => val.length > 0),
+  tagline: Yup.string().required("Required"),
+  longDescription: Yup.string().required("Required"),
+});
+
 export const AdminVenue: React.FC = () => {
-  const [imageBannerUrl, setImageBannerUrl] = useState<string>(
-    "https://www.printedtoday.co.uk/wp-content/uploads/2019/02/Pvc_Banners_Next_Day-1.png"
-  );
-  const [imageLogoUrl, setImageLogoUrl] = useState<string>(
-    "https://www.printedtoday.co.uk/wp-content/uploads/2019/02/Pvc_Banners_Next_Day-1.png"
-  );
+  const { register, handleSubmit, errors, watch } = useForm<
+    Partial<FormInputs>
+  >({
+    mode: "onSubmit",
+    reValidateMode: "onChange",
+    validationSchema,
+    defaultValues: {},
+  });
+  const values = watch();
+  const onSubmit = () => {};
+  const urlSafeName = values.name?.replace(/\W/g, "");
 
   return (
     <WithNavigationBar>
@@ -19,31 +56,41 @@ export const AdminVenue: React.FC = () => {
           Create your space in{" "}
           <span className="title-adornment">The Sparkleverse</span>
         </h3>
-        <div className="form">
+        <form className="form" onSubmit={handleSubmit(onSubmit)}>
           <div className="input-container">
             <div className="input-title">Name your space</div>
             <input
+              name="name"
+              ref={register}
               className="wide-input-block"
               placeholder="This can't be changed in the future"
             />
-            <div className="input-subtext">
-              The URL of your space will be: ...
-            </div>
+            {errors.name ? (
+              <span className="input-error">{errors.name.message}</span>
+            ) : (
+              <div className="input-subtext">
+                {`The URL of your space will be: ${urlSafeName}`}
+              </div>
+            )}
           </div>
           <div className="input-container">
             <div className="input-title">Upload a banner page photo</div>
             <ImageInput
-              imageUrl={imageBannerUrl}
-              onSetImage={setImageBannerUrl}
+              name={"bannerImageUrl"}
+              image={values.bannerImageUrl}
+              ref={register}
+              error={errors.bannerImageUrl}
             />
           </div>
           <div className="input-container">
             <div className="input-title">Upload a square logo</div>
             <ImageInput
+              ref={register}
+              image={values.logoImageUrl}
+              name={"logoImageUrl"}
               containerClassName="input-logo-container"
               imageClassName="input-logo-image"
-              imageUrl={imageLogoUrl}
-              onSetImage={setImageLogoUrl}
+              error={errors.logoImageUrl}
             />
           </div>
           <div className="input-container">
@@ -52,6 +99,9 @@ export const AdminVenue: React.FC = () => {
               className="wide-input-block"
               placeholder="Briefly say what people will find here"
             />
+            {errors.tagline && (
+              <span className="input-error">{errors.tagline.message}</span>
+            )}
           </div>
           <div className="input-container">
             <div className="input-title">Long description</div>
@@ -59,44 +109,65 @@ export const AdminVenue: React.FC = () => {
               className="wide-input-block input-centered align-left"
               placeholder={LONG_DESCRIPTION_PLACEHOLDER}
             />
+            {errors.longDescription && (
+              <span className="input-error">
+                {errors.longDescription.message}
+              </span>
+            )}
           </div>
-        </div>
+          <div className="centered-flex input-container">
+            <div>
+              <input
+                className="btn btn-primary"
+                type="submit"
+                value="Create venue"
+              />
+            </div>
+          </div>
+        </form>
       </div>
     </WithNavigationBar>
   );
 };
 
 interface ImageInputProps {
-  imageUrl?: string;
-  onSetImage: (val: string) => void;
+  name: string;
+  image?: FileList;
   containerClassName?: string;
   imageClassName?: string;
+  error?: FieldError;
 }
 
-const ImageInput: React.FC<ImageInputProps> = (props) => {
-  const { imageUrl, onSetImage, containerClassName, imageClassName } = props;
+// eslint-disable-next-line
+const ImageInput = React.forwardRef<HTMLInputElement, ImageInputProps>(
+  (props, ref) => {
+    const { image, containerClassName, imageClassName, name, error } = props;
 
-  const handleImageChange = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      if (!e.target.files?.length) return;
-      const file = e.target.files[0];
-      const fileUrl = URL.createObjectURL(file);
-      onSetImage(fileUrl);
-    },
-    [onSetImage]
-  );
+    const imageUrl = useMemo(
+      () => image && image.length > 0 && URL.createObjectURL(image[0]),
+      [image]
+    );
 
-  return (
-    <div className={`image-input default-container ${containerClassName}`}>
-      {imageUrl && (
-        <img className={`default-image ${imageClassName}`} src={imageUrl} />
-      )}
-      <input
-        type="file"
-        accept="image/x-png,image/gif,image/jpeg"
-        onChange={handleImageChange}
-        className="default-input"
-      />
-    </div>
-  );
-};
+    return (
+      <>
+        <div className={`image-input default-container ${containerClassName}`}>
+          {imageUrl ? (
+            <img className={`default-image ${imageClassName}`} src={imageUrl} />
+          ) : (
+            <div className="centered-flex empty">
+              <h6 className=" text">Click to upload an image</h6>
+            </div>
+          )}
+          <input
+            name={name}
+            type="file"
+            ref={ref}
+            accept="image/x-png,image/gif,image/jpeg"
+            className="default-input"
+          />
+        </div>
+        {error?.message && <span className="input-error">{error.message}</span>}
+      </>
+    );
+  }
+);
