b3a4cbd9e40f423001d4d2cb28d353ed61d0126c
maxbull <maxbull@live.co.uk>
2020-07-30 15:13:11 +0100

    351: Add square input, tagline, and long description inputs

--- src/pages/Account/AdminVenue.tsx
+++ src/pages/Account/AdminVenue.tsx
@@ -1,26 +1,65 @@
 import React, { useCallback, useState } from "react";
 import WithNavigationBar from "components/organisms/WithNavigationBar";
 
+const LONG_DESCRIPTION_PLACEHOLDER =
+  "Describe what is unique andwonderful and sparkling about your venue";
+
 export const AdminVenue: React.FC = () => {
-  const [imageFileUrl, setImageFileUrl] = useState<string>(
+  const [imageBannerUrl, setImageBannerUrl] = useState<string>(
+    "https://www.printedtoday.co.uk/wp-content/uploads/2019/02/Pvc_Banners_Next_Day-1.png"
+  );
+  const [imageLogoUrl, setImageLogoUrl] = useState<string>(
     "https://www.printedtoday.co.uk/wp-content/uploads/2019/02/Pvc_Banners_Next_Day-1.png"
   );
 
   return (
     <WithNavigationBar>
       <div id="admin-venue" className="container admin-container">
-        <div className="title">
+        <h3 className="title">
           Create your space in{" "}
           <span className="title-adornment">The Sparkleverse</span>
-        </div>
+        </h3>
         <div className="form">
-          <input
-            name="email"
-            className="wide-input-block"
-            placeholder="Name your space"
-          />
-          <div className="url-text">The URL of your space will be: ...</div>
-          <ImageInput imageUrl={imageFileUrl} onSetImage={setImageFileUrl} />
+          <div className="input-container">
+            <div className="input-title">Name your space</div>
+            <input
+              className="wide-input-block"
+              placeholder="This can't be changed in the future"
+            />
+            <div className="input-subtext">
+              The URL of your space will be: ...
+            </div>
+          </div>
+          <div className="input-container">
+            <div className="input-title">Upload a banner page photo</div>
+            <ImageInput
+              imageUrl={imageBannerUrl}
+              onSetImage={setImageBannerUrl}
+            />
+          </div>
+          <div className="input-container">
+            <div className="input-title">Upload a square logo</div>
+            <ImageInput
+              containerClassName="input-logo-container"
+              imageClassName="input-logo-image"
+              imageUrl={imageLogoUrl}
+              onSetImage={setImageLogoUrl}
+            />
+          </div>
+          <div className="input-container">
+            <div className="input-title">Tagline</div>
+            <input
+              className="wide-input-block"
+              placeholder="Briefly say what people will find here"
+            />
+          </div>
+          <div className="input-container">
+            <div className="input-title">Long description</div>
+            <textarea
+              className="wide-input-block input-centered align-left"
+              placeholder={LONG_DESCRIPTION_PLACEHOLDER}
+            />
+          </div>
         </div>
       </div>
     </WithNavigationBar>
@@ -31,10 +70,11 @@ interface ImageInputProps {
   imageUrl?: string;
   onSetImage: (val: string) => void;
   containerClassName?: string;
+  imageClassName?: string;
 }
 
 const ImageInput: React.FC<ImageInputProps> = (props) => {
-  const { imageUrl, onSetImage, containerClassName } = props;
+  const { imageUrl, onSetImage, containerClassName, imageClassName } = props;
 
   const handleImageChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -48,7 +88,9 @@ const ImageInput: React.FC<ImageInputProps> = (props) => {
 
   return (
     <div className={`image-input default-container ${containerClassName}`}>
-      {imageUrl && <img className="default-image" src={imageUrl} />}
+      {imageUrl && (
+        <img className={`default-image ${imageClassName}`} src={imageUrl} />
+      )}
       <input
         type="file"
         accept="image/x-png,image/gif,image/jpeg"
