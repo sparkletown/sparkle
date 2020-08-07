3682b926219445ebf62243b0d5aaa531c3392fc4
Mathieu Dähne <mathieud@theodo.co.uk>
2020-08-07 20:12:27 +0200

    Limit loaded users everywhere

--- src/hooks/useConnectRecentUsers.ts
+++ src/hooks/useConnectRecentUsers.ts
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import { useFirestoreConnect } from "react-redux-firebase";
+import dayjs from "dayjs";
 
 // Prevent App Crash by only loading users who connected after the 31st of July
 // @debt performance this has to be fixed better
 const useConnectRecentUsers = () => {
+  const [yesterday] = useState(dayjs().add(-1, "day").unix());
   useFirestoreConnect([
     {
       collection: "users",
-      where: [["lastSeenAt", ">", 1596060000]],
+      where: [["lastSeenAt", ">", yesterday]],
       storeAs: "users",
     },
   ]);
