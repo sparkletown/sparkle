1fecfe62b8f1c03d59b533aec82a8c308fc7336e
Chris Adams <chris@cadams.com.au>
2020-08-07 03:38:56 -0700

    Make it like, work, and stuff

--- src/components/organisms/AppRouter/AppRouter.jsx
+++ src/components/organisms/AppRouter/AppRouter.jsx
@@ -58,8 +58,11 @@ const AppRouter = () => {
     <Router basename="/">
       <Switch>
         <Route
-          path="/SparkleVerse"
-          component={() => <Redirect to={SPARKLEVERSE_MARKETING_URL} />}
+          path="/sparkleverse"
+          component={() => {
+            window.location.href = SPARKLEVERSE_MARKETING_URL;
+            return null;
+          }}
         />
         <Route path="/account/profile" component={Profile} />
         <Route path="/account/questions" component={Questions} />
