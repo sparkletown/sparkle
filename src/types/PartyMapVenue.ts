9a311cea70b273859de6e140048795ac16625e97
Mathieu Dähne <mathieud@theodo.co.uk>
2020-08-11 12:01:24 +0200

    PartyMap type proposition

--- src/types/PartyMapVenue.ts
+++ src/types/PartyMapVenue.ts
@@ -1,6 +1,7 @@
 import { Venue } from "./Venue";
 import { VenueTemplate } from "./VenueTemplate";
-import { RoomData } from "./RoomData";
+// import { RoomData } from "./RoomData";
+// import { EventData } from "./EventData";
 
 export const isPartyMapVenue = (venue: Venue): venue is PartyMapVenue =>
   venue.template === VenueTemplate.partymap;
@@ -16,15 +17,49 @@ export interface PartyMapVenue extends Venue {
     text: string;
     program_url?: string;
   };
-  start_utc_seconds: number;
-  duration_hours: number;
+  // These fields belong to an Event
+  // start_utc_seconds: number;
+  // duration_hours: number;
+  // party_name: string;
+
   entrance_hosted_hours: number;
-  party_name: string;
   unhosted_entry_video_url: string;
   map_url: string;
   map_viewbox: string;
   password: string;
   admin_password?: string;
   owners: string[];
-  rooms: RoomData[];
+  // rooms: RoomData[];
+  // subvenues replaces rooms
+  // Should this be a subcollection?
+  subVenues: {
+    id: string;
+    on_map: boolean;
+    attendance_x?: string;
+    attendance_y?: string;
+  };
+}
+
+// will be a subcollection of PartyMapVenue, same as current Events
+export interface PartyMapEvent {
+  name: string;
+  start_utc_seconds: number;
+  description: string;
+  descriptions?: string[];
+  duration_minutes: number;
+  // Should this be a subcollection?
+  subVenues: {
+    id: string;
+    schedule: PartyMapScheduleItem[];
+  };
+}
+
+// this is roughlt EventData
+export interface PartyMapScheduleItem {
+  start_minute: number;
+  duration_minutes: number;
+  host: string;
+  name: string;
+  text: string;
+  interactivity: string;
 }
