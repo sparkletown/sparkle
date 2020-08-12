a92e5bc85f9bd48215151107a6f43e58ca091530
Mathieu Dähne <mathieud@theodo.co.uk>
2020-08-12 18:09:11 +0200

    Remove zoom

--- src/components/templates/Preplaya/Preplaya.tsx
+++ src/components/templates/Preplaya/Preplaya.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 import { useFirestoreConnect } from "react-redux-firebase";
 import { Modal } from "react-bootstrap";
 import { VenueLandingPage } from "pages/VenueLandingPage";
@@ -16,14 +16,13 @@ const Preplaya = () => {
   useFirestoreConnect("venues");
   const [showModal, setShowModal] = useState(false);
   const [venue, setVenue] = useState<Venue>();
-  const [zoom, setZoom] = useState(1);
   const [scale, setScale] = useState(
-    (window.innerWidth / PLAYA_WIDTH_AND_HEIGHT) * zoom
+    window.innerWidth / PLAYA_WIDTH_AND_HEIGHT
   );
 
   useEffect(() => {
     const rescale = () => {
-      setScale((window.innerWidth / PLAYA_WIDTH_AND_HEIGHT) * zoom);
+      setScale(window.innerWidth / PLAYA_WIDTH_AND_HEIGHT);
     };
 
     window.addEventListener("resize", rescale);
@@ -31,10 +30,6 @@ const Preplaya = () => {
       window.removeEventListener("resize", rescale);
     };
   }, []);
-
-  useEffect(() => {
-    setScale((window.innerWidth / PLAYA_WIDTH_AND_HEIGHT) * zoom);
-  }, [zoom]);
 
   const { venues } = useSelector((state) => ({
     venues: state.firestore.ordered.venues,
@@ -52,7 +47,6 @@ const Preplaya = () => {
           className="playa-background"
           src="/maps/playa2d.jpg"
           alt="Playa Background Map"
-          style={{ width: `${zoom * 100}%` }}
         />
         {venues?.filter(isPlaced).map((venue) => (
           <div
@@ -71,23 +65,6 @@ const Preplaya = () => {
             />
           </div>
         ))}
-      </div>
-      <div className="zoom-controls">
-        <div
-          className="zoom-action"
-          onClick={() => setZoom((zoom) => zoom + 1)}
-        >
-          +
-        </div>
-        <div
-          className="zoom-action"
-          onClick={() => setZoom((zoom) => Math.max(1, zoom - 1))}
-        >
-          -
-        </div>
-        <div className="zoom-action" onClick={() => setZoom(1)}>
-          reset
-        </div>
       </div>
       <Modal show={showModal} onHide={() => setShowModal(false)}>
         {venue && <VenueLandingPage venue={venue} venueRequestStatus={true} />}
