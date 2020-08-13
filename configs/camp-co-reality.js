94533e0f7387b6187ef941935d7651e2aee32016
Chris Adams <chris@cadams.com.au>
2020-08-13 02:36:02 -0700

    Reposition the rooms on the camp

--- configs/camp-co-reality.js
+++ configs/camp-co-reality.js
@@ -15,8 +15,8 @@ module.exports = {
   owners: ["tpl7htc3ceOgWI5J4xdLTNOfehh1"], // staging
   rooms: [
     {
-      x_percent: 10,
-      y_percent: 10,
+      x_percent: 65,
+      y_percent: 32,
       width_percent: 20,
       height_percent: 20,
       title: "The Eco-loos",
@@ -24,6 +24,90 @@ module.exports = {
       about: "A space for you to meet people and encounter endless possibility",
       image_url:
         "https://firebasestorage.googleapis.com/v0/b/co-reality-staging.appspot.com/o/users%2FYFLe3gR6LnPwn6sCWqgys1aXbDG2%2Fimages%2FSparkle_CAMP-CR_ECOLOOS.png?alt=media&token=65bbc735-bb2b-4cec-b31c-20d091b62e0a",
+      url: "https://zoom.us/j/4157588161",
+    },
+    {
+      x_percent: 15,
+      y_percent: 37,
+      width_percent: 20,
+      height_percent: 20,
+      title: "Krab",
+      subtitle: "",
+      about: "",
+      image_url:
+        "https://firebasestorage.googleapis.com/v0/b/co-reality-staging.appspot.com/o/users%2FYFLe3gR6LnPwn6sCWqgys1aXbDG2%2Fimages%2FSparkle_CAMP-CR_KRAB.png?alt=media&token=28ee60a7-d5b8-4ef1-8d8f-88a399a699f6",
+      url: "https://zoom.us/j/4157588161",
+    },
+    {
+      x_percent: 3,
+      y_percent: 2,
+      width_percent: 20,
+      height_percent: 20,
+      title: "",
+      subtitle: "",
+      about: "",
+      image_url:
+        "https://firebasestorage.googleapis.com/v0/b/co-reality-staging.appspot.com/o/users%2FYFLe3gR6LnPwn6sCWqgys1aXbDG2%2Fimages%2FSparkle_CAMP-CR_UFO2.png?alt=media&token=7efee2aa-cc2c-46ce-bf5e-b70e25fad32f",
+      url: "https://zoom.us/j/4157588161",
+    },
+    {
+      x_percent: 33,
+      y_percent: 2,
+      width_percent: 38,
+      height_percent: 38,
+      title: "",
+      subtitle: "",
+      about: "",
+      image_url:
+        "https://firebasestorage.googleapis.com/v0/b/co-reality-staging.appspot.com/o/users%2FYFLe3gR6LnPwn6sCWqgys1aXbDG2%2Fimages%2FCamp_CoReality_logo.png?alt=media&token=042f213c-eaf5-43e5-b388-04f5b4b00391",
+      url: "https://zoom.us/j/4157588161",
+    },
+    {
+      x_percent: 34,
+      y_percent: 35,
+      width_percent: 37,
+      height_percent: 37,
+      title: "",
+      subtitle: "",
+      about: "",
+      image_url:
+        "https://firebasestorage.googleapis.com/v0/b/co-reality-staging.appspot.com/o/users%2FYFLe3gR6LnPwn6sCWqgys1aXbDG2%2Fimages%2FSparkle_CAMP-CR.png?alt=media&token=8e8379c9-9625-4c6b-932d-fd7cf2435872",
+      url: "https://zoom.us/j/4157588161",
+    },
+    {
+      x_percent: 5,
+      y_percent: 60,
+      width_percent: 25,
+      height_percent: 25,
+      title: "",
+      subtitle: "",
+      about: "",
+      image_url:
+        "https://firebasestorage.googleapis.com/v0/b/co-reality-staging.appspot.com/o/users%2FYFLe3gR6LnPwn6sCWqgys1aXbDG2%2Fimages%2FSparkle_CAMP-CR_cuddle.png?alt=media&token=951a7786-2259-43e8-8edf-03a80c4382e6",
+      url: "https://zoom.us/j/4157588161",
+    },
+    {
+      x_percent: 75,
+      y_percent: 43,
+      width_percent: 20,
+      height_percent: 20,
+      title: "",
+      subtitle: "",
+      about: "",
+      image_url:
+        "https://firebasestorage.googleapis.com/v0/b/co-reality-staging.appspot.com/o/users%2FYFLe3gR6LnPwn6sCWqgys1aXbDG2%2Fimages%2FSparkle_CAMP-CR_KITCHEN.png?alt=media&token=e6d2d8ad-6708-49d3-8f33-1833aaa1fa01",
+      url: "https://zoom.us/j/4157588161",
+    },
+    {
+      x_percent: 58,
+      y_percent: 65,
+      width_percent: 19,
+      height_percent: 19,
+      title: "",
+      subtitle: "",
+      about: "",
+      image_url:
+        "https://firebasestorage.googleapis.com/v0/b/co-reality-staging.appspot.com/o/users%2FYFLe3gR6LnPwn6sCWqgys1aXbDG2%2Fimages%2FSparkle_CAMP-CR_MISINFO.png?alt=media&token=316ecd31-0361-4c19-94ae-804d46c43207",
       url: "https://zoom.us/j/4157588161",
     },
   ],
