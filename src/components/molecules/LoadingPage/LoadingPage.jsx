58204a27fc0693e4eb85451a751f2cf7d7cf6229
Lorenzo <lorenzos@theodo.co.uk>
2020-08-13 18:19:52 +0100

    Loading icon displaying when loading the playa page

---
+++ src/components/molecules/LoadingPage/LoadingPage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./sparkle-styling/loading.scss";
+
+export const LoadingPage = () => {
+  return (
+    <div className="loading-screen">
+      <div className="loading-content">
+        <div className="burningman-loading-container">
+          <span className="loading-sparkle-1"></span>
+          <span className="loading-sparkle-2"></span>
+          <div className="burningman-loading">
+            <div className="burningman-loading-anim"></div>
+          </div>
+        </div>
+        <span className="loading-randomquote"></span>
+      </div>
+    </div>
+  );
+};
