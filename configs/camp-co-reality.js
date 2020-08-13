74061c70d0e6ad14ff42b250c596cd2cf6ee3f2f
Chris Adams <chris@cadams.com.au>
2020-08-13 00:55:23 -0700

    Add more scripts

---
+++ configs/camp-co-reality.js
@@ -0,0 +1,53 @@
+module.exports = {
+  template: "themecamp",
+  host: {
+    url: "https://co-reality.co",
+    name: "Co-reality Collective",
+    icon: "/collective-icon.png",
+  },
+  description: {
+    name: "Welcome to Camp Co-reality.",
+    // program_url: "TODO",
+  },
+  name: "Camp Co-reality",
+  map_url:
+    "https://firebasestorage.googleapis.com/v0/b/co-reality-staging.appspot.com/o/users%2FYFLe3gR6LnPwn6sCWqgys1aXbDG2%2Fimages%2FSPA_050820_Camp_Co_Reality_Blank_120820_1345.png?alt=media&token=4d55eb5b-e0ee-4477-871b-8c09ceb013c3",
+  owners: ["tpl7htc3ceOgWI5J4xdLTNOfehh1"], // staging
+  rooms: [
+    {
+      x_percent: 10,
+      y_percent: 10,
+      width_percent: 20,
+      height_percent: 20,
+      title: "The Eco-loos",
+      subtitle: "Environmentally friendly unhosted space",
+      about: "A space for you to meet people and encounter endless possibility",
+      image_url:
+        "https://firebasestorage.googleapis.com/v0/b/co-reality-staging.appspot.com/o/users%2FYFLe3gR6LnPwn6sCWqgys1aXbDG2%2Fimages%2FSparkle_CAMP-CR_ECOLOOS.png?alt=media&token=65bbc735-bb2b-4cec-b31c-20d091b62e0a",
+      url: "https://zoom.us/j/4157588161",
+    },
+  ],
+  config: {
+    theme: {
+      primaryColor: "orange",
+    },
+    landingPageConfig: {
+      coverImageUrl:
+        "https://firebasestorage.googleapis.com/v0/b/co-reality-staging.appspot.com/o/users%2FYFLe3gR6LnPwn6sCWqgys1aXbDG2%2Fimages%2FSPA_050820_Camp_Co_Reality_Blank_120820_1345.png?alt=media&token=4d55eb5b-e0ee-4477-871b-8c09ceb013c3",
+      subtitle:
+        "Our camp is an example of what's possible in the SparkleVerse.",
+    },
+  },
+  host: {
+    icon: "/collective-icon.png",
+  },
+  profile_questions: [],
+  code_of_conduct_questions: [],
+  mapIconImageUrl:
+    "https://firebasestorage.googleapis.com/v0/b/co-reality-staging.appspot.com/o/assets%2FmapIcons%2FSparkle_SmallTent_Single_1.png?alt=media&token=e1b1fc2c-dccc-4626-b567-ae59d1c15be8",
+  placement: {
+    x: 2000,
+    y: 2000,
+    state: "placed",
+  },
+};
