bc2aa573cd03b213266e15e2502a646f79090e16
Chris Adams <chris@cadams.com.au>
2020-08-19 10:07:10 -0700

    Initial potentially working relay

--- backend/websocket-relay/src/index.ts
+++ backend/websocket-relay/src/index.ts
@@ -1,45 +1,79 @@
-import { validate } from "jsonschema";
 import http from "http";
 import sockjs from "sockjs";
 import redis from "redis";
 
-// Setup Redis pub/sub.
-// NOTE: You must create two Redis clients, as
-// the one that subscribes can't also publish.
+// Setup Redis pub/sub. Need two Redis clients, as the one that subscribes can't also publish.
 const pub = redis.createClient();
 const sub = redis.createClient();
 sub.subscribe("global");
 
-const clients: sockjs.Connection[] = [];
+type Connection = {
+  conn: sockjs.Connection;
+  uid?: string;
+};
+
+export enum RelayMessageType {
+  Hello = "hello",
+  Update = "update",
+}
+
+export type RelayMessage = {
+  type: RelayMessageType;
+  uid?: string;
+  x?: number;
+  y?: number;
+  speaking?: boolean;
+};
+
+const clients: { [id: string]: Connection } = {};
 const handler = sockjs.createServer();
 
 // Listen for messages being published to this server.
 sub.on("message", function (channel, msg) {
-  // Broadcast the message to all connected clients on this server.
-  for (var i = 0; i < clients.length; i++) {
-    clients[i].write(msg);
+  // Broadcast to all connected clients
+  for (const conn of Object.values(clients)) {
+    conn.conn.write(msg);
   }
 });
 
-const validate = (message, conn) => {
-  const errors: string[] = [];
-  return errors;
-};
-
-// Setup our SockJS server.
 handler.on("connection", function (conn) {
-  clients.push(conn);
+  clients[conn.id] = { conn };
 
-  conn.on("data", function (message) {
-    const verrs = validate(message, conn);
-    if (verrs.length) {
-      verrs.forEach((err) => conn.write(err));
+  conn.on("data", function (data) {
+    console.log("data:", data);
+    try {
+      const client = clients[conn.id];
+      const message: RelayMessage = JSON.parse(data);
+      switch (message.type) {
+        case RelayMessageType.Hello:
+          if (client.uid) {
+            throw new Error("Cannot set uid twice");
+          }
+          if (!message.uid) {
+            throw new Error("Hello must have uid");
+          }
+          client.uid = message.uid;
+          break;
+        case RelayMessageType.Update:
+          if (!client.uid) {
+            throw new Error("Must Hello first");
+          }
+          if (message.uid !== client.uid) {
+            throw new Error("UID mismatch");
+          }
+          if (!message.x || !message.y || !message.speaking) {
+            throw new Error("Missing data");
+          }
+          pub.publish("global", JSON.stringify(message));
+      }
+    } catch (e) {
+      conn.write(`Bad message: ${data}; error: ${e}`);
+      conn.close();
     }
-    pub.publish("global", message);
   });
 
   conn.on("close", function () {
-    clients.splice(clients.indexOf(conn), 1);
+    delete clients[conn.id];
   });
 });
 
