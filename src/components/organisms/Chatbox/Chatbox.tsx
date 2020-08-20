27b2e8d9382ecc82e54112eb9109696741be820e
Mathieu Dähne <mathieud@theodo.co.uk>
2020-08-20 17:49:37 +0200

    Try and fix perf issue with the videos

--- src/components/organisms/Chatbox/Chatbox.tsx
+++ src/components/organisms/Chatbox/Chatbox.tsx
@@ -259,7 +259,7 @@ const Chatbox: React.FunctionComponent<PropsType> = ({
                 user &&
                 chatsToDisplay.map((chat) => (
                   <ChatMessage
-                    key={chat.ts_utc.valueOf()}
+                    key={`${chat.ts_utc.valueOf()}-${chat.id}`}
                     user={user}
                     users={users}
                     setSelectedUserProfile={setSelectedUserProfile}
