83c7aaf8526cfa0014ae2db4d49871333b271020
Lorenzo <lorenzos@theodo.co.uk>
2020-08-28 16:02:30 +0100

    CI test

--- src/utils/getLinkFromText.tsx
+++ src/utils/getLinkFromText.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const getLinkFromText = (text: string) => {
-  const urlRegex = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+  const urlRegex = /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
   const newText = text.split(" ");
   return (
     <>
