89e160644caa49a943dee10fcae9f09ee63ef366
Dimo Bachev <dibat@abv.bg>
2020-09-01 16:56:29 +0300

    Fixes for the playa and venue chat - https://trello.com/c/M4QW5YVS 1. Show author and time of the message above the bubble next to profile pic 2. Long bubble text are displayed on multiple lines, and shared URL are broken to not bust out of the container 3. Restyled chat scrolling bar

--- src/components/molecules/Message/Message.tsx
+++ src/components/molecules/Message/Message.tsx
@@ -4,6 +4,7 @@ import { RestrictedChatMessage } from "components/context/ChatContext";
 import { DEFAULT_PROFILE_IMAGE } from "settings";
 import { WithId } from "utils/id";
 import { getLinkFromText } from "../../../utils/getLinkFromText";
+import { formatUtcSeconds } from "../../../utils/time";
 
 interface MessageProps {
   sender: WithId<User>;
@@ -16,19 +17,28 @@ export const Message: React.FC<MessageProps> = ({
   message,
   onClick,
 }) => {
-  const profileImageSize = 40;
+  const profileImageSize = 30;
   return (
-    <div className="message" key={`${message.from}-${message.ts_utc}`}>
-      <img
-        onClick={onClick}
-        key={`${message.from}-messaging-the-band`}
-        className="profile-icon"
-        src={sender.pictureUrl || DEFAULT_PROFILE_IMAGE}
-        title={sender.partyName}
-        alt={`${sender.partyName} profile`}
-        width={profileImageSize}
-        height={profileImageSize}
-      />
+    <div
+      className="message chat-message"
+      key={`${message.from}-${message.ts_utc}`}
+    >
+      <div className="sender-info">
+        <img
+          onClick={onClick}
+          key={`${message.from}-messaging-the-band`}
+          className="profile-icon"
+          src={sender.pictureUrl || DEFAULT_PROFILE_IMAGE}
+          title={sender.partyName}
+          alt={`${sender.partyName} profile`}
+          width={profileImageSize}
+          height={profileImageSize}
+        />
+        <div>
+          {sender.partyName}{" "}
+          <span className="timestamp">{formatUtcSeconds(message.ts_utc)}</span>
+        </div>
+      </div>
       <div className="message-bubble split-words">
         {getLinkFromText(message.text)}
       </div>
