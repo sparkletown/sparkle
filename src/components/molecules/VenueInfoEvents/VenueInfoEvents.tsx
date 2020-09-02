ff6dcb7e3d215e01c65ca97204b96598c7807142
Denis Dimitrov <dedimitrov@melontech.com>
2020-09-02 12:27:04 +0300

    apply ui fixes on the venue cards

--- src/components/molecules/VenueInfoEvents/VenueInfoEvents.tsx
+++ src/components/molecules/VenueInfoEvents/VenueInfoEvents.tsx
@@ -53,40 +53,40 @@ const VenueInfoEvents: React.FunctionComponent<PropsType> = ({
           </>
         ) : (
           <>
-            <div className="title-container">
-              <img src="/sparkle-icon.png" alt="sparkle icon" />
-              <span
-                style={{ fontSize: 20, fontWeight: "bold", color: "yellow" }}
-              >{`What's on now`}</span>
-            </div>
-            <div className="description-container">
-              {eventsNow.length > 0 ? (
-                <>
-                  <div className="events-list">
-                    <EventDisplay
-                      event={eventsNow[0]}
-                      venue={venue}
-                      joinNowButton={joinNowButton}
-                    />
-                  </div>
-                </>
-              ) : (
-                <div className="yellow">No events currently</div>
-              )}
-            </div>
+            {eventsNow.length ? (
+              <div className="whatson-container">
+                <div className="whatson-title-container">What's on now</div>
+                <div className="whatson-description-container">
+                  {eventsNow.map((event) => (
+                    <>
+                      <div className="whatson-description-container-title">
+                        {event.name}
+                      </div>
+                      <div className="whatson-description-container-description">
+                        {event.description}
+                      </div>
+                    </>
+                  ))}
+                </div>
+              </div>
+            ) : (
+              <></>
+            )}
           </>
         )}
       </div>
       {showButton && (
         <div className="centered-flex">
           <button
-            className="btn btn-primary"
+            className={`btn ${
+              eventsNow.length ? "btn-primary" : "btn-secondary"
+            } btn-block`}
             // @debt would be nice not to refresh the page
             onClick={() =>
               (window.location.href = venuePlayaPreviewUrl(venue.id))
             }
           >
-            View on Playa
+            {eventsNow.length ? "Join now" : "View on Playa"}
           </button>
         </div>
       )}
