e382c97c77c4ca6c33225e86111a1a1b1678bbb2
Chris Adams <chris@cadams.com.au>
2020-09-16 05:29:25 +0300

    Remove code of conduct questions and date of birth from registration for non burn

--- src/components/organisms/AuthenticationModal/RegisterForm/RegisterForm.tsx
+++ src/components/organisms/AuthenticationModal/RegisterForm/RegisterForm.tsx
@@ -75,9 +75,9 @@ const RegisterForm: React.FunctionComponent<PropsType> = ({
     try {
       if (IS_BURN) await axios.get(CODE_CHECK_URL + data.code);
       const auth = await signUp(data);
-      if (auth.user) {
+      if (IS_BURN && auth.user) {
         updateUserPrivate(auth.user.uid, {
-          codes_used: IS_BURN ? [data.code] : [],
+          codes_used: [data.code],
           date_of_birth: data.date_of_birth,
         });
       }
@@ -147,27 +147,30 @@ const RegisterForm: React.FunctionComponent<PropsType> = ({
             <span className="input-error">Password is required</span>
           )}
         </div>
-        <div className="input-group">
-          <input
-            name="date_of_birth"
-            className="input-block input-centered"
-            type="date"
-            max={dayjs().subtract(18, "year").format("YYYY-MM-DD")}
-            ref={register}
-          />
-          <span
-            className={`input-${
-              errors.date_of_birth && errors.date_of_birth.type === "pattern"
-                ? "error"
-                : "info"
-            }`}
-          >
-            You must be at least 18 years old to continue
-          </span>
-          {errors.date_of_birth && errors.date_of_birth.type === "required" && (
-            <span className="input-error">Password is required</span>
-          )}
-        </div>
+        {IS_BURN && (
+          <div className="input-group">
+            <input
+              name="date_of_birth"
+              className="input-block input-centered"
+              type="date"
+              max={dayjs().subtract(18, "year").format("YYYY-MM-DD")}
+              ref={register}
+            />
+            <span
+              className={`input-${
+                errors.date_of_birth && errors.date_of_birth.type === "pattern"
+                  ? "error"
+                  : "info"
+              }`}
+            >
+              You must be at least 18 years old to continue
+            </span>
+            {errors.date_of_birth &&
+              errors.date_of_birth.type === "required" && (
+                <span className="input-error">Date of birth is required</span>
+              )}
+          </div>
+        )}
         {IS_BURN && (
           <div className="input-group">
             <input
@@ -192,33 +195,34 @@ const RegisterForm: React.FunctionComponent<PropsType> = ({
             )}
           </div>
         )}
-        {CODE_OF_CONDUCT_QUESTIONS.map((q) => (
-          <div className="input-group" key={q.name}>
-            <label
-              htmlFor={q.name}
-              className={`checkbox ${watch(q.name) && "checkbox-checked"}`}
-            >
-              {q.link && (
-                <a href={q.link} target="_blank" rel="noopener noreferrer">
-                  {q.text}
-                </a>
+        {IS_BURN &&
+          CODE_OF_CONDUCT_QUESTIONS.map((q) => (
+            <div className="input-group" key={q.name}>
+              <label
+                htmlFor={q.name}
+                className={`checkbox ${watch(q.name) && "checkbox-checked"}`}
+              >
+                {q.link && (
+                  <a href={q.link} target="_blank" rel="noopener noreferrer">
+                    {q.text}
+                  </a>
+                )}
+                {!q.link && q.text}
+              </label>
+              <input
+                type="checkbox"
+                name={q.name}
+                id={q.name}
+                ref={register({
+                  required: true,
+                })}
+              />
+              {/* @ts-ignore @debt questions should be typed if possible */}
+              {q.name in errors && errors[q.name].type === "required" && (
+                <span className="input-error">Required</span>
               )}
-              {!q.link && q.text}
-            </label>
-            <input
-              type="checkbox"
-              name={q.name}
-              id={q.name}
-              ref={register({
-                required: true,
-              })}
-            />
-            {/* @ts-ignore @debt questions should be typed if possible */}
-            {q.name in errors && errors[q.name].type === "required" && (
-              <span className="input-error">Required</span>
-            )}
-          </div>
-        ))}
+            </div>
+          ))}
         <input
           className="btn btn-primary btn-block btn-centered"
           type="submit"
