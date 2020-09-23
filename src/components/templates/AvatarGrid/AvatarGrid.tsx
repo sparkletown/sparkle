5f88333f82f80ae731b5ba59a1c0d575e48b097a
Denis Dimitrov <dedimitrov@melontech.com>
2020-09-23 19:46:29 +0300

    add avatarlayer to avatargrid

--- src/components/templates/AvatarGrid/AvatarGrid.tsx
+++ src/components/templates/AvatarGrid/AvatarGrid.tsx
@@ -2,15 +2,25 @@ import UserProfilePicture from "components/molecules/UserProfilePicture";
 import firebase from "firebase/app";
 import { useSelector } from "hooks/useSelector";
 import { useUser } from "hooks/useUser";
-import React, { useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { WithId } from "utils/id";
 import { User } from "types/User";
 import { useParams } from "react-router-dom";
 import "./AvatarGrid.scss";
+import { unstable_batchedUpdates } from "react-dom";
 import UserProfileModal from "components/organisms/UserProfileModal";
 import { AvatarGridRoom } from "types/AvatarGrid";
 import { RoomModal } from "./RoomModal";
 import ChatDrawer from "components/organisms/ChatDrawer";
+import AvatarLayer from "../Playa/AvatarLayer";
+import { UserVideoState } from "types/RelayMessage";
+import { MenuConfig } from "../Playa/Playa";
 
 type Props = {
   venueName: string;
@@ -19,16 +29,50 @@ type Props = {
 const DEFAULT_COLUMNS = 40;
 const DEFAULT_ROWS = 25;
 
+const GATE_X = 1920;
+const GATE_Y = 1080;
+
 const AvatarGrid = ({ venueName }: Props) => {
+  const [selectedUserProfile, setSelectedUserProfile] = useState<
+    WithId<User>
+  >();
+  const [centerX, setCenterX] = useState(GATE_X);
+  const [centerY, setCenterY] = useState(GATE_Y);
+  const [myX, setMyX] = useState<number>();
+  const [myY, setMyY] = useState<number>();
+  const [centeredOnMe, setCenteredOnMe] = useState<boolean>();
+  const [meIsLocated, setMeIsLocated] = useState(false);
+  const [bikeMode, setBikeMode] = useState<boolean | undefined>(false);
+  const [videoState, setVideoState] = useState<string>();
+  const [away, setAway] = useState(false);
+  const [heartbeat, setHeartbeat] = useState<number>();
+
+  const toggleBikeMode = useCallback(() => {
+    setBikeMode(!bikeMode);
+  }, [bikeMode]);
+  const toggleVideoState = useCallback(() => {
+    setVideoState((prev) =>
+      prev === UserVideoState.Open ? UserVideoState.Locked : UserVideoState.Open
+    );
+  }, []);
+
+  const [movingUp, setMovingUp] = useState(false);
+  const [movingDown, setMovingDown] = useState(false);
+  const [movingLeft, setMovingLeft] = useState(false);
+  const [movingRight, setMovingRight] = useState(false);
+
   const { venueId } = useParams();
   const { user, profile } = useUser();
   const { venue, partygoers } = useSelector((state) => ({
     partygoers: state.firestore.ordered.partygoers,
     venue: state.firestore.data.currentVenue,
   }));
-  const [selectedUserProfile, setSelectedUserProfile] = useState<
-    WithId<User>
-  >();
+  const [showUserTooltip, setShowUserTooltip] = useState(false);
+  const [hoveredUser, setHoveredUser] = useState<User | null>();
+  const userRef = useRef<HTMLDivElement | null>(null);
+  const [showMenu, setShowMenu] = useState(false);
+  const [menu, setMenu] = useState<MenuConfig>();
+  const menuRef = useRef<HTMLDivElement | null>(null);
   const [isRoomModalOpen, setIsRoomModalOpen] = useState<boolean>(false);
   const [selectedRoom, setSelectedRoom] = useState<AvatarGridRoom | undefined>(
     undefined
@@ -50,6 +94,10 @@ const AvatarGrid = ({ venueName }: Props) => {
     }
     partygoersBySeat[row][column] = partygoer;
   });
+
+  useEffect(() => {
+    setCenteredOnMe(myX === centerX && myY === centerY);
+  }, [centerX, centerY, myX, myY]);
 
   const takeSeat = (row: number, column: number) => {
     if (!user || !profile) return;
@@ -73,6 +121,59 @@ const AvatarGrid = ({ venueName }: Props) => {
       });
   };
 
+  const setMyLocation = useMemo(
+    () => (x: number, y: number) => {
+      unstable_batchedUpdates(() => {
+        setCenterX(x);
+        setCenterY(y);
+        setMyX(x);
+        setMyY(y);
+        setMeIsLocated(true);
+      });
+    },
+    []
+  );
+
+  const avatarLayer = useMemo(
+    () => (
+      <AvatarLayer
+        bikeMode={bikeMode}
+        setBikeMode={setBikeMode}
+        videoState={videoState}
+        setVideoState={setVideoState}
+        toggleVideoState={toggleVideoState}
+        away={away}
+        setAway={setAway}
+        heartbeat={heartbeat}
+        setHeartbeat={setHeartbeat}
+        movingUp={movingUp}
+        movingDown={movingDown}
+        movingLeft={movingLeft}
+        movingRight={movingRight}
+        setMyLocation={setMyLocation}
+        setSelectedUserProfile={setSelectedUserProfile}
+        setShowUserTooltip={setShowUserTooltip}
+        setHoveredUser={setHoveredUser}
+        setShowMenu={setShowMenu}
+        setMenu={setMenu}
+        userRef={userRef}
+        menuRef={menuRef}
+      />
+    ),
+    [
+      bikeMode,
+      videoState,
+      away,
+      heartbeat,
+      movingUp,
+      movingDown,
+      movingLeft,
+      movingRight,
+      setMyLocation,
+      toggleVideoState,
+    ]
+  );
+
   if (!venue) {
     return null;
   }
@@ -86,6 +187,71 @@ const AvatarGrid = ({ venueName }: Props) => {
         className="avatar-grid-container"
         style={{ backgroundImage: `url(${venue?.mapBackgroundImageUrl})` }}
       >
+        {avatarLayer}
+        {meIsLocated && (
+          <div className="avatar-controls">
+            <div
+              className="up"
+              onMouseDown={(event) => {
+                setMovingUp(true);
+                event.preventDefault();
+              }}
+              onTouchStart={(event) => {
+                setMovingUp(true);
+                event.preventDefault();
+              }}
+              onMouseUp={() => setMovingUp(false)}
+              onTouchEnd={() => setMovingUp(false)}
+            >
+              <div className="btn" />
+            </div>
+            <div
+              className="down"
+              onMouseDown={(event) => {
+                setMovingDown(true);
+                event.preventDefault();
+              }}
+              onTouchStart={(event) => {
+                setMovingDown(true);
+                event.preventDefault();
+              }}
+              onMouseUp={() => setMovingDown(false)}
+              onTouchEnd={() => setMovingDown(false)}
+            >
+              <div className="btn" />
+            </div>
+            <div
+              className="left"
+              onMouseDown={(event) => {
+                setMovingLeft(true);
+                event.preventDefault();
+              }}
+              onTouchStart={(event) => {
+                setMovingLeft(true);
+                event.preventDefault();
+              }}
+              onMouseUp={() => setMovingLeft(false)}
+              onTouchEnd={() => setMovingLeft(false)}
+            >
+              <div className="btn" />
+            </div>
+            <div
+              className="right"
+              onMouseDown={(event) => {
+                setMovingRight(true);
+                event.preventDefault();
+              }}
+              onTouchStart={(event) => {
+                setMovingRight(true);
+                event.preventDefault();
+              }}
+              onMouseUp={() => setMovingRight(false)}
+              onTouchEnd={() => setMovingRight(false)}
+            >
+              <div className="btn" />
+            </div>
+          </div>
+        )}
         {venue.spaces?.map((room, index) => {
           return (
             <>
