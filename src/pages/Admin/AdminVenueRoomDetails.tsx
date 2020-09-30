eb6461c61a35cf4ad1a2a227cfeb7bf242720ab8
Aivonix <aivonix@gmail.com>
2020-09-30 15:35:43 +0300

    removing one unnecessesary func

--- src/pages/Admin/AdminVenueRoomDetails.tsx
+++ src/pages/Admin/AdminVenueRoomDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { CampRoomData } from "types/CampRoomData";
 import { Link, useHistory } from "react-router-dom";
 import { Venue } from "types/Venue";
