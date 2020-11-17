75cfcefb6902fcee4d769be1b8c01ea8eed7bc7c
Glenn 'devalias' Grant <glenn@devalias.net>
2020-11-17 14:37:10 +1100

    refactor Firestore types to allow easier use + add firestore selectors + selector makers

--- src/types/Firestore.ts
+++ src/types/Firestore.ts
@@ -37,52 +37,58 @@ interface UserVisit {
 }
 
 export interface Firestore {
-  status: {
-    requesting: VenueStatus;
-    requested: VenueStatus;
-    timestamps: VenueTimestamps;
-  };
-  data: {
-    currentVenue?: AnyVenue;
-    parentVenue?: AnyVenue;
-    currentEvent: Record<string, VenueEvent>;
-    venueChats: Record<string, RestrictedChatMessage> | null;
-    venueEvents: Record<string, VenueEvent>;
-    userPurchaseHistory: Record<string, Purchase>;
-    partygoers: Record<string, User>;
-    users: Record<string, User>;
-    privatechats: Record<string, PrivateChatMessage>;
-    experiences: Record<string, Experience>;
-    eventPurchase: Record<string, Purchase>;
-    reactions: Record<string, Reaction>;
-    venues?: Record<string, AnyVenue>;
-    events?: Record<string, VenueEvent>;
-    playaVenues?: Record<string, AnyVenue>; // for the admin playa preview
-    allUsers?: Record<string, User>;
-    userModalVisits?: Record<string, UserVisit>;
-    userRoles: Record<string, Role>;
-    allowAllRoles: Record<string, Role>;
-    adminRole: AdminRole;
-  };
-  ordered: {
-    currentVenue: Array<WithId<AnyVenue>>;
-    currentEvent: Array<WithId<VenueEvent>>;
-    venueChats: Array<WithId<RestrictedChatMessage>>;
-    venueEvents: Array<WithId<VenueEvent>>;
-    userPurchaseHistory: Array<WithId<Purchase>>;
-    partygoers: Array<WithId<User>>;
-    users: Array<WithId<User>>;
-    privatechats: Array<WithId<PrivateChatMessage>>;
-    experiences: Array<WithId<Experience>>;
-    eventPurchase: Array<WithId<Purchase>>;
-    reactions: Array<WithId<Reaction>>;
-    venues?: Array<WithId<AnyVenue>>;
-    events?: Array<WithId<VenueEvent>>;
-    playaVenues?: Array<WithId<AnyVenue>>;
-    statsOnlineUsers?: Array<WithId<User>>;
-    statsOpenVenues?: Array<WithId<AnyVenue>>;
-    allUsers?: Array<WithId<User>>;
-    userModalVisits?: Array<WithId<UserVisit>>;
-    chatRequests?: Array<WithId<ChatRequest>>;
-  };
+  status: FirestoreStatus;
+  data: FirestoreData;
+  ordered: FirestoreOrdered;
+}
+
+export interface FirestoreStatus {
+  requesting: VenueStatus;
+  requested: VenueStatus;
+  timestamps: VenueTimestamps;
+}
+
+export interface FirestoreData {
+  currentVenue?: AnyVenue;
+  parentVenue?: AnyVenue;
+  currentEvent: Record<string, VenueEvent>;
+  venueChats: Record<string, RestrictedChatMessage> | null;
+  venueEvents: Record<string, VenueEvent>;
+  userPurchaseHistory: Record<string, Purchase>;
+  partygoers: Record<string, User>;
+  users: Record<string, User>;
+  privatechats: Record<string, PrivateChatMessage>;
+  experiences: Record<string, Experience>;
+  eventPurchase: Record<string, Purchase>;
+  reactions: Record<string, Reaction>;
+  venues?: Record<string, AnyVenue>;
+  events?: Record<string, VenueEvent>;
+  playaVenues?: Record<string, AnyVenue>; // for the admin playa preview
+  allUsers?: Record<string, User>;
+  userModalVisits?: Record<string, UserVisit>;
+  userRoles: Record<string, Role>;
+  allowAllRoles: Record<string, Role>;
+  adminRole: AdminRole;
+}
+
+export interface FirestoreOrdered {
+  currentVenue: Array<WithId<AnyVenue>>;
+  currentEvent: Array<WithId<VenueEvent>>;
+  venueChats: Array<WithId<RestrictedChatMessage>>;
+  venueEvents: Array<WithId<VenueEvent>>;
+  userPurchaseHistory: Array<WithId<Purchase>>;
+  partygoers: Array<WithId<User>>;
+  users: Array<WithId<User>>;
+  privatechats: Array<WithId<PrivateChatMessage>>;
+  experiences: Array<WithId<Experience>>;
+  eventPurchase: Array<WithId<Purchase>>;
+  reactions: Array<WithId<Reaction>>;
+  venues?: Array<WithId<AnyVenue>>;
+  events?: Array<WithId<VenueEvent>>;
+  playaVenues?: Array<WithId<AnyVenue>>;
+  statsOnlineUsers?: Array<WithId<User>>;
+  statsOpenVenues?: Array<WithId<AnyVenue>>;
+  allUsers?: Array<WithId<User>>;
+  userModalVisits?: Array<WithId<UserVisit>>;
+  chatRequests?: Array<WithId<ChatRequest>>;
 }
