c754088658de9791e766cbaeb1dc54491400bc2a
Chris Adams <chris@cadams.com.au>
2020-11-19 17:53:09 +1100

    Address review comments

--- src/pages/Admin/Venue/DetailsForm.tsx
+++ src/pages/Admin/Venue/DetailsForm.tsx
@@ -81,7 +81,7 @@ export const DetailsForm: React.FC<DetailsFormProps> = ({
       },
       defaultValues: {
         ...defaultValues,
-        parentId: parentIdQuery ?? "",
+        parentId: parentIdQuery ?? defaultValues?.parentId ?? "",
       },
     }
   );
