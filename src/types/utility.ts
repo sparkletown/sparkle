0ce15c4eca82a05a57472d5a336456c9350ad01f
Glenn 'devalias' Grant <glenn@devalias.net>
2020-11-20 18:54:26 +1100

    add Point, Bounds types to type/utility

--- src/types/utility.ts
+++ src/types/utility.ts
@@ -2,10 +2,17 @@ export type ExtractProps<T> = T extends React.FunctionComponent<infer U>
   ? U
   : never;
 
+export type Point = {
+  x: number;
+  y: number;
+};
+
 export type Dimensions = {
   width: number;
   height: number;
 };
+
+export type Bounds = Point & Dimensions;
 
 export type Matrix<T> = T[][];
 
