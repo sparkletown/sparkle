4c105fb0b222b4167af74c272dcb8867ffcce902
Glenn 'devalias' Grant <glenn@devalias.net>
2020-11-20 23:31:33 +1100

    WIP: super hacky commit, but should have all the required bits

--- src/components/templates/FireBarrel/FireSeat.tsx
+++ src/components/templates/FireBarrel/FireSeat.tsx
@@ -1,53 +1,43 @@
-import React, { useState } from "react";
-
-import { useSelector } from "hooks/useSelector";
-
-import VideoErrorModal from "components/organisms/Room/VideoErrorModal";
-import LocalParticipant from "components/organisms/Room/LocalParticipant";
+import React from "react";
+import { LocalParticipant, RemoteParticipant } from "twilio-video";
 
 import * as S from "./FireBarrel.styled";
-import { BarrelPeple } from "./FireBarrel";
-import { useUser } from "hooks/useUser";
-import { useVideoState } from "./useVideo";
+// import { BarrelPeple } from "./FireBarrel";
 
 interface FireSeatProps {
-  person: BarrelPeple;
-  chairNumber: number;
-  roomName?: string;
+  participant: LocalParticipant | RemoteParticipant;
+  // person: BarrelPeple;
+  // chairNumber: number;
+  // roomName?: string;
 }
 
-const FireSeat: React.FC<FireSeatProps> = ({
-  person,
-  chairNumber,
-  roomName,
-}) => {
-  const { user } = useUser();
-  const { room } = useVideoState({ userUid: user?.uid, roomName });
-
-  const [videoError, setVideoError] = useState<string>("");
-
-  const users = useSelector((state) => state.firestore.data.partygoers);
-  const profileData = room ? users[room.localParticipant.identity] : undefined;
-
+// TODO: potentially don't even need this anymore, can delete maybe if you want? I inlined the only relevant looking bits from here
+const FireSeat: React.FC<FireSeatProps> = (
+  {
+    // person,
+    // chairNumber,
+    // roomName,
+  }
+) => {
   return (
     <S.Chair isEmpty={false}>
-      {user && room && profileData && (
-        <LocalParticipant
-          participant={room?.localParticipant}
-          profileData={profileData}
-          profileDataId={room?.localParticipant.identity}
-          showIcon={false}
-        />
-      )}
-      <VideoErrorModal
-        show={!!videoError}
-        onHide={() => setVideoError("")}
-        errorMessage={videoError}
-        // onRetry={connectToVideoRoom}
-        onRetry={() => {}}
-        onBack={() => {}}
-        // onBack={() => (setSeatedAtTable ? leaveSeat() : setVideoError(""))}
-      />
+      {
+        // <LocalParticipant
+        //   participant={localParticipant}
+        //   profileData={profileData}
+        //   profileDataId={room?.localParticipant.identity}
+        //   showIcon={false}
+        // />
+      }
+      {/*<VideoErrorModal*/}
+      {/*  show={!!videoError}*/}
+      {/*  onHide={() => setVideoError("")}*/}
+      {/*  errorMessage={videoError}*/}
+      {/*  // onRetry={connectToVideoRoom}*/}
+      {/*  onRetry={() => {}}*/}
+      {/*  onBack={() => {}}*/}
+      {/*  // onBack={() => (setSeatedAtTable ? leaveSeat() : setVideoError(""))}*/}
+      {/*/>*/}
     </S.Chair>
   );
 };
