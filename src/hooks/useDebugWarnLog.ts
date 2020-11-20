cc1ee23207789df221851a474c9ac1062de659f2
Glenn 'devalias' Grant <glenn@devalias.net>
2020-11-20 18:49:57 +1100

    refactor useDebugWarnLog() hook to display context alongside messages + group log lines for readability

--- src/hooks/useDebugWarnLog.ts
+++ src/hooks/useDebugWarnLog.ts
@@ -7,26 +7,43 @@ import { DependencyList, useEffect, useRef } from "react";
  *
  * @param deps the array of dependencies that will be passed directly to useEffect
  * @param label to help you differentiate between different instances in the JS console
+ * @param context additional context to be displayed alongside the log message
  *
  * @example
  *   const somethingThatMayCauseReRenders = () => {}
+ *   const usefulContext = 42
  *
- *   useDebugWarnLog([somethingThatMayCauseReRenders], "FooComponent::barLabel")
- *   // [useDebugWarnLog::FooComponent::barLabel] deps changed, will cause re-render (count: 1)
+ *   useDebugWarnLog([somethingThatMayCauseReRenders], "FooComponent::barLabel", { usefulContext })
+ *   // [useDebugWarnLog::FooComponent::barLabel] deps changed, will cause re-render (count: 1), { usefulContext }
  */
-export const useDebugWarnLog = (deps: DependencyList, label?: String) => {
+export const useDebugWarnLog = (
+  deps: DependencyList,
+  label?: String,
+  context?: object
+) => {
   const countRef = useRef(0);
 
   useEffect(() => {
     countRef.current += 1;
 
     const prefix = label ? `[useDebugWarnLog::${label}]` : "[useDebugWarnLog]";
-    console.warn(
-      prefix,
-      "deps changed, will cause re-render",
-      `(count: ${countRef.current})`
+    displayWarning(
+      `${prefix} (count: ${countRef.current})`,
+      `deps changed, will cause re-render`,
+      context
     );
     // We are consciously using a dynamic props pattern here as it is a helper
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [...deps, label]);
+};
+
+const warningStyles = "color: var(--warning, #F39C12)";
+
+const displayWarning = (title: string, msg: string, context?: object) => {
+  console.groupCollapsed(`%c${title}`, warningStyles);
+  console.log(msg, context);
+  console.groupCollapsed("click to show stacktrace");
+  console.warn("stacktrace");
+  console.groupEnd();
+  console.groupEnd();
 };
