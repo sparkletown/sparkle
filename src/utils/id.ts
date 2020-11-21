db093d3b6305f082ccdea171c00149c6731a5756
Glenn 'devalias' Grant <glenn@devalias.net>
2020-11-21 22:25:29 +1100

    add generic withId() helper

--- src/utils/id.ts
+++ src/utils/id.ts
@@ -2,6 +2,11 @@ import { VenueEvent } from "types/VenueEvent";
 
 export type WithId<T extends object> = T & { id: string };
 
+export const withId = <T extends object>(obj: T, id: string): WithId<T> => ({
+  ...obj,
+  id,
+});
+
 export type WithVenueId<T extends object> = T & { venueId: string };
 
 export const withVenueId = (
