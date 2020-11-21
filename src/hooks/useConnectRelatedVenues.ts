56e7b7e779ccc7533efae6529a1c0cd31dee4b5a
Glenn 'devalias' Grant <glenn@devalias.net>
2020-11-21 22:43:31 +1100

    refactor useConnectRelatedVenues() hook to optionally allow selecting related venue events as well + returns them

--- src/hooks/useConnectRelatedVenues.ts
+++ src/hooks/useConnectRelatedVenues.ts
@@ -1,61 +1,256 @@
+import { useCallback, useMemo } from "react";
+
+import { RootState } from "index";
+
+import { AnyVenue } from "types/Firestore";
+import { SparkleSelector } from "types/SparkleSelector";
+import { ReactHook } from "types/utility";
+import { Venue } from "types/Venue";
+import { VenueEvent } from "types/VenueEvent";
+
+import { isTruthyFilter } from "utils/filter";
+import { WithId, withVenueId, WithVenueId } from "utils/id";
 import {
-  ReduxFirestoreQuerySetting,
-  useFirestoreConnect,
-} from "react-redux-firebase";
-import { siblingVenuesSelector, subvenuesSelector } from "utils/selectors";
+  emptyArraySelector,
+  makeSubvenueEventsSelector,
+  maybeArraySelector,
+  parentVenueEventsSelector,
+  parentVenueOrderedSelector,
+  siblingVenuesSelector,
+  subvenuesSelector,
+  venueEventsSelector,
+} from "utils/selectors";
+
 import { useConnectCurrentVenueNG } from "./useConnectCurrentVenueNG";
 import { useSelector } from "./useSelector";
-import { SparkleRFQConfig } from "./useSparkleFirestoreConnect";
+import {
+  SparkleRFQConfig,
+  useSparkleFirestoreConnect,
+} from "./useSparkleFirestoreConnect";
 
-export const useConnectRelatedVenues = (venueId?: string) => {
+const toEventsWithVenueIds = (venueId: string) => (event: VenueEvent) =>
+  withVenueId(event, venueId);
+
+const venueEventsSelectorToEventsWithVenueIds = (
+  venues: WithId<AnyVenue>[]
+) => (state: RootState) =>
+  venues.flatMap(
+    (venue) =>
+      makeSubvenueEventsSelector(venue.id)(state)?.map(
+        toEventsWithVenueIds(venue.id)
+      ) ?? []
+  );
+
+interface UseConnectRelatedVenuesProps {
+  venueId?: string;
+
+  /** @default false **/
+  withEvents?: boolean;
+}
+
+interface UseConnectRelatedVenuesReturn {
+  currentVenue?: WithId<AnyVenue>;
+  relatedVenues: WithId<AnyVenue>[];
+
+  relatedVenueEvents: WithVenueId<VenueEvent>[];
+  parentVenueEvents: WithVenueId<VenueEvent>[];
+  venueEvents: WithVenueId<VenueEvent>[];
+  siblingVenueEvents: WithVenueId<VenueEvent>[];
+  subvenueEvents: WithVenueId<VenueEvent>[];
+}
+
+export const useConnectRelatedVenues: ReactHook<
+  UseConnectRelatedVenuesProps,
+  UseConnectRelatedVenuesReturn
+> = ({ venueId, withEvents = false }) => {
   const { currentVenue } = useConnectCurrentVenueNG(venueId);
 
-  const subvenuesQuery: SparkleRFQConfig = {
-    collection: "venues",
-    where: [["parentId", "==", venueId]],
-    storeAs: "subvenues",
-  };
-  const subvenues = useSelector(subvenuesSelector);
-  const subvenueEventsQueries: ReduxFirestoreQuerySetting[] =
-    subvenues?.map((subvenue) => ({
+  const parentId: string | undefined = currentVenue?.parentId;
+
+  const parentEventsWithVenueIdsSelector: SparkleSelector<
+    WithVenueId<VenueEvent>[]
+  > = useMemo(() => {
+    if (!withEvents || !parentId)
+      return emptyArraySelector as SparkleSelector<WithVenueId<VenueEvent>[]>;
+
+    return (state: RootState) =>
+      parentVenueEventsSelector(state)?.map(toEventsWithVenueIds(parentId)) ??
+      [];
+  }, [parentId, withEvents]);
+
+  const venueEventsWithVenueIdsSelector: SparkleSelector<
+    WithVenueId<VenueEvent>[]
+  > = useMemo(() => {
+    if (!withEvents || !venueId)
+      return emptyArraySelector as SparkleSelector<WithVenueId<VenueEvent>[]>;
+
+    return (state: RootState) =>
+      venueEventsSelector(state)?.map(toEventsWithVenueIds(venueId)) ?? [];
+  }, [venueId, withEvents]);
+
+  const siblingNotVenueSelector: SparkleSelector<
+    WithId<Venue>[]
+  > = useCallback(
+    (state) =>
+      siblingVenuesSelector(state)?.filter(
+        (sibling) => sibling.id !== venueId
+      ) ?? [],
+    [venueId]
+  );
+
+  const parentVenue = useSelector(parentVenueOrderedSelector);
+  const subvenues = useSelector(subvenuesSelector) ?? [];
+  const siblingVenues = useSelector(siblingNotVenueSelector) ?? [];
+
+  const maybeParentEventsSelector = useCallback(
+    maybeArraySelector(withEvents, parentEventsWithVenueIdsSelector),
+    [withEvents]
+  );
+
+  const maybeVenueEventsSelector = useCallback(
+    maybeArraySelector(withEvents, venueEventsWithVenueIdsSelector),
+    [withEvents]
+  );
+
+  const maybeSiblingEventsSelector = useCallback(
+    maybeArraySelector(
+      withEvents,
+      venueEventsSelectorToEventsWithVenueIds(siblingVenues)
+    ),
+    [withEvents]
+  );
+
+  const maybeSubvenueEventsSelector = useCallback(
+    maybeArraySelector(
+      withEvents,
+      venueEventsSelectorToEventsWithVenueIds(subvenues)
+    ),
+    []
+  );
+
+  const parentVenueEvents: WithVenueId<VenueEvent>[] = useSelector(
+    maybeParentEventsSelector
+  );
+
+  const venueEvents: WithVenueId<VenueEvent>[] = useSelector(
+    maybeVenueEventsSelector
+  );
+
+  const siblingVenueEvents: WithVenueId<VenueEvent>[] = useSelector(
+    maybeSiblingEventsSelector
+  );
+
+  const subvenueEvents: WithVenueId<VenueEvent>[] = useSelector(
+    maybeSubvenueEventsSelector
+  );
+
+  /////////////////////////////////
+  // Firestore Connect Configs/etc
+  /////////////////////////////////
+
+  // Sibling
+  const siblingVenuesQuery: SparkleRFQConfig | undefined = !!parentId
+    ? {
+        collection: "venues",
+        where: [["parentId", "==", parentId]],
+        storeAs: "siblingVenues",
+      }
+    : undefined;
+
+  // Sub
+  const subvenuesQuery: SparkleRFQConfig | undefined = !!venueId
+    ? {
+        collection: "venues",
+        where: [["parentId", "==", venueId]],
+        storeAs: "subvenues",
+      }
+    : undefined;
+
+  const makeEventsQueryConfig = (
+    doc: string,
+    storeAs: string
+  ): SparkleRFQConfig =>
+    ({
       collection: "venues",
-      doc: subvenue.id,
+      doc,
       subcollections: [{ collection: "events" }],
       orderBy: ["start_utc_seconds", "asc"],
-      storeAs: `subvenueEvents-${subvenue.id}`,
-    })) ?? [];
-  useFirestoreConnect([subvenuesQuery, ...subvenueEventsQueries]);
+      storeAs,
+    } as SparkleRFQConfig); // @debt a little hacky, but we're consciously subverting our helper protections;
 
-  const parentVenueEventsQuery: SparkleRFQConfig = {
-    collection: "venues",
-    doc: currentVenue?.parentId,
-    subcollections: [{ collection: "events" }],
-    orderBy: ["start_utc_seconds", "asc"],
-    storeAs: "parentVenueEvents",
-  };
+  // Parent Events
+  const parentVenueEventsQuery: SparkleRFQConfig | undefined =
+    parentId && withEvents
+      ? makeEventsQueryConfig(parentId, "parentVenueEvents")
+      : undefined;
 
-  const siblingVenuesQuery: SparkleRFQConfig = {
-    collection: "venues",
-    where: [["parentId", "==", currentVenue?.parentId]],
-    storeAs: "siblingVenues",
-  };
-  const siblingVenues = useSelector(siblingVenuesSelector);
-  const siblingVenueEventsQueries: ReduxFirestoreQuerySetting[] =
-    siblingVenues
-      ?.filter((v) => v.id !== venueId)
-      .map((siblingVenue) => ({
-        collection: "venues",
-        doc: siblingVenue.id,
-        storeAs: `siblingVenueEvents-${siblingVenue.id}`,
-      })) ?? [];
+  // Sibling Events
+  const siblingVenueEventsQueries: SparkleRFQConfig[] = withEvents
+    ? siblingVenues.map((sibling) =>
+        makeEventsQueryConfig(sibling.id, `siblingVenueEvents-${sibling.id}`)
+      )
+    : [];
 
-  useFirestoreConnect(
-    currentVenue?.parentId
-      ? [
-          parentVenueEventsQuery,
-          siblingVenuesQuery,
-          ...siblingVenueEventsQueries,
-        ]
-      : []
+  // Sub Events
+  const subvenueEventsQueries: SparkleRFQConfig[] = withEvents
+    ? subvenues.map((subvenue) =>
+        makeEventsQueryConfig(subvenue.id, `subvenueEvents-${subvenue.id}`)
+      )
+    : [];
+
+  // Combine / filter for valid queries
+  const allValidQueries: SparkleRFQConfig[] = [
+    siblingVenuesQuery,
+    subvenuesQuery,
+    parentVenueEventsQuery,
+    ...siblingVenueEventsQueries,
+    ...subvenueEventsQueries,
+  ].filter(isTruthyFilter);
+
+  // Connect
+  useSparkleFirestoreConnect(allValidQueries);
+
+  /////////////////////////////////
+  // Return
+  /////////////////////////////////
+
+  const relatedVenueEvents: WithVenueId<VenueEvent>[] = useMemo(
+    () =>
+      [
+        ...parentVenueEvents,
+        ...venueEvents,
+        ...siblingVenueEvents,
+        ...subvenueEvents,
+      ].sort((a, b) => a.start_utc_seconds - b.start_utc_seconds),
+    [parentVenueEvents, venueEvents, siblingVenueEvents, subvenueEvents]
+  );
+
+  const relatedVenues = useMemo(
+    () =>
+      [currentVenue, parentVenue, ...siblingVenues, ...subvenues].filter(
+        isTruthyFilter
+      ),
+    [currentVenue, parentVenue, siblingVenues, subvenues]
+  );
+
+  return useMemo(
+    () => ({
+      currentVenue,
+      relatedVenues,
+      relatedVenueEvents,
+      parentVenueEvents,
+      venueEvents,
+      siblingVenueEvents,
+      subvenueEvents,
+    }),
+    [
+      currentVenue,
+      relatedVenues,
+      relatedVenueEvents,
+      parentVenueEvents,
+      venueEvents,
+      siblingVenueEvents,
+      subvenueEvents,
+    ]
   );
 };
