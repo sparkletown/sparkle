6c16f891faf3e3eb398043ae4a030e3be79ae2f7
Glenn 'devalias' Grant <glenn@devalias.net>
2020-11-26 00:20:42 +1100

    add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+// import type { Config } from "@jest/types";
+
+// @debt make this work as a .ts
+
+/**
+ * Promise style Jest config (in case we need to use other promises here in future)
+ *
+ * @see https://jestjs.io/docs/en/configuration
+ */
+// export default async (): Promise<Config.InitialOptions> => {
+module.exports = async () => {
+  return {
+    rootDir: "<rootDir>",
+    verbose: true,
+    maxWorkers: "50%",
+
+    testMatch: [
+      "{.,src}/**/__tests__/**/*.+(ts|tsx|js)",
+      "{.,src}/**/?(*.)+(spec|test).+(ts|tsx|js)",
+    ],
+
+    // @debt make these work to actually ignore the paths we want
+    // testPathIgnorePatterns: ["/node_modules/", "<rootDir>/cypress/.*"],
+    // transformIgnorePatterns: ["/node_modules/", "<rootDir>/cypress/.*"],
+
+    transform: {
+      "^.+\\.(ts|tsx)$": "ts-jest",
+    },
+  };
+};
