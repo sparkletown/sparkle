ad84c1e18c4fe3365dbbcfbb08b934df3a7789a9
Glenn 'devalias' Grant <glenn@devalias.net>
2020-11-30 14:33:16 +1100

    cleanup PartyMapAdmin + useCallback()'s

--- src/components/templates/PartyMap/PartyMapAdmin.tsx
+++ src/components/templates/PartyMap/PartyMapAdmin.tsx
@@ -6,38 +6,59 @@ import firebase from "firebase/app";
 import { PartyMapVenue } from "types/PartyMapVenue";
 
 import { useSelector } from "hooks/useSelector";
+
+// TODO: we should move this to a common helper location
+const makeUpdateBanner = (
+  venueId: string,
+  onError: (errorMsg: string) => void
+) => (message?: string) => {
+  const params = {
+    venueId,
+    bannerMessage: message ? message : "",
+  };
+
+  firebase
+    .functions()
+    .httpsCallable("venue-adminUpdateBannerMessage")(params)
+    .catch((e) => onError(e.toString()));
+};
+
+// TODO: can this ever be undefined?
 const partyMapVenueSelector = (state: RootState) =>
   state.firestore.ordered.currentVenue?.[0] as PartyMapVenue;
 
-export const PartyMapAdmin: FC = () => {
-  const [bannerMessage, setBannerMessage] = useState("");
-  const [error, setError] = useState<string | null>();
-
+export const PartyMapAdmin: React.FC = () => {
   const currentVenue = useSelector(partyMapVenueSelector);
+  const venueId = currentVenue.id;
 
+  const [error, setError] = useState<string | null>();
+  const [bannerMessage, setBannerMessage] = useState(
+    currentVenue?.bannerMessage
+  );
   useEffect(() => {
     setBannerMessage(currentVenue?.bannerMessage || "");
-  }, [currentVenue]);
+  }, [currentVenue?.bannerMessage]);
 
-  const updateBanner = useCallback(
-    (message: string | null) => {
-      const params = {
-        venueId: currentVenue?.id,
-        bannerMessage: message ? message : "",
-      };
-      firebase
-        .functions()
-        .httpsCallable("venue-adminUpdateBannerMessage")(params)
-        .catch((e) => setError(e.toString()));
+  const handleInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setError(null);
+      setBannerMessage(e.target.value);
     },
-    [currentVenue]
+    []
   );
 
-  const setBanner = useCallback(() => {
-    updateBanner(bannerMessage);
-  }, [bannerMessage, updateBanner]);
+  const updateBannerInFirestore = useCallback(
+    makeUpdateBanner(venueId, (errorMsg) => setError(errorMsg)),
+    []
+  );
 
-  const removeBanner = useCallback(() => updateBanner(""), [updateBanner]);
+  const saveBanner = useCallback(() => {
+    updateBannerInFirestore(bannerMessage);
+  }, [updateBannerInFirestore, bannerMessage]);
+
+  const clearBanner = useCallback(() => updateBannerInFirestore(""), [
+    updateBannerInFirestore,
+  ]);
 
   return (
     <>
@@ -49,17 +70,14 @@ export const PartyMapAdmin: FC = () => {
         <input
           type="text"
           value={bannerMessage}
-          onChange={(e) => {
-            setError(null);
-            setBannerMessage(e.target.value);
-          }}
+          onChange={handleInputChange}
           placeholder="Banner message"
         />
         {error && <span className="error">{error}</span>}
-        <button className="btn btn-primary" type="submit" onClick={setBanner}>
+        <button className="btn btn-primary" type="submit" onClick={saveBanner}>
           Set Banner
         </button>
-        <button className="btn btn-danger" type="submit" onClick={removeBanner}>
+        <button className="btn btn-danger" type="submit" onClick={clearBanner}>
           Remove
         </button>
       </div>
