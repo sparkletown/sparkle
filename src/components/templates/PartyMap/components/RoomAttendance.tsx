881676de95fe02cb743898b3bff51818292514fa
Glenn 'devalias' Grant <glenn@devalias.net>
2020-12-02 17:40:52 +1100

    cleanup RoomAttendance component + useMemo

--- src/components/templates/PartyMap/components/RoomAttendance.tsx
+++ src/components/templates/PartyMap/components/RoomAttendance.tsx
@@ -1,70 +1,62 @@
-import React, { FC } from "react";
+import React from "react";
 
 import { PartyMapRoomData } from "types/PartyMapRoomData";
 import { PartyMapVenue } from "types/PartyMapVenue";
 
 import { partygoersSelector } from "utils/selectors";
+import { isTruthy } from "utils/types";
 
 import { useSelector } from "hooks/useSelector";
 
 import "./RoomAttendance.scss";
 
-interface PropsType {
+interface RoomAttendanceProps {
   venue: PartyMapVenue;
   room: PartyMapRoomData;
 }
 
 const MAX_AVATARS_VISIBLE = 2;
 
-export const RoomAttendance: FC<PropsType> = ({ venue, room }) => {
-  const partygoers = useSelector(partygoersSelector);
+export const RoomAttendance: React.FC<RoomAttendanceProps> = ({
+  venue,
+  room,
+}) => {
+  const partygoers = useSelector(partygoersSelector) ?? [];
 
-  const usersInRoom =
-    partygoers?.filter(
-      (partygoer) => partygoer.lastSeenIn[`${venue.name}/${room.title}`]
-    ) ?? [];
+  const usersInRoom = partygoers.filter(
+    (partygoer) => partygoer.lastSeenIn[`${venue.name}/${room.title}`]
+  );
 
   const numberOfUsersInRoom = usersInRoom?.length;
+  const numberOfExtraUsersInRoom = Math.max(
+    numberOfUsersInRoom - MAX_AVATARS_VISIBLE,
+    0
+  );
+  const hasExtraUsersInRoom = isTruthy(numberOfExtraUsersInRoom);
 
+  const userAvatars = usersInRoom
+    .slice(0, MAX_AVATARS_VISIBLE)
+    .map((user, index) => (
+      <div key={`user-avatar-${user.id ?? index}`}>
+        <div
+          className="attendance-avatar"
+          style={{ backgroundImage: `url(${user.pictureUrl})` }}
+        />
+      </div>
+    ));
+
+  // TODO: do we want to keep this? What happens if we just render the empty wrapper div?
   if (numberOfUsersInRoom < 1) return <></>;
 
   return (
     <div className="attendance-avatars">
-      {/* TODO: extract this + useMemo */}
-      {usersInRoom.map((user, index) => {
-        return (
-          <div key={`user-avatar-${index}`}>
-            {index < MAX_AVATARS_VISIBLE && (
-              <div
-                className="attendance-avatar"
-                style={{ backgroundImage: `url(${user.pictureUrl})` }}
-              />
-            )}
-            {/* TODO: do we need this empty div here? If so, why? */}
-            <div />
-          </div>
-        );
-      })}
+      {userAvatars}
 
-      {numberOfUsersInRoom > MAX_AVATARS_VISIBLE && (
-        <div className="avatars-inside">
-          +{numberOfUsersInRoom - MAX_AVATARS_VISIBLE}
-        </div>
+      {hasExtraUsersInRoom && (
+        <div className="avatars-inside">+{numberOfExtraUsersInRoom}</div>
       )}
     </div>
   );
 };
-
-// TODO: this was how CampAttendance looked before we deleted it
-// const CampAttendance: FC<PropsType> = ({ attendees }) => {
-//   return attendees > 0 ? (
-//       <div className="camp-venue-people-container">
-//         <div className="camp-venue-people">{attendees}</div>
-//         <FontAwesomeIcon icon={faUser} />
-//       </div>
-//   ) : (
-//       <></>
-//   );
-// };
 
 export default RoomAttendance;
