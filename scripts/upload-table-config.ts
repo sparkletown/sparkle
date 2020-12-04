16e8c3a28777dd7f8f5c93df41ecfca04c765843
Chris Adams <chris@cadams.com.au>
2020-12-05 07:29:39 +1100

    Tweak table upload script

--- scripts/upload-table-config.ts
+++ scripts/upload-table-config.ts
@@ -1,148 +1,25 @@
 import admin from "firebase-admin";
 import serviceAccount from "./prodAccountKey.json";
 import "firebase/firestore";
+import { Table } from "../src/types/Table";
 
-const TABLES = [
-  {
-    capacity: 14,
-    title: "Table 1",
-    reference: "jazzbar-table-1",
+const generateTables: (props: {
+  num: number;
+  capacity: number;
+  startFrom?: number;
+  cols?: number;
+}) => Table[] = ({ num, capacity, startFrom, cols }) =>
+  Array.from(Array(num)).map((_, idx) => ({
+    capacity,
+    title: `Table ${idx + (startFrom ?? 0) + 1}`,
+    reference: `Table ${idx + (startFrom ?? 0) + 1}`,
     rows: 2,
-    columns: 7,
-  },
-  {
-    capacity: 14,
-    title: "Table 2",
-    reference: "jazzbar-table-2",
-    rows: 2,
-    columns: 7,
-  },
-  {
-    capacity: 14,
-    title: "Table 3",
-    reference: "jazzbar-table-3",
-    rows: 2,
-    columns: 7,
-  },
-  {
-    capacity: 14,
-    title: "Table 4",
-    reference: "jazzbar-table-4",
-    rows: 2,
-    columns: 7,
-  },
-  {
-    capacity: 14,
-    title: "Table 5",
-    reference: "jazzbar-table-5",
-    rows: 2,
-    columns: 7,
-  },
-  {
-    capacity: 14,
-    title: "Table 6",
-    reference: "jazzbar-table-6",
-    rows: 2,
-    columns: 7,
-  },
-  {
-    capacity: 14,
-    title: "Table 7",
-    reference: "jazzbar-table-7",
-    rows: 2,
-    columns: 7,
-  },
-  {
-    capacity: 14,
-    title: "Table 8",
-    reference: "jazzbar-table-8",
-    rows: 2,
-    columns: 7,
-  },
-  {
-    capacity: 14,
-    title: "Table 9",
-    reference: "jazzbar-table-9",
-    rows: 2,
-    columns: 7,
-  },
-  {
-    capacity: 14,
-    title: "Table 10",
-    reference: "jazzbar-table-10",
-    rows: 2,
-    columns: 7,
-  },
-  {
-    capacity: 14,
-    title: "Table 11",
-    reference: "jazzbar-table-11",
-    rows: 2,
-    columns: 7,
-  },
-  {
-    capacity: 14,
-    title: "Table 12",
-    reference: "jazzbar-table-12",
-    rows: 2,
-    columns: 7,
-  },
-  {
-    capacity: 14,
-    title: "Table 13",
-    reference: "jazzbar-table-13",
-    rows: 2,
-    columns: 7,
-  },
-  {
-    capacity: 14,
-    title: "Table 14",
-    reference: "jazzbar-table-14",
-    rows: 2,
-    columns: 7,
-  },
-  {
-    capacity: 14,
-    title: "Table 15",
-    reference: "jazzbar-table-15",
-    rows: 2,
-    columns: 7,
-  },
-  {
-    capacity: 14,
-    title: "Table 16",
-    reference: "jazzbar-table-16",
-    rows: 2,
-    columns: 7,
-  },
-  {
-    capacity: 14,
-    title: "Table 17",
-    reference: "jazzbar-table-17",
-    rows: 2,
-    columns: 7,
-  },
-  {
-    capacity: 14,
-    title: "Table 18",
-    reference: "jazzbar-table-18",
-    rows: 2,
-    columns: 7,
-  },
-  {
-    capacity: 14,
-    title: "Table 19",
-    reference: "jazzbar-table-19",
-    rows: 2,
-    columns: 7,
-  },
-  {
-    capacity: 14,
-    title: "Table 20",
-    reference: "jazzbar-table-20",
-    rows: 2,
-    columns: 7,
-  },
+    columns: cols ?? 3,
+  }));
+
+const TABLES: Table[] = [
+  ...generateTables({ num: 5, capacity: 6 }),
+  ...generateTables({ num: 5, capacity: 2, startFrom: 5, cols: 2 }),
 ];
 
 function usage() {
