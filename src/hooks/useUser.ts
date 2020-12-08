cf3d9dd8d113ba3070e762b370bb899cf8da97c0
Chris Adams <chris@cadams.com.au>
2020-12-09 07:10:07 +1100

    More self-documenting code

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,7 +1,14 @@
 import { authSelector, profileSelector } from "utils/selectors";
 import { useSelector } from "hooks/useSelector";
+import { FirebaseReducer } from "react-redux-firebase";
+import { User } from "types/User";
 
-export const useUser = () => {
+type UseUserResult = {
+  user: FirebaseReducer.AuthState | undefined;
+  profile: FirebaseReducer.Profile<User> | undefined;
+};
+
+export const useUser = (): UseUserResult => {
   const auth = useSelector(authSelector);
   const profile = useSelector(profileSelector);
 
