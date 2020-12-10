401560e8e9effe1b0a287bc64abd5f91a91cb25f
Chris Adams <chris@cadams.com.au>
2020-12-10 22:40:31 +1100

    Fix lint

--- src/hooks/useDispatch.ts
+++ src/hooks/useDispatch.ts
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-restricted-imports
-import { RootState } from "index";
 import { useDispatch as _useDispatch } from "react-redux";
 import { ThunkDispatch } from "redux-thunk";
 import { RootActions } from "store/actions";
+import { RootState } from "index";
 
 export const useDispatch = () =>
   _useDispatch<ThunkDispatch<RootState, void, RootActions>>();
