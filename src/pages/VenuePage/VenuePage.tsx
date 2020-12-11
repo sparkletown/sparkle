940fd53bafaba3ad88309ad7f807fe030675fd3b
Chris Adams <chris@cadams.com.au>
2020-12-11 23:31:38 +1100

    Add Mixpanel analytics (#972)
    * Add Mixpanel analytics
    
    Just tracking one event, we should add more asap
    
    * Cleaner isLoaded check
    
    * Update src/hooks/useMixpanel.ts
    
    Co-authored-by: Glenn 'devalias' Grant <glenn@devalias.net>
    
    * Update src/pages/VenuePage/VenuePage.tsx
    
    Co-authored-by: Glenn 'devalias' Grant <glenn@devalias.net>
    
    * Address review comments
    
    * Prevent uninitialized LogRocket.identify call
    
    * Update src/pages/VenuePage/VenuePage.tsx
    
    Co-authored-by: Glenn 'devalias' Grant <glenn@devalias.net>
    
    * Simplify calling mixpanel hook
    
    Co-authored-by: Glenn 'devalias' Grant <glenn@devalias.net>

--- src/pages/VenuePage/VenuePage.tsx
+++ src/pages/VenuePage/VenuePage.tsx
@@ -34,6 +34,7 @@ import { useConnectCurrentEvent } from "hooks/useConnectCurrentEvent";
 import { useConnectPartyGoers } from "hooks/useConnectPartyGoers";
 import { useConnectUserPurchaseHistory } from "hooks/useConnectUserPurchaseHistory";
 import { useInterval } from "hooks/useInterval";
+import { useMixpanel } from "hooks/useMixpanel";
 import { useSelector } from "hooks/useSelector";
 import { useUser } from "hooks/useUser";
 import { useVenueId } from "hooks/useVenueId";
@@ -73,6 +74,7 @@ const hasPaidEvents = (template: VenueTemplate) => {
 const VenuePage = () => {
   const venueId = useVenueId();
   const firestore = useFirestore();
+  const mixpanel = useMixpanel();
 
   const history = useHistory();
   const [currentTimestamp] = useState(Date.now() / 1000);
@@ -239,6 +241,16 @@ const VenuePage = () => {
       : undefined
   );
 
+  const venueTemplate = venue.template;
+
+  useEffect(() => {
+    if (user && profile && venueId && venueTemplate) {
+      mixpanel.track("VenuePage loaded", {
+        venueId,
+        template: venueTemplate,
+      });
+    }
+  }, [user, profile, venueId, venueTemplate, mixpanel]);
   if (!user) {
     return <Login formType="initial" />;
   }
