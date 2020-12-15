b01f80af73a4e1dc28467f85201a3ed6c4fa3532
Glenn 'devalias' Grant <glenn@devalias.net>
2020-12-15 16:14:44 +1100

    add scripts/create-users (#1008)
    * add checkFileExists, parseCredentialFile, findUserByEmail to scripts/lib/helpers
    
    * add scripts/create-users
    
    * allow email addresses to be read from CLI args
    
    Co-authored-by: Chris Adams <chris@cadams.com.au>

--- scripts/lib/helpers.ts
+++ scripts/lib/helpers.ts
@@ -1,4 +1,4 @@
-import { writeFileSync } from "fs";
+import { existsSync, readFileSync, writeFileSync } from "fs";
 
 import admin from "firebase-admin";
 import formatDate from "date-fns/format/index.js";
@@ -25,7 +25,7 @@ export const initFirebaseAdminApp = (
     appName,
     credentialPath = resolve(__dirname, "../prodAccountKey.json"),
   }: { appName?: string; credentialPath?: string }
-) =>
+): admin.app.App =>
   admin.initializeApp(
     {
       projectId,
@@ -34,3 +34,30 @@ export const initFirebaseAdminApp = (
     },
     appName
   );
+
+export const checkFileExists = (path: string) => existsSync(path);
+
+interface CredentialFile {
+  type?: string;
+  project_id?: string;
+  private_key_id?: string;
+  private_key?: string;
+  client_email?: string;
+  client_id?: string;
+  auth_uri?: string;
+  token_uri?: string;
+  auth_provider_x509_cert_url?: string;
+  client_x509_cert_url?: string;
+}
+
+export const parseCredentialFile = (
+  credentialPath = resolve(__dirname, "../prodAccountKey.json")
+): CredentialFile => JSON.parse(readFileSync(credentialPath, "utf8"));
+
+export const findUserByEmail = (app: admin.app.App) => (
+  email: string
+): Promise<admin.auth.UserRecord | undefined> =>
+  app
+    .auth()
+    .getUserByEmail(email)
+    .catch(() => undefined);
