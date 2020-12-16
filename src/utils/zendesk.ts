91e71455d6023ca217887ce278acf3cff5462da3
Chris Adams <chris@cadams.com.au>
2020-12-17 03:15:20 +1100

    Allow certain paths to always show Zendesk

--- src/utils/zendesk.ts
+++ src/utils/zendesk.ts
@@ -1,9 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { ZENDESK_URL_PREFIXES } from "settings";
 import { loadScript } from "./loadScript";
 
 const ZENDESK_SCRIPT_SRC =
   "https://static.zdassets.com/ekr/snippet.js?key=57ad1fbc-d174-4561-8102-b3c8d98436d5";
 
 const ZENDESK_SCRIPT_ID = "ze-snippet";
+const ZENDESK_WARMUP_DELAY_MS = 6000;
 
 export const initializeZendesk = () => {
   loadScript({
@@ -14,19 +18,25 @@ export const initializeZendesk = () => {
 };
 
 const hideZendeskWidget = () => {
-  if (window.zE) {
-    window.zE("webWidget", "hide");
+  if ((window as any).zE) {
+    const showZendeskForThisPath = ZENDESK_URL_PREFIXES.find((prefix) =>
+      window.location.pathname.startsWith(prefix)
+    );
+
+    if (!showZendeskForThisPath) {
+      (window as any).zE("webWidget", "hide");
+    }
   }
 };
 
 export const showZendeskWidget = () => {
-  if (window.zE) {
-    zE("webWidget", "show");
+  if ((window as any).zE) {
+    (window as any).zE("webWidget", "show");
   } else {
     setTimeout(() => {
-      if (window.zE) {
-        zE("webWidget", "show");
+      if ((window as any).zE) {
+        (window as any).zE("webWidget", "show");
       }
-    }, 6000);
+    }, ZENDESK_WARMUP_DELAY_MS);
   }
 };
