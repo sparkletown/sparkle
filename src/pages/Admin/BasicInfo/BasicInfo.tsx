0a39397a958797d9c506c2342ba706a84fdc36d8
IvanBuljovcic <ivan.buljovcic@gmail.com>
2020-12-18 16:08:09 +0100

    Basic Info view component

---
+++ src/pages/Admin/BasicInfo/BasicInfo.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import * as S from './BasicInfo.styles';
+
+const BasicInfo: React.FC<any> = (props) => {
+  return <h1>BasicInfo</h1>
+}
+
+export default BasicInfo;
