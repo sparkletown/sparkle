b0aba5e8577e584b4a1357b28a28588104c6cdea
IvanBuljovcic <ivan.buljovcic@gmail.com>
2020-12-18 16:07:31 +0100

    useADminVenues Hook created

---
+++ src/hooks/useAdminVenues.ts
@@ -0,0 +1,10 @@
+import { ReduxFirestoreQuerySetting, useFirestoreConnect } from "react-redux-firebase";
+
+export const adminVenuesQuery = (userUid: string): ReduxFirestoreQuerySetting => ({
+  collection: "venues",
+  where: [["owners", "array-contains", userUid || ""]],
+})
+
+export const useAdminVenues = (userUid: string) => {
+  useFirestoreConnect(adminVenuesQuery(userUid));
+}
