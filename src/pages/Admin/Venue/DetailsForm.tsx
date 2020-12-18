7e7187775632717db76217f113a8aae9ca9e5fc4
Chris Adams <chris@cadams.com.au>
2020-12-18 19:08:43 +1100

    Beginnings of using venueId to save, for when venue is renamed

--- src/pages/Admin/Venue/DetailsForm.tsx
+++ src/pages/Admin/Venue/DetailsForm.tsx
@@ -282,12 +282,14 @@ interface DetailsFormLeftProps {
   onSubmit: ReturnType<ReturnType<typeof useForm>["handleSubmit"]>;
   errors: FieldErrors<FormValues>;
   editing?: boolean;
+  venueId?: string;
   setValue: ReturnType<typeof useForm>["setValue"];
   formError: boolean;
 }
 
 const DetailsFormLeft: React.FC<DetailsFormLeftProps> = ({
   editing,
+  venueId,
   state,
   values,
   isSubmitting,
@@ -336,7 +338,12 @@ const DetailsFormLeft: React.FC<DetailsFormLeftProps> = ({
         </h4>
         <input type="hidden" name="name" ref={register} value={values.name} />
         <span className="input-info">
-          The URL of your venue will be: <b>{urlSafeName}</b>
+          The URL of your venue will be:{" "}
+          <b>
+            {venueId
+              ? `${window.location.host}${venueLandingUrl(venueId)}`
+              : urlSafeName}
+          </b>
         </span>
       </div>
     );
