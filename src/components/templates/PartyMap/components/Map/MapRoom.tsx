4759d2727370e61bf561f1cb7c541aedc827d699
Glenn 'devalias' Grant <glenn@devalias.net>
2021-01-18 18:04:12 +1100

    simplify MapRoom component + fix styles

--- src/components/templates/PartyMap/components/Map/MapRoom.tsx
+++ src/components/templates/PartyMap/components/Map/MapRoom.tsx
@@ -1,12 +1,10 @@
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useCallback, useMemo } from "react";
 import classNames from "classnames";
 
 import { retainAttendance } from "store/actions/Attendance";
 
 import { Room } from "types/rooms";
 import { PartyMapVenue, RoomVisibility } from "types/venues";
-
-import { isTruthy } from "utils/types";
 
 import { useDispatch } from "hooks/useDispatch";
 
@@ -27,35 +25,23 @@ export const MapRoom: React.FC<MapRoomProps> = ({
   isSelected,
   selectRoom,
 }) => {
-  const [roomHovered, setRoomHovered] = useState<Room | undefined>(undefined);
-
-  const isRoomHovered = isTruthy(roomHovered);
-
   const dispatch = useDispatch();
 
   const handleRoomHovered = useCallback(() => {
-    setRoomHovered(room);
     dispatch(retainAttendance(true));
-  }, [dispatch, room]);
+  }, [dispatch]);
 
   const handleRoomUnhovered = useCallback(() => {
-    setRoomHovered(undefined);
     dispatch(retainAttendance(false));
   }, [dispatch]);
 
-  const showRoomTitleAlways =
-    !venue.roomVisibility ||
-    venue.roomVisibility === RoomVisibility.nameCount ||
-    venue.roomVisibility === RoomVisibility.count;
+  const containerClasses = classNames("maproom", {
+    "maproom--always-show-label":
+      venue.roomVisibility === RoomVisibility.nameCount ||
+      venue.roomVisibility === RoomVisibility.count,
+  });
 
-  const showRoomTitleWhenHovered =
-    venue.roomVisibility === RoomVisibility.hover &&
-    roomHovered?.title === room.title;
-
-  const showRoomTitle =
-    showRoomTitleAlways || (showRoomTitleWhenHovered && isRoomHovered);
-
-  const containerStyles = useMemo(
+  const roomPositionStyles = useMemo(
     () => ({
       left: `${room.x_percent}%`,
       top: `${room.y_percent}%`,
@@ -67,24 +53,18 @@ export const MapRoom: React.FC<MapRoomProps> = ({
 
   return (
     <div
-      className={classNames("map-room-container", {
-        "room-selected": isSelected,
-      })}
-      style={containerStyles}
+      className={containerClasses}
+      style={roomPositionStyles}
       onClick={selectRoom}
       onMouseEnter={handleRoomHovered}
       onMouseLeave={handleRoomUnhovered}
     >
-      <div className="room-image">
-        <img src={room.image_url} alt={room.title} />
-      </div>
+      <img className="maproom__image" src={room.image_url} alt={room.title} />
 
-      {showRoomTitle && (
-        <div className="room-title">
-          {room.title}
-          <RoomAttendance venue={venue} room={room} />
-        </div>
-      )}
+      <div className="maproom__label">
+        {room.title}
+        <RoomAttendance venue={venue} room={room} />
+      </div>
     </div>
   );
 };
