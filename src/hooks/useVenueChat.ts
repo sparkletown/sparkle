1f42d89c0a0bcbfc3b5f1e39ef51df6f36cdf4d8
mike-lvov <mike.lvov.soft@gmail.com>
2021-02-02 16:34:17 +0200

    Adds display message

--- src/hooks/useVenueChat.ts
+++ src/hooks/useVenueChat.ts
@@ -5,10 +5,15 @@ import { chatSort } from "utils/chat";
 import { getDaysAgoInSeconds, roundToNearestHour } from "utils/time";
 
 import { VENUE_CHAT_AGE_DAYS } from "settings";
+import { ChatMessage } from "store/actions/Chat";
 
 import { useSelector } from "./useSelector";
 import { useFirestoreConnect } from "./useFirestoreConnect";
 import { useVenueId } from "./useVenueId";
+import { WithId } from "utils/id";
+import { User } from "types/User";
+import { useUser } from "./useUser";
+import { useWorldUsersById } from "./users";
 
 export const useConnectVenueChat = (venueId?: string) => {
   useFirestoreConnect(
@@ -23,8 +28,31 @@ export const useConnectVenueChat = (venueId?: string) => {
   );
 };
 
+type DisplayMessage = {
+  text: string;
+  author: WithId<User>;
+  displayDate: firebase.firestore.Timestamp;
+  isMyMessage: boolean;
+};
+
+const getDisplayChatMessage = (
+  message: ChatMessage,
+  authorsById: Record<string, User>,
+  userId?: string
+): DisplayMessage => ({
+  text: message.text,
+  author: { ...authorsById[message.from], id: message.from },
+  displayDate: message.ts_utc,
+  isMyMessage: userId === message.from,
+});
+
 export const useVenueChat = () => {
   const venueId = useVenueId();
+  const { worldUsersById } = useWorldUsersById();
+  const { user } = useUser();
+
+  const userId = user?.uid;
+
   useConnectVenueChat(venueId);
 
   const chats = useSelector(venueChatsSelector) ?? [];
