2dd304cc808cdc03dcdff4bb439213d432aeea62
Glenn 'devalias' Grant <glenn@devalias.net>
2021-02-12 16:25:11 +1100

    fix typo in ScheduleItem component's .schedule-item-container class

--- src/components/templates/PartyMap/components/ScheduleItem/ScheduleItem.tsx
+++ src/components/templates/PartyMap/components/ScheduleItem/ScheduleItem.tsx
@@ -24,7 +24,7 @@ export const ScheduleItem: React.FunctionComponent<PropsType> = ({
 }) => {
   const dispatch = useDispatch();
   return (
-    <div className="shedule-item-container">
+    <div className="schedule-item-container">
       <div className={`time-section ${isCurrentEvent ? "primary" : ""}`}>
         <div>
           <b>{formatUtcSeconds(event.start_utc_seconds)}</b>
