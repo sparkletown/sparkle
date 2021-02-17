3f5ca815405edb9facd44f7f060e1c936525498d
Denis Dimitrov <dedimitrov@melontech.com>
2021-02-17 15:10:15 +0200

    handle empty venues

--- src/components/organisms/AdminVenues/AdminVenues.tsx
+++ src/components/organisms/AdminVenues/AdminVenues.tsx
@@ -3,6 +3,7 @@ import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import { AnyVenue, VenueTemplate } from "types/venues";
+
 import { WithId } from "utils/id";
 
 import "./AdminVenues.scss";
@@ -16,6 +17,8 @@ export const AdminVenues: React.FC<AdminVenuesProps> = ({ venues }) => {
     return venues?.filter((venue) => venue.template === VenueTemplate.partymap);
   }, [venues]);
 
+  const hasVenues: boolean = !!partyVenues.length;
+
   return (
     <div className="admin-venue">
       <div className="admin-venue__create-button">
@@ -24,33 +27,39 @@ export const AdminVenues: React.FC<AdminVenuesProps> = ({ venues }) => {
           Create a new space
         </Button>
       </div>
-      <div className="admin-venue__cards">
-        {partyVenues.map((venue, index) => {
-          return (
-            <div key={index} className="card">
-              <div
-                className="card__bg"
-                style={{
-                  backgroundImage: `url(${venue.mapBackgroundImageUrl})`,
-                  backgroundSize: "cover",
-                }}
-              ></div>
-              <div className="card__info">
+      {!hasVenues && (
+        <div className="admin-venue__empty-venues">
+          <h3>Welcome!</h3>
+          <h3>Create your first Sparkle space</h3>
+        </div>
+      )}
+      {hasVenues && (
+        <div className="admin-venue__cards">
+          {partyVenues.map((venue, index) => {
+            return (
+              <div key={index} className="card">
                 <div
-                  className="card__logo"
-                  style={{ backgroundImage: `url(${venue.host?.icon})` }}
+                  className="card__bg"
+                  style={{
+                    backgroundImage: `url(${venue.mapBackgroundImageUrl})`,
+                    backgroundSize: "cover",
+                  }}
                 ></div>
-                <div className="card__text">
+                <div className="card__info">
+                  <div
+                    className="card__logo"
+                    style={{ backgroundImage: `url(${venue.host?.icon})` }}
+                  ></div>
                   <h3>{venue.name}</h3>
                 </div>
+                <Link className="card__button" to={`/admin_v2/${venue.id}`}>
+                  Manage Party
+                </Link>
               </div>
-              <Link className="card__button" to={`/admin_v2/${venue.id}`}>
-                Manage Party
-              </Link>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/Admin/Sidebar/Sidebar.tsx
+++ src/pages/Admin/Sidebar/Sidebar.tsx
@@ -1,9 +1,9 @@
-import { useSelector } from "hooks/useSelector";
-import { useVenueId } from "hooks/useVenueId";
 import React from "react";
-import { Button, Form, Nav } from "react-bootstrap";
-import { Link, useHistory } from "react-router-dom";
-import { VenueTemplate } from "types/venues";
+import { Button, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+import { useVenueId } from "hooks/useVenueId";
+
 import { SidebarOption } from "../Admin_v2";
 
 import * as S from "./Sidebar.styles";
@@ -22,14 +22,6 @@ const AdminSidebar: React.FC<AdminSidebarProps> = ({
   onClick,
 }) => {
   const venueId = useVenueId();
-  const history = useHistory();
-
-  const venues = useSelector((state) => state.firestore.ordered.venues);
-  const partyVenues = venues?.filter(
-    (venue) => venue.template === VenueTemplate.partymap
-  );
-  const handleVenueChange = (event: React.ChangeEvent<HTMLSelectElement>) =>
-    history.push(`/admin_v2/${event.target.value}`);
 
   const initialVenue = venueId ?? selectVenue;
 
@@ -40,22 +32,6 @@ const AdminSidebar: React.FC<AdminSidebarProps> = ({
       </Button>
 
       <S.InnerWrapper>
-        <Form.Control
-          as="select"
-          custom
-          onChange={handleVenueChange}
-          defaultValue={initialVenue}
-        >
-          <option disabled aria-disabled value={selectVenue}>
-            - Select Venue-
-          </option>
-          {partyVenues?.map((venue) => (
-            <option key={venue.id!} value={venue.id!}>
-              {venue.name}
-            </option>
-          ))}
-        </Form.Control>
-
         {initialVenue !== selectVenue && (
           <Nav activeKey={selected} onSelect={onClick}>
             {sidebarOptions.map((option: SidebarOption) => {
