b998cf8fcb5aaee89ba07a05c52c7825d34d142e
Malaika <malaika.aiyar@gmail.com>
2021-02-19 14:33:38 +0000

    Update RoomEdit.tsx

--- src/pages/Admin/Room/Edit/RoomEdit.tsx
+++ src/pages/Admin/Room/Edit/RoomEdit.tsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Form, Modal } from "react-bootstrap";
-import { useForm } from "react-hook-form";
 import ImageInput from "components/atoms/ImageInput";
-
-import * as S from "./RoomEdit.styles";
-import { CustomInputsType, RoomTemplate, ROOM_TEMPLATES } from "settings";
-import { Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { Form, Modal, Button } from "react-bootstrap";
+import { RoomTemplate, ROOM_TEMPLATES } from "settings";
 import { EditRoomProps } from "./RoomEdit.types";
 
-const EditRoom: React.FC<EditRoomProps> = ({
+import "./RoomEdit.scss";
+
+export const EditRoom: React.FC<EditRoomProps> = ({
   isVisible,
   onClickOutsideHandler,
   room,
@@ -38,92 +37,13 @@ const EditRoom: React.FC<EditRoomProps> = ({
       title: room.title,
       url: room.url,
       description: room.description,
-      tempalte: room.template,
+      template: room.template,
     },
   });
 
   const values = watch();
 
   const handleImageChange = (val: string) => setValue("image_url", val, false);
-
-  const renderNameInput = () => (
-    <Form.Row>
-      <S.InputWrapper>
-        <Form.Label>Name your room</Form.Label>
-        <Form.Control
-          type="text"
-          ref={register}
-          name="title"
-          placeholder="Room name"
-          custom
-        />
-      </S.InputWrapper>
-    </Form.Row>
-  );
-
-  const renderUrlInput = () => (
-    <S.InputWrapper>
-      <span>The room url</span>
-      <input type="text" ref={register} name="url" placeholder="Room url" />
-    </S.InputWrapper>
-  );
-
-  const renderDescriptionInput = () => (
-    <S.InputWrapper>
-      <span>The room description (optional)</span>
-      <input
-        type="text"
-        ref={register}
-        name="description"
-        placeholder="Description"
-      />
-    </S.InputWrapper>
-  );
-
-  const renderLogoInput = () => (
-    <S.InputWrapper>
-      <span>How you want the room to appear on the map</span>
-
-      <ImageInput
-        onChange={handleImageChange}
-        name="image"
-        forwardRef={register}
-        small
-        nameWithUnderscore
-        imgUrl={room.image_url}
-      />
-    </S.InputWrapper>
-  );
-
-  const renderCustomInput = (input: CustomInputsType) => (
-    <S.InputWrapper key={input.name}>
-      <span>{input.title}</span>
-
-      <input type="text" name={input.name} ref={register} />
-    </S.InputWrapper>
-  );
-
-  const renderTemplateSelect = () => {
-    return (
-      <S.InputWrapper>
-        <Form.Label>Change room template</Form.Label>
-        <Form.Control
-          as="select"
-          custom
-          name="tempalte"
-          ref={register}
-          onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
-            setSelectedTemplate(e.target.value)
-          }
-        >
-          {ROOM_TEMPLATES.map((template) => (
-            <option key={template.name}>{template.template}</option>
-          ))}
-        </Form.Control>
-      </S.InputWrapper>
-    );
-  };
-
   const onSubmit = () => submitHandler(values, room.roomIndex!);
 
   return (
@@ -134,16 +54,88 @@ const EditRoom: React.FC<EditRoomProps> = ({
 
       <Modal.Body>
         <Form onSubmit={handleSubmit(onSubmit)}>
-          {renderNameInput()}
-          {renderUrlInput()}
-          {renderDescriptionInput()}
-          {renderLogoInput()}
+          <Form.Row>
+            <div className="input-wrapper">
+              <Form.Label>Name your room</Form.Label>
+              <Form.Control
+                type="text"
+                ref={register}
+                name="title"
+                placeholder="Room name"
+                custom
+              />
+            </div>
+          </Form.Row>
 
-          {roomTemplate?.customInputs?.map((input) => renderCustomInput(input))}
+          <Form.Row>
+            <div className="input-wrapper">
+              <Form.Label>The room url</Form.Label>
+              <Form.Control
+                type="text"
+                ref={register}
+                name="url"
+                placeholder="Room url"
+                custom
+              />
+            </div>
+          </Form.Row>
 
-          {renderTemplateSelect()}
+          <Form.Row>
+            <div className="input-wrapper">
+              <Form.Label>The room description (optional)</Form.Label>
+              <Form.Control
+                type="text"
+                ref={register}
+                name="description"
+                placeholder="Description"
+                custom
+              />
+            </div>
+          </Form.Row>
 
-          <S.ButtonsWrapper>
+          <Form.Row>
+            <div className="input-wrapper">
+              <Form.Label>
+                How you want the room to appear on the map
+              </Form.Label>
+              <ImageInput
+                onChange={handleImageChange}
+                name="image"
+                forwardRef={register}
+                small
+                nameWithUnderscore
+                imgUrl={room.image_url}
+              />
+            </div>
+          </Form.Row>
+
+          {roomTemplate?.customInputs?.map((input) => (
+            <div className="input-wrapper" key={input.name}>
+              <span>{input.title}</span>
+              <input type="text" name={input.name} ref={register} />
+            </div>
+          ))}
+
+          <Form.Row>
+            <div className="input-wrapper">
+              <Form.Label>Change room template</Form.Label>
+              <Form.Control
+                as="select"
+                custom
+                name="template"
+                ref={register}
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                  setSelectedTemplate(e.target.value)
+                }
+              >
+                {ROOM_TEMPLATES.map((template) => (
+                  <option key={template.name}>{template.template}</option>
+                ))}
+              </Form.Control>
+            </div>
+          </Form.Row>
+
+          <div className="buttons-wrapper">
             <Button variant="danger" onClick={deleteHandler}>
               Delete room
             </Button>
@@ -151,7 +143,7 @@ const EditRoom: React.FC<EditRoomProps> = ({
             <Button type="submit" disabled={isSubmitting || !dirty}>
               Save room
             </Button>
-          </S.ButtonsWrapper>
+          </div>
         </Form>
       </Modal.Body>
     </Modal>
