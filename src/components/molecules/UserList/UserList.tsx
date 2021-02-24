0f1d1dfed769052e72abceb2de946bdcd890ecf2
Glenn 'devalias' Grant <glenn@devalias.net>
2021-02-24 17:12:00 +1100

    cleanup UserList + remove unnecessary undefined check

--- src/components/molecules/UserList/UserList.tsx
+++ src/components/molecules/UserList/UserList.tsx
@@ -44,16 +44,20 @@ const UserList: React.FunctionComponent<PropsType> = ({
   const [selectedUserProfile, setSelectedUserProfile] = useState<
     WithId<User>
   >();
-  const usersSanitized = _users?.filter(
+
+  const usersSanitized = _users.filter(
     (user) => !user.anonMode && user.partyName && user.id
   );
+
   const usersToDisplay = isExpanded
     ? usersSanitized
     : usersSanitized?.slice(0, limit);
+
   const attendance = usersSanitized.length + (attendanceBoost ?? 0);
   const venue = useSelector(currentVenueSelectorData);
 
-  if (!usersSanitized || attendance < 1) return <></>;
+  if (attendance < 1) return null;
+
   return (
     <>
       <div className="container userlist-container">
@@ -63,6 +67,7 @@ const UserList: React.FunctionComponent<PropsType> = ({
             {attendance === 1 ? "person" : "people"}{" "}
             {isCamp && IS_BURN ? "in the camp" : activity}
           </p>
+
           {!disableSeeAll && usersSanitized.length > limit && (
             <p
               className="clickable-text"
@@ -73,6 +78,7 @@ const UserList: React.FunctionComponent<PropsType> = ({
             </p>
           )}
         </div>
+
         <div className="row no-margin">
           {usersToDisplay.map(
             (user) =>
