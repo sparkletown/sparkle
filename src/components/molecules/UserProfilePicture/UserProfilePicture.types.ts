4b5d034bd28cf4bc3c67c7ed73c1d79c55d0ec9b
Glenn 'devalias' Grant <glenn@devalias.net>
2021-02-24 16:43:27 +1100

    sort non-optional UserProfilePicture props to the tope + add @debt comment

--- src/components/molecules/UserProfilePicture/UserProfilePicture.types.ts
+++ src/components/molecules/UserProfilePicture/UserProfilePicture.types.ts
@@ -1,13 +1,15 @@
 import { User } from "types/User";
 import { WithId } from "utils/id";
 
+// @debt follow our standard patterns and move this back into ./UserProfilePicture.tsx
 export interface UserProfilePictureProp {
+  user: WithId<User>;
+  setSelectedUserProfile: (user: WithId<User>) => void;
+
   isAudioEffectDisabled?: boolean;
   miniAvatars?: boolean;
   avatarClassName?: string;
   avatarStyle?: object;
   containerStyle?: object;
-  setSelectedUserProfile: (user: WithId<User>) => void;
-  user: WithId<User>;
   reactionPosition?: "right" | "left" | undefined;
 }
