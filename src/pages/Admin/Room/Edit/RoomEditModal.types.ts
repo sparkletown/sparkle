24c75f86065c582fd483d826ce2c7aa30b8c644a
Malaika <malaika.aiyar@gmail.com>
2021-02-25 20:30:54 +0000

    Delete RoomEditModal.types.ts

--- src/pages/Admin/Room/Edit/RoomEditModal.types.ts
+++
@@ -1,9 +0,0 @@
-import { RoomData_v2 } from "types/rooms";
-
-export interface RoomEditModalProps {
-  isVisible: boolean;
-  onClickOutsideHandler: () => void;
-  room: RoomData_v2;
-  submitHandler: (values: RoomData_v2, index: number) => void;
-  deleteHandler: () => void;
-}
