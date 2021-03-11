c70bb942d434013e372930f56055bbe516f1985e
Glenn 'devalias' Grant <glenn@devalias.net>
2021-03-11 18:34:08 +1100

    add CustomLoader schema validation + isCustomLoader type predicate

--- src/types/CustomLoader.ts
+++ src/types/CustomLoader.ts
@@ -1,5 +1,16 @@
+import * as Yup from "yup";
+
 export type CustomLoader = {
   url: string;
   text: string;
   title: string;
 };
+
+export const CustomLoaderSchema = Yup.object().shape<CustomLoader>({
+  url: Yup.string().required(),
+  text: Yup.string().required(),
+  title: Yup.string().required(),
+});
+
+export const isCustomLoader = (data: {}): data is CustomLoader =>
+  CustomLoaderSchema.isValidSync(data);
