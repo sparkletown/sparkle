2d53ec39d456d86f44c0c9151bf78243a3ff4359
Glenn 'devalias' Grant <glenn@devalias.net>
2021-03-11 22:04:58 +1100

    wire up prototype of the LoadingPage

--- src/components/molecules/LoadingPage/LoadingPage.tsx
+++ src/components/molecules/LoadingPage/LoadingPage.tsx
@@ -1,57 +1,85 @@
-import React, { useState } from "react";
-import { IS_BURN } from "secrets";
+import React, { useMemo, useState } from "react";
 
-import { useInterval } from "hooks/useInterval";
-// import { useCustomLoaders } from "hooks/useCustomLoaders";
+import { CustomLoader } from "types/CustomLoader";
+
+import { useCustomLoaders } from "hooks/useCustomLoaders";
 
 import "./loading.scss";
 
-const quotes = IS_BURN
-  ? [
-      "Navigating dust storms...",
-      "Roadtripping across Nevada...",
-      "In a traffic jam in Gerlach...",
-      "Biking out to deep playa...",
-      "Fixing tyre punctures...",
-      "Reimagining the real...",
-      "Dodging cow pats in the paddock...",
-      "Fighting dropbears...",
-      "Herding koalas...",
-      "Learning the local lingo...",
-      "Becoming good friends with the NEEEEEIGHBOURS...",
-      "Throwing shrimp on the barbie...",
-      "Watering the speakers...",
-      "Massaging minds...",
-      "Laying out the croissants...",
-      "Adjusting microphone volumes...",
-    ]
-  : ["Loading..."];
+const getRandomInt = (max: number, randomValue: () => number = Math.random) => {
+  return Math.floor(randomValue() * Math.floor(max));
+};
+
+// TODO: move this into useCustomLoaders, or settings, or similar?
+export const DEFAULT_LOADER: CustomLoader = {
+  title: "Loading Sparkle",
+  text: "Loading Sparkle",
+  url: "",
+};
 
 export const LoadingPage = () => {
-  const [quote, setQuote] = useState("Loading...");
+  const { customLoaders } = useCustomLoaders();
 
-  // const loaders = useCustomLoaders();
+  // TODO: randomly choose one of the custom loaders to use
+  //   This should be 'stable per load' and not change even if there are multiple re-renders
+  // TODO: for some reason it seems to run at least twice, with changing int values.. which makes it 'flash' and change backgrounds
+  // TODO: move all of this into useCustomLoaders or similar?
 
-  useInterval(
-    () => {
-      setQuote(quotes[Math.floor(Math.random() * quotes.length)]);
-    },
-    IS_BURN ? 1000 : undefined
-  );
+  // Set the random integer once, but calculate how that maps to an index later
+  const [rawRandomValue] = useState<number>(Math.random());
 
+  const chosenLoader: CustomLoader = useMemo(() => {
+    if (customLoaders.length < 1) return DEFAULT_LOADER;
+
+    const randomIndex = getRandomInt(
+      customLoaders.length - 1,
+      () => rawRandomValue
+    );
+
+    // TODO: remove this debug logging
+    console.log({ rawRandomValue, randomIndex });
+
+    return customLoaders[randomIndex] ?? DEFAULT_LOADER;
+  }, [rawRandomValue, customLoaders]);
+
+  // TODO: move these inline styles into scss
   return (
-    <div className="loading-screen">
-      <div className="loading-content">
-        <div className="burningman-loading-container">
-          <span className="loading-sparkle-1" />
-          <span className="loading-sparkle-2" />
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#7c46fb", // $primary
+        backgroundImage: `url(${chosenLoader.url})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div
+        className="loading-screen"
+        style={{ backgroundColor: "transparent", backdropFilter: "blur(15px)" }}
+      >
+        <div className="loading-content">
           <div className="burningman-loading">
             <div className="burningman-loading-anim" />
           </div>
+          {chosenLoader.url && (
+            // TODO: Unhardcode the width/height here
+            //   https://stackoverflow.com/questions/600743/how-to-get-div-height-to-auto-adjust-to-background-size/22211990#22211990
+            <div
+              style={{
+                backgroundImage: `url(${chosenLoader.url})`,
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                width: "600px",
+                height: "800px",
+                borderRadius: "20px",
+              }}
+            />
+          )}
+          <span className="loading-randomquote show">Loading...</span>
         </div>
-        <span className={`loading-randomquote ${quote && "show"}`}>
-          {quote}
-        </span>
       </div>
     </div>
   );
