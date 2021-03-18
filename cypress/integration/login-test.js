45f105d839b4158f343ef549db41ece2abfc0002
Malaika <malaika.aiyar@gmail.com>
2021-03-18 23:16:41 +0000

    Update login-test.js

--- cypress/integration/login-test.js
+++ cypress/integration/login-test.js
@@ -1,13 +1,16 @@
 /* eslint-disable no-undef */
+const EMAIL = process.env.CYPRESS_TEST_STAGING_EMAIL;
+const PASSWORD = process.env.CYPRESS_TEST_STAGING_PASSWORD;
+
 const logIn = () => {
   cy.get(".login-button")
     .click();
 
   cy.get(".form > .input-group:nth-child(1) > input")
-    .type('malaika.aiyar@gmail.com');
+    .type(EMAIL);
 
   cy.get(".form > .input-group:nth-child(2) > input")
-    .type('QWE#qwe3');
+    .type(PASSWORD);
 
   cy.get('.form > .btn')
     .click();
@@ -35,104 +38,104 @@ describe("Test staging.sparkle.space/in/devaliashacksville", function () {
     logOut()
   });
 
-  it("can visit and interact with the jazzbar", () => {
+  // it("can visit and interact with the jazzbar", () => {
 
-    // visiting site
-    cy.visit("https://staging.sparkle.space/in/devaliashacksville");
-    cy.clearCookies();
+  //   // visiting site
+  //   cy.visit("https://staging.sparkle.space/in/devaliashacksville");
+  //   cy.clearCookies();
 
-    // logging in again
-    // logIn()
+  //   // logging in again
+  //   // logIn()
 
-    // getting into jazzbar
+  //   // getting into jazzbar
 
-    cy.get(".maproom")
-      .contains("jazzbar")
-      .parent()
-      .parent()
-      .click();
+  //   cy.get(".maproom")
+  //     .contains("jazzbar")
+  //     .parent()
+  //     .parent()
+  //     .click();
 
-    // don't know why it requires two clicks? the modal seems to disappear before cypress can click on it
-    cy.get(".maproom")
-      .contains("jazzbar")
-      .parent()
-      .parent()
-      .click();
+  //   // don't know why it requires two clicks? the modal seems to disappear before cypress can click on it
+  //   cy.get(".maproom")
+  //     .contains("jazzbar")
+  //     .parent()
+  //     .parent()
+  //     .click();
 
-    cy.get('.room-modal-ongoing-event-container > .room-entry-button')
-      .click();
+  //   cy.get('.room-modal-ongoing-event-container > .room-entry-button')
+  //     .click();
 
-    cy.location('pathname')
-      .should('eq', '/in/devaliasjazzbar');
+  //   cy.location('pathname')
+  //     .should('eq', '/in/devaliasjazzbar');
 
-    // // interact with video ...
+  //   // // interact with video ...
 
-    // // go to a table
-    cy.get('.music-bar-content > .jazzbar-table-component-container')
-      .each(($table) => {
-        cy.get('.chat-sidebar__controller')
-          .click();
+  //   // // go to a table
+  //   cy.get('.music-bar-content > .jazzbar-table-component-container')
+  //     .each(($table) => {
+  //       cy.get('.chat-sidebar__controller')
+  //         .click();
 
-        // enter table room
-        cy.wrap($table).get('.table-item > .add-participant-button')
-          .first()
-          .click();
-        cy.get('.modal-container > .btn')
-          .click();
+  //       // enter table room
+  //       cy.wrap($table).get('.table-item > .add-participant-button')
+  //         .first()
+  //         .click();
+  //       cy.get('.modal-container > .btn')
+  //         .click();
 
-        // interact with table and video ...
+  //       // interact with table and video ...
 
-        // leave table
-        cy.get('.back-button-container > .back-button')
-          .click();
-    });
-
-
-    // logging back out
-    // logOut()
-
-  });
-
-  it("can visit and interact with the auditorium", () => {
-    // visiting site
-    cy.visit("https://staging.sparkle.space/in/devaliashacksville");
-    cy.clearCookies();
-
-    // logging in
-    // logIn()
+  //       // leave table
+  //       cy.get('.back-button-container > .back-button')
+  //         .click();
+  //   });
 
 
-    // getting into auditorium
-    cy.get(".maproom")
-      .contains("auditorium")
-      .parent()
-      .parent()
-      .click();
+  //   // logging back out
+  //   // logOut()
 
-    // same as for jazzbar button, don't know why it requires me to click on the room button twice for it to work
-    cy.get(".maproom")
-      .contains("auditorium")
-      .parent()
-      .parent()
-      .click();
+  // });
 
-    cy.get('.room-modal-ongoing-event-container > .room-entry-button')
-      .click();
+  // it("can visit and interact with the auditorium", () => {
+  //   // visiting site
+  //   cy.visit("https://staging.sparkle.space/in/devaliashacksville");
+  //   cy.clearCookies();
 
-    cy.location('pathname')
-      .should('eq', '/in/devaliasauditorium');
+  //   // logging in
+  //   // logIn()
 
-    cy.get('.audience-container > .audience > div:nth-child(2) > div:nth-child(5)')
-      .click();
 
-    // more interactions here ...
+  //   // getting into auditorium
+  //   cy.get(".maproom")
+  //     .contains("auditorium")
+  //     .parent()
+  //     .parent()
+  //     .click();
 
-    cy.get(".leave-seat-button")
-      .click();
+  //   // same as for jazzbar button, don't know why it requires me to click on the room button twice for it to work
+  //   cy.get(".maproom")
+  //     .contains("auditorium")
+  //     .parent()
+  //     .parent()
+  //     .click();
 
-    // logging out
-    logOut()
+  //   cy.get('.room-modal-ongoing-event-container > .room-entry-button')
+  //     .click();
 
-  });
+  //   cy.location('pathname')
+  //     .should('eq', '/in/devaliasauditorium');
+
+  //   cy.get('.audience-container > .audience > div:nth-child(2) > div:nth-child(5)')
+  //     .click();
+
+  //   // more interactions here ...
+
+  //   cy.get(".leave-seat-button")
+  //     .click();
+
+  //   // logging out
+  //   logOut()
+
+  // });
 
 });
