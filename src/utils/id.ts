149902ce342d69fccf08b6a3bceef0d32dfe9b1e
Glenn 'devalias' Grant <glenn@devalias.net>
2021-04-01 16:34:18 +1100

    add WithoutId type + withoutId helper

--- src/utils/id.ts
+++ src/utils/id.ts
@@ -1,3 +1,5 @@
+import omit from "lodash/omit";
+
 // @debt Move this file into types/ or possibly even merge it with types/utility.ts
 
 export type WithId<T extends object> = T & { id: string };
@@ -6,6 +8,11 @@ export const withId = <T extends object>(obj: T, id: string): WithId<T> => ({
   ...obj,
   id,
 });
+
+export type WithoutId<T extends object> = Pick<T, Exclude<keyof T, "id">>;
+
+export const withoutId = <T extends object>(obj: T): WithoutId<T> =>
+  omit(obj, "id");
 
 export type WithVenueId<T extends object> = T & { venueId: string };
 
