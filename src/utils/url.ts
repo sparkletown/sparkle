2f6ea709f89a31c05a78276358a2dc3aa89cf8cd
Yurii Lubynets <yurichix1@gmail.com>
2021-04-01 14:57:29 +0300

    update pattern to label the metadata

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -47,12 +47,13 @@ export const openUrl = (url: string) => {
         `Invalid URL ${url} on page ${window.location.href}; allowing for now (workaround)`
       ),
       (event) => {
-        event.addMetadata("utils/url::openUrl", { url });
+        event.addMetadata("context", { func: "utils/url::openUrl", url });
       }
     );
     // @debt keep the checking in place so we can debug further, but don't block attempts to open
     // return;
   }
+
   if (isExternalUrl(url)) {
     window.open(url, "_blank", "noopener,noreferrer");
   } else {
