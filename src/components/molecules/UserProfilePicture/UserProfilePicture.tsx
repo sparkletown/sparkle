d460c0e08cfda99a33847cba0423e005ea604233
Malaika <malaika.aiyar@gmail.com>
2021-04-08 21:25:16 +0100

    Update UserProfilePicture.tsx

--- src/components/molecules/UserProfilePicture/UserProfilePicture.tsx
+++ src/components/molecules/UserProfilePicture/UserProfilePicture.tsx
@@ -33,7 +33,7 @@ export interface UserProfilePictureProp {
 // @debt This component should be divided into a few with simpler logic. Also, remove `styled components`
 // @debt the UserAvatar component serves a very similar purpose to this, we should unify them as much as possible
 const UserProfilePicture: React.FC<UserProfilePictureProp> = ({
-  isAudioEffectDisabled,
+  isAudioEffectDisabled = true,
   miniAvatars = false,
   avatarClassName = "profile-icon",
   avatarStyle,
