cc1ccefb70a401105f203302e40da457125b5f07
Glenn 'devalias' Grant <glenn@devalias.net>
2021-04-08 19:56:09 +1000

    export react scheduler tracing's unstable_wrap as as wrapReactScheduler in utils/performance

--- src/utils/performance.ts
+++ src/utils/performance.ts
@@ -1,6 +1,9 @@
 import firebase from "firebase/app";
 
-export { unstable_trace as traceReactScheduler } from "scheduler/tracing";
+export {
+  unstable_trace as traceReactScheduler,
+  unstable_wrap as wrapReactScheduler,
+} from "scheduler/tracing";
 
 /**
  * A centralised location to track all custom code trace names used within the app.
