f4c868471dcad8c0b597c686ef3a796ef5c4aa89
mike-lvov <mike.lvov.soft@gmail.com>
2021-04-29 22:56:05 +0300

    Fixes poster preview

--- src/components/templates/PosterHall/components/PosterPreview/PosterPreview.tsx
+++ src/components/templates/PosterHall/components/PosterPreview/PosterPreview.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import classNames from "classnames";
 
 import { PosterPageVenue } from "types/venues";
@@ -16,7 +16,6 @@ export const PosterPreview: React.FC<PosterPreviewProps> = ({
   posterVenue,
   onClick,
 }) => {
-  // TODO: Remove poster object and put all of its properties into the venue itself
   const { title, author, categories } = posterVenue.poster ?? {};
 
   const posterClassnames = classNames("PosterPreview", {
@@ -25,28 +24,35 @@ export const PosterPreview: React.FC<PosterPreviewProps> = ({
 
   const hasCategories = categories?.length && categories.length > 0;
 
+  const renderedCategories = useMemo(
+    () => (
+      <div className="PosterPreview__categories">
+        {categories?.map((category) => (
+          <div
+            key={category.title + category.color}
+            className="PosterPreview__category"
+            style={{
+              backgroundColor: category.color,
+            }}
+          >
+            {category.title}
+          </div>
+        ))}
+      </div>
+    ),
+    [categories]
+  );
+
   return (
     <div className={posterClassnames} onClick={onClick}>
       <p className="PosterPreview__title">{title}</p>
-      {hasCategories && (
-        <div className="PosterPreview__categories">
-          {categories?.map((category) => (
-            <div
-              key={category.title + category.color}
-              className="PosterPreview__category"
-              style={{
-                backgroundColor: category.color,
-              }}
-            >
-              {category.title}
-            </div>
-          ))}
-        </div>
-      )}
+
+      {hasCategories && renderedCategories}
+
       {author && (
         <div className="PosterPreview__author">
-          <div className="PosterPreview__author__name">{author.name}</div>
-          <div className="PosterPreview__author__institution">
+          <div className="PosterPreview__author-name">{author.name}</div>
+          <div className="PosterPreview__author-institution">
             {author.institution}
           </div>
         </div>
