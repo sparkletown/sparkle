cc504ba83a67397c00bf04b883a3c20a08f03ec3
Viktoryia <viktoryia.work@gmail.com>
2021-05-05 15:11:51 +0300

    Return EventDisplay to its staging state

--- src/components/molecules/EventDisplay/EventDisplay.tsx
+++ src/components/molecules/EventDisplay/EventDisplay.tsx
@@ -1,16 +1,13 @@
-import React, { useState, useMemo } from "react";
-import { format } from "date-fns";
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBookmark as solidBookmark } from "@fortawesome/free-solid-svg-icons";
-import { faBookmark as regularBookmark } from "@fortawesome/free-regular-svg-icons";
-
+import React, { useMemo } from "react";
 import classNames from "classnames";
 
 import { AnyVenue, VenueEvent } from "types/venues";
+import { Room } from "types/rooms";
 
-import { getCurrentTimeInUTCSeconds, formatHour, getMinutes } from "utils/time";
+import { formatHourAndMinute, getCurrentTimeInUTCSeconds } from "utils/time";
 import { WithId } from "utils/id";
+
+import { useRoom } from "hooks/useRoom";
 
 import "./EventDisplay.scss";
 
@@ -20,7 +17,14 @@ export interface EventDisplayProps {
 }
 
 export const EventDisplay: React.FC<EventDisplayProps> = ({ event, venue }) => {
-  const [isBookmarked, setBookmark] = useState(false);
+  const eventRoomTitle = event.room;
+
+  const room = useMemo(
+    () => venue?.rooms?.find((room) => room.title === eventRoomTitle),
+    [venue, eventRoomTitle]
+  );
+
+  const buttonText = `${event.room ?? "Enter"} ${venue && `- ${venue.name}`}`;
 
   const isLiveEvent =
     event.start_utc_seconds < getCurrentTimeInUTCSeconds() &&
@@ -31,58 +35,49 @@ export const EventDisplay: React.FC<EventDisplayProps> = ({ event, venue }) => {
     "schedule-event-container--live": isLiveEvent,
   });
 
-  const starEventHour = formatHour(event.start_utc_seconds);
-
-  const oneHourBehind = format(
-    new Date().setHours(new Date().getHours() - 1),
-    "HH"
-  );
-  const duration = Math.floor(event.duration_minutes / 60);
-  const beginnigToShow = Number(starEventHour) - Number(oneHourBehind);
-
-  const currentMinutes = getMinutes(getCurrentTimeInUTCSeconds());
-
-  const timeToShow = useMemo(() => {
-    const minutesShowing =
-      currentMinutes >= 1020 ? 1020 : (new Date().getHours() - 1) * 60;
-    const startTime = getMinutes(event.start_utc_seconds);
-
-    return startTime - minutesShowing;
-  }, [currentMinutes, event.start_utc_seconds]);
-
   return (
-    <div
-      className={containerClasses}
-      style={{
-        marginLeft: `${timeToShow * 3.33 + 90}px`,
-        width: `${
-          beginnigToShow * 200 + 100 + duration * 200 > 4800
-            ? 4800 - (Number(beginnigToShow) * 200 + 100)
-            : duration * 200
-        }px`,
-      }}
-    >
+    <div className={containerClasses}>
+      <div className="schedule-event-time">
+        <div className="schedule-event-time-start">
+          {formatHourAndMinute(event.start_utc_seconds)}
+        </div>
+        <div className="schedule-event-time-end">
+          {formatHourAndMinute(
+            event.start_utc_seconds + event.duration_minutes * 60
+          )}
+        </div>
+        {isLiveEvent && <span className="schedule-event-time-live">Live</span>}
+      </div>
       <div className="schedule-event-info">
         <div className="schedule-event-info-title">{event.name}</div>
-        <div className="schedule-event-info-description">by {event.host}</div>
-      </div>
-      <div
-        style={{
-          height: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-        onClick={(e) => {
-          e.stopPropagation();
-          setBookmark(!isBookmarked);
-        }}
-      >
-        <FontAwesomeIcon
-          icon={isBookmarked ? solidBookmark : regularBookmark}
-          style={{ cursor: "pointer", width: "15px", height: "20px" }}
-        />
+        <div className="schedule-event-info-description">
+          {event.description}
+        </div>
+        <div className="schedule-event-info-room">
+          {event.room && room && venue ? (
+            <EnterRoomButton room={room} venue={venue}>
+              {buttonText}
+            </EnterRoomButton>
+          ) : (
+            <div>{buttonText}</div>
+          )}
+        </div>
       </div>
     </div>
   );
+};
+
+interface EnterRoomButtonProps {
+  room: Room;
+  venue: WithId<AnyVenue>;
+}
+
+const EnterRoomButton: React.FC<EnterRoomButtonProps> = ({
+  room,
+  venue,
+  children,
+}) => {
+  const { enterRoom } = useRoom({ room, venueName: venue.name });
+
+  return <div onClick={enterRoom}>{children}</div>;
 };
