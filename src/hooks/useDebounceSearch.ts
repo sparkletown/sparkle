9acbbec8f58d7d7b122f095b27d50c10400627c7
mike-lvov <mike.lvov.soft@gmail.com>
2021-05-05 12:08:19 +0300

    Adds comment; REmoved debounce cancel

--- src/hooks/useDebounceSearch.ts
+++ src/hooks/useDebounceSearch.ts
@@ -23,10 +23,6 @@ export const useDebounceSearch = () => {
 
   useEffect(() => {
     debouncedSearch(searchInputValue);
-
-    return () => {
-      debouncedSearch.cancel();
-    };
   }, [searchInputValue, debouncedSearch]);
 
   return {
