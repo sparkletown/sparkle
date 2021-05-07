156b89c1cd1fa5cf9d32d1abcb6b6157a71e755c
Glenn 'devalias' Grant <glenn@devalias.net>
2021-05-07 20:48:32 +1000

    add getVenueCollectionRef + getVenueRef

--- src/api/sovereignVenue.ts
+++ src/api/sovereignVenue.ts
@@ -4,6 +4,12 @@ import { AnyVenue } from "types/venues";
 
 import { withId, WithId } from "utils/id";
 
+export const getVenueCollectionRef = () =>
+  firebase.firestore().collection("venues");
+
+export const getVenueRef = (venueId: string) =>
+  getVenueCollectionRef().doc(venueId);
+
 // TODO: return an array of all venueId's we find on the full way up to the sovereignVenue
 export const fetchSovereignVenueId = async (
   venueId: string,
@@ -29,11 +35,7 @@ export const fetchSovereignVenueId = async (
 export const fetchVenue = async (
   venueId: string
 ): Promise<WithId<AnyVenue> | undefined> => {
-  const venueDoc = await firebase
-    .firestore()
-    .collection("venues")
-    .doc(venueId)
-    .get();
+  const venueDoc = await getVenueRef(venueId).get();
 
   // TODO: Use proper data validation + firestore model converters to set this type rather than just forcing it with 'as'
   //  see .withConverter(soundConfigConverter) in fetchSoundConfigs in src/api/sounds.ts as an example
