1927b078bbc3019e0f5b887c12e01b37fdaebf81
mike-lvov <mike.lvov.soft@gmail.com>
2021-05-12 23:36:17 +0300

    Finishes the UI implementation

--- src/components/atoms/ChatMessage/ChatMessage.tsx
+++ src/components/atoms/ChatMessage/ChatMessage.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useMemo } from "react";
 import classNames from "classnames";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faReply } from "@fortawesome/free-solid-svg-icons";
@@ -6,6 +6,8 @@ import { faReply } from "@fortawesome/free-solid-svg-icons";
 import { MessageToDisplay } from "types/chat";
 
 import { WithId } from "utils/id";
+
+import { useShowHide } from "hooks/useShowHide";
 
 import { ChatMessageInfo } from "components/atoms/ChatMessageInfo";
 
@@ -34,17 +36,53 @@ export const ChatMessage: React.FC<ChatProps> = ({
     message,
   ]);
 
-  const containerStyles = classNames("chat-message", {
-    "chat-message--me": isMine,
+  const { isShown, toggle } = useShowHide();
+
+  const containerStyles = classNames("ChatMessage", {
+    "ChatMessage--me": isMine,
   });
+
+  const renderedReplies = useMemo(
+    () =>
+      replies?.map((reply) => (
+        <div key={reply.id} className="ChatMessage__reply">
+          {reply.text}
+          <ChatMessageInfo
+            message={reply}
+            deleteMessage={() => deleteMessage(reply.id)}
+          />
+        </div>
+      )),
+    [replies, deleteMessage]
+  );
+
+  const hasReplies = renderedReplies !== undefined;
+
+  const repliesCount = renderedReplies && renderedReplies.length;
+
+  const hasMultipleReplies = repliesCount && repliesCount > 1;
+
+  const replyText = hasMultipleReplies ? "replies" : "reply";
 
   return (
     <div className={containerStyles}>
-      <div className="chat-message__text">
-        {text}
-        <FontAwesomeIcon icon={faReply} size="sm" onClick={onReplyClick} />
+      <div className="ChatMessage__body">
+        <div className="ChatMessage__main">
+          <div className="ChatMessage__text">{text}</div>
 
-        {replies && replies}
+          <div className="ChatMessage__reply-icon">
+            <FontAwesomeIcon icon={faReply} size="sm" onClick={onReplyClick} />
+          </div>
+          {hasReplies && (
+            <div className="ChatMessage__simple-button" onClick={toggle}>
+              {!isShown ? `${repliesCount} ${replyText}` : `hide ${replyText}`}
+            </div>
+          )}
+        </div>
+
+        {isShown && (
+          <div className="ChatMessage__replies">{renderedReplies}</div>
+        )}
       </div>
       <ChatMessageInfo
         message={message}
