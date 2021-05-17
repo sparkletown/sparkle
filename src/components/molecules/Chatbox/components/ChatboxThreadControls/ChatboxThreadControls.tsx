bd4964d9a7d68efbeff25cc05192530f56d09252
mike-lvov <mike.lvov.soft@gmail.com>
2021-05-17 12:02:08 +0300

    Changes name

--- src/components/molecules/Chatbox/components/ChatboxThreadControls/ChatboxThreadControls.tsx
+++ src/components/molecules/Chatbox/components/ChatboxThreadControls/ChatboxThreadControls.tsx
@@ -5,12 +5,12 @@ import { TextButton } from "components/atoms/TextButton";
 import "./ChatboxThreadControls.scss";
 
 export interface ChatboxThreadControlsProps {
-  onThreadLeave: () => void;
+  closeThread: () => void;
   threadAuthor?: string;
 }
 
 export const ChatboxThreadControls: React.FC<ChatboxThreadControlsProps> = ({
-  onThreadLeave,
+  closeThread,
   threadAuthor,
 }) => {
   return (
@@ -18,7 +18,7 @@ export const ChatboxThreadControls: React.FC<ChatboxThreadControlsProps> = ({
       <span className="ChatboxThreadControls__thread-author">
         replying to <b>{threadAuthor}...</b>
       </span>
-      <TextButton label="Cancel" onClick={onThreadLeave} />
+      <TextButton label="Cancel" onClick={closeThread} />
     </div>
   );
 };
