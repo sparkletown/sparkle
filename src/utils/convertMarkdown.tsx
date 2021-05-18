15c46188485b738892fb552dd0fbdbee9b649a9e
Yaroslav Halchenko <debian@onerussian.com>
2021-05-18 10:46:33 -0400

    use remark-external-links to provide rel for external links

--- src/utils/convertMarkdown.tsx
+++ src/utils/convertMarkdown.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import glm from "remark-gfm";
 import emoji from "remark-emoji";
+import externalLinks from "remark-external-links";
 import sanitize from "rehype-sanitize";
 
 export const convertMarkdown = (text: string | undefined) => {
@@ -11,7 +12,7 @@ export const convertMarkdown = (text: string | undefined) => {
     return (
       <ReactMarkdown
         linkTarget={"_blank"}
-        remarkPlugins={[[glm], [emoji]]}
+        remarkPlugins={[[glm], [emoji], [externalLinks]]}
         rehypePlugins={[[sanitize]]}
         key={index}
       >
