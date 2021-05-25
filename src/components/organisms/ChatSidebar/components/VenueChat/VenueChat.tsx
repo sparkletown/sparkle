3eab7d36319de5649ad1efdb391a1120842b1b17
Yaroslav Halchenko <debian@onerussian.com>
2021-05-25 18:53:52 -0400

    BF: set onClick to selectVenueChat for venue-chat
    was trying to mimic what I observed for RecepientChat.tsx and PrivateChats.tsx .
    
    It fixes the issue of "c" shortcut not working when returning to VenueChat

--- src/components/organisms/ChatSidebar/components/VenueChat/VenueChat.tsx
+++ src/components/organisms/ChatSidebar/components/VenueChat/VenueChat.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Chatbox } from "components/molecules/Chatbox";
 
 import { useVenueChat } from "hooks/useVenueChat";
+import { useChatSidebarControls } from "hooks/chatSidebar";
 
 import "./VenueChat.scss";
 
@@ -13,9 +14,10 @@ export const VenueChat: React.FC = () => {
     messagesToDisplay,
     sendThreadReply,
   } = useVenueChat();
+  const { selectVenueChat } = useChatSidebarControls();
 
   return (
-    <div className="venue-chat">
+    <div className="venue-chat" onClick={() => selectVenueChat()}>
       <Chatbox
         messages={messagesToDisplay}
         sendMessage={sendMessage}
