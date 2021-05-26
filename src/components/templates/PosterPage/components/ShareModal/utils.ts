6d86720d1506504a9126377db11db39e11afa951
Oleksii Pohorelyi <oleksii.pohorelyi@gmail.com>
2021-05-26 12:07:57 +0300

    discussion_r638223214

--- src/components/templates/PosterPage/components/ShareModal/utils.ts
+++ src/components/templates/PosterPage/components/ShareModal/utils.ts
@@ -2,6 +2,10 @@ import { FACEBOOK_APP_ID } from "secrets";
 import { PosterPageVenue } from "types/venues";
 import { WithId } from "utils/id";
 
+const getCategoriesFromVeune = (venue: WithId<PosterPageVenue>) => {
+  return venue?.poster?.categories || [];
+};
+
 export const getTitleTextForSharing = (
   venue: WithId<PosterPageVenue>,
   url: string
@@ -11,8 +15,26 @@ export const getTitleTextForSharing = (
   return `Check out this OHBM Poster, ${venue.poster.title} by ${venue.poster.authorName} at ${url}`;
 };
 
-export const getFacebookHref = (url: string, text: string) =>
-  `https://www.facebook.com/sharer/sharer.php?app_id=${FACEBOOK_APP_ID}&u=${url}&quote=${text}&hashtag=#OHBM2021`;
+export const getFacebookHref = (
+  venue: WithId<PosterPageVenue>,
+  url: string,
+  text: string
+) => {
+  const categories = getCategoriesFromVeune(venue);
+  const hashtagsString = categories.reduce((result, hash, index, array) => {
+    const isLast = index === array.length - 1;
+    const separator = isLast ? "" : ", ";
+    return result + `#${hash}${separator}`;
+  }, "");
 
-export const getTwitterHref = (text: string) =>
-  `https://twitter.com/intent/tweet?text=${text}&hashtags=OHBM2021`;
+  return `https://www.facebook.com/sharer/sharer.php?app_id=${FACEBOOK_APP_ID}&u=${url}&quote=${text}&hashtag=${hashtagsString}`;
+};
+
+export const getTwitterHref = (
+  venue: WithId<PosterPageVenue>,
+  text: string
+) => {
+  const categories = getCategoriesFromVeune(venue);
+  const hashtagsString = categories.join(",");
+  return `https://twitter.com/intent/tweet?text=${text}&hashtags=${hashtagsString}`;
+};
