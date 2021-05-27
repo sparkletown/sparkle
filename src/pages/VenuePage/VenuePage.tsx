ea0453e694f40198da0cd0db9d38caa1f081eec7
oleksii-front <84011566+oleksii-front@users.noreply.github.com>
2021-05-27 23:28:59 +0300

    add ability to share a PosterPage on social media (#1396)
    Share a posterhall poster on social media
    
    Co-authored-by: mike-lvov <63194656+mike-lvov@users.noreply.github.com>
    Co-authored-by: Glenn 'devalias' Grant <glenn@devalias.net>
    Co-authored-by: mike-lvov <mike.lvov.soft@gmail.com>

--- src/pages/VenuePage/VenuePage.tsx
+++ src/pages/VenuePage/VenuePage.tsx
@@ -3,7 +3,7 @@ import { Redirect, useHistory } from "react-router-dom";
 import { useTitle } from "react-use";
 import { isBefore } from "date-fns";
 
-import { LOC_UPDATE_FREQ_MS } from "settings";
+import { LOC_UPDATE_FREQ_MS, PLATFORM_BRAND_NAME } from "settings";
 
 import { VenueTemplate } from "types/venues";
 
@@ -46,8 +46,6 @@ import { LoadingPage } from "components/molecules/LoadingPage/LoadingPage";
 import TemplateWrapper from "./TemplateWrapper";
 
 import { updateTheme } from "./helpers";
-
-import { PLATFORM_BRAND_NAME } from "settings";
 
 import "./VenuePage.scss";
 
