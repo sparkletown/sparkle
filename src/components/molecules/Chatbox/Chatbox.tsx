12738b9645a434753001e91301613f4d7944cb8e
Oleksii Pohorelyi <oleksii.pohorelyi@gmail.com>
2021-05-28 17:20:03 +0300

    discussion_r640229177

--- src/components/molecules/Chatbox/Chatbox.tsx
+++ src/components/molecules/Chatbox/Chatbox.tsx
@@ -94,7 +94,7 @@ export const Chatbox: React.FC<ChatboxProps> = ({
             setActiveOption={setActiveOption}
           />
         )}
-        {activeOption === ChatOptionType.question && (
+        {isQuestionOptions && (
           <ChatboxThreadControls
             text="asking a question"
             closeThread={closeQuestionOption}
