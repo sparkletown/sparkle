eb0e08fb4571b7d870e898bed4269c8d17995cb9
mike-lvov <63194656+mike-lvov@users.noreply.github.com>
2021-05-28 17:26:57 +0300

    add sorting + proper 'show more' button in ScreeningRoom (#1464)
    * Implements sorting + Proper Show more button

--- src/settings.ts
+++ src/settings.ts
@@ -588,6 +588,7 @@ export const POSTERPAGE_MAX_VIDEO_PARTICIPANTS = 10;
 export const SEARCH_DEBOUNCE_TIME = 200; // ms
 
 export const DEFAULT_DISPLAYED_POSTER_PREVIEW_COUNT = 48;
+export const DEFAULT_DISPLAYED_VIDEO_PREVIEW_COUNT = 12;
 
 export const USER_STATUSES = [UserStatus.available, UserStatus.busy];
 
