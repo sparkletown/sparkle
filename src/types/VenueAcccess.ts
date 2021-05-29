72f6a8b90762952afdef6845d8e8e6d96fcde67e
Glenn 'devalias' Grant <glenn@devalias.net>
2021-05-29 16:11:26 +1000

    add @debt comments to VenueAccessMode

--- src/types/VenueAcccess.ts
+++ src/types/VenueAcccess.ts
@@ -1,3 +1,5 @@
+// @debt Should we add some kind of 'none'/'disabled' entry to VenueAccessMode?
+// @debt Refactor VenueAccessMode to use lowercase, non-plural keys (but also figure how to backfill the database to account for this change first)
 export enum VenueAccessMode {
   Emails = "Emails",
   Codes = "Codes",
