2544486348c307fcfffc48bc2e8b328e207bf435
margulies <daniel.margulies@gmail.com>
2021-06-13 05:52:54 +0200

    adds date to schedule and minor fixes

--- src/components/templates/PartyMap/components/ScheduleItem/ScheduleItem.tsx
+++ src/components/templates/PartyMap/components/ScheduleItem/ScheduleItem.tsx
@@ -4,7 +4,7 @@ import { VenueEvent } from "types/venues";
 
 import { retainAttendance } from "store/actions/Attendance";
 
-import { formatUtcSeconds } from "utils/time";
+import { formatUtcSeconds, formatDate } from "utils/time";
 
 import { useDispatch } from "hooks/useDispatch";
 
@@ -30,6 +30,9 @@ export const ScheduleItem: React.FunctionComponent<PropsType> = ({
     <div className="schedule-item-container">
       <div className={`time-section ${isCurrentEvent ? "primary" : ""}`}>
         <div>
+          <b>{formatDate(event.start_utc_seconds)}</b>
+        </div>
+        <div>
           <b>{formatUtcSeconds(event.start_utc_seconds)}</b>
         </div>
         <div>
@@ -44,9 +47,11 @@ export const ScheduleItem: React.FunctionComponent<PropsType> = ({
             <div>
               <b>{event.name}</b>
             </div>
-            <div>
-              by <b>{event.host}</b>
-            </div>
+            {event.host && (
+              <div>
+                by <b>{event.host}</b>
+              </div>
+            )}
           </div>
           <div className="event-description">
             <RenderMarkdown text={event.description} />
