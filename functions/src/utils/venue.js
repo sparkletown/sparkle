a4da06468699003ec8a7bdc2fa38887b8482440a
Glenn 'devalias' Grant <glenn@devalias.net>
2021-06-15 18:38:19 +1000

    fix bug in checkIfValidVenueId when venueId isn't a string

--- functions/src/utils/venue.js
+++ functions/src/utils/venue.js
@@ -2,7 +2,11 @@ const getVenueId = (name) => {
   return name.replace(/\W/g, "").toLowerCase();
 };
 
-const checkIfValidVenueId = (venueId) => /[a-z0-9_]{1,250}/.test(venueId);
+const checkIfValidVenueId = (venueId) => {
+  if (typeof venueId !== "string") return false;
+
+  return /[a-z0-9_]{1,250}/.test(venueId);
+};
 
 exports.getVenueId = getVenueId;
 exports.checkIfValidVenueId = checkIfValidVenueId;
