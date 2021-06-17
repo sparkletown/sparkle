10bfd70e1645296bcd472e60ed0f728c0594a859
Glenn 'devalias' Grant <glenn@devalias.net>
2021-06-17 20:57:58 +1000

    fix TypeError: yup.lazy(...).default is not a function

--- functions/src/types/auth.js
+++ functions/src/types/auth.js
@@ -10,11 +10,9 @@ const AuthConfigSchema = yup.object().shape({
   revokePath: yup.string(),
   authorizeHost: yup.string(),
   authorizePath: yup.string(),
-  scope: yup
-    .lazy((val) =>
-      Array.isArray(val) ? yup.array().of(yup.string()) : yup.string()
-    )
-    .default(""),
+  scope: yup.lazy((val) =>
+    Array.isArray(val) ? yup.array().of(yup.string()) : yup.string().default("")
+  ),
 
   // I4A
   // @debt This is required for the current I4A implementation, but in future we should refactor this schema be more generic
