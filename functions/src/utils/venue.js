b1a0c754fa2a8deae8481d5e1458786e05b42428
Glenn 'devalias' Grant <glenn@devalias.net>
2021-06-18 20:17:29 +1000

    refactor checkIfValidVenueId to extract VENUE_ID_REGEX + create VenueIdSchema

--- functions/src/utils/venue.js
+++ functions/src/utils/venue.js
@@ -1,12 +1,13 @@
+const { VenueIdSchema } = require("../types/venue");
+
+const VENUE_ID_REGEX = /[a-z0-9_]{1,250}/;
+
 const getVenueId = (name) => {
   return name.replace(/\W/g, "").toLowerCase();
 };
 
-const checkIfValidVenueId = (venueId) => {
-  if (typeof venueId !== "string") return false;
+const checkIfValidVenueId = (venueId) => VenueIdSchema.isValidSync(venueId);
 
-  return /[a-z0-9_]{1,250}/.test(venueId);
-};
-
+exports.VENUE_ID_REGEX = VENUE_ID_REGEX;
 exports.getVenueId = getVenueId;
 exports.checkIfValidVenueId = checkIfValidVenueId;
