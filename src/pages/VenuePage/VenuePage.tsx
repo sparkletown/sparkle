8c3c6074a6106a16b03cb0f9980596f8d21123d6
mike-lvov <mike.lvov.soft@gmail.com>
2021-06-22 01:23:49 +0300

    Moves the wrapper

--- src/pages/VenuePage/VenuePage.tsx
+++ src/pages/VenuePage/VenuePage.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy, useEffect } from "react";
+import React, { Suspense, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import { useTitle } from "react-use";
 
@@ -24,7 +24,6 @@ import {
 } from "utils/userLocation";
 import { venueEntranceUrl } from "utils/url";
 import { showZendeskWidget } from "utils/zendesk";
-import { tracePromise } from "utils/performance";
 import { isCompleteProfile, updateProfileEnteredVenueIds } from "utils/profile";
 import { isTruthy } from "utils/types";
 import { hasEventFinished, isEventStartingSoon } from "utils/event";
@@ -49,21 +48,9 @@ import { updateTheme } from "./helpers";
 
 import "./VenuePage.scss";
 
-const Login = lazy(() =>
-  tracePromise("VenuePage::lazy-import::Login", () =>
-    import("pages/Account/Login").then(({ Login }) => ({
-      default: Login,
-    }))
-  )
-);
-
-const TemplateWrapper = lazy(() =>
-  tracePromise("VenuePage::lazy-import::TemplateWrapper", () =>
-    import("./TemplateWrapper").then(({ TemplateWrapper }) => ({
-      default: TemplateWrapper,
-    }))
-  )
-);
+import Login from "pages/Account/Login";
+import { useRelatedVenues } from "hooks/useRelatedVenues";
+import { TemplateWrapper } from "./TemplateWrapper";
 
 // @debt Refactor this constant into settings, or types/templates, or similar?
 const hasPaidEvents = (template: VenueTemplate) => {
@@ -101,6 +88,10 @@ export const VenuePage: React.FC = () => {
   const venueTemplate = venue?.template;
 
   const event = currentEvent?.[0];
+
+  const { isLoading } = useRelatedVenues({
+    currentVenueId: venueId,
+  });
 
   useEffect(() => {
     if (!venue) return;
@@ -248,7 +239,7 @@ export const VenuePage: React.FC = () => {
     }
   }
 
-  if (!user) {
+  if (!user || isLoading) {
     return <LoadingPage />;
   }
 
