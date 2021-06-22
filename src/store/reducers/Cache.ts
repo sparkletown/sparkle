521af0c437f0338f6b3f135e71bcc17e738c1fca
Soichi Hayashi <soichih@gmail.com>
2021-06-22 18:35:59 -0400

    removed events cache

--- src/store/reducers/Cache.ts
+++ src/store/reducers/Cache.ts
@@ -1,28 +1,17 @@
 //import { CacheActions } from "../actions/Cache";
-import { VenueEvent } from "types/venues";
-import { WithId, WithVenueId } from "utils/id";
+import { WithId } from "utils/id";
 
 import { User } from "types/User";
 
 import firebase from "firebase/app";
 //import "firebase/firestore";
 
-import { SCHEDULE_LOAD_FROM_GS } from "settings";
-
 interface cacheState {
-  events: Promise<WithVenueId<WithId<VenueEvent>>[]>;
   usersRecord: Record<string, WithId<User>>;
   usersArray: WithId<User>[];
 }
 
 const initialState: cacheState = {
-  events: new Promise(async (resolve) => {
-    //const storage = firebase.storage();
-    //const url = await storage.ref().child(SCHEDULE_LOAD_FROM_GS).getDownloadURL();
-    fetch(SCHEDULE_LOAD_FROM_GS)
-      .then((res) => res.json())
-      .then(resolve);
-  }),
   usersRecord: {},
   usersArray: [],
 };
