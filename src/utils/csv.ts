4d738f57062940da79d639db77ee105a61f36532
ermiaSparkle <ermia@sparkle.space>
2021-06-22 18:23:27 +0430

    delete comments

--- src/utils/csv.ts
+++ src/utils/csv.ts
@@ -4,12 +4,6 @@ import neatCsv from "neat-csv";
 export const getCSVRows = async (filePath: string) => {
   let results: Array<Object> = [];
   let file = await fs.createReadStream(filePath);
-  // console.log(await neatCsv(file));
   results = await neatCsv(file);
   return results;
-  // .pipe(csv())
-  // .on('data', (data) => results.push(data))
-  // .on('end', () => {
-  //   return results
-  // });
 };
