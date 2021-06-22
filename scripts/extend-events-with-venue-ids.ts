cb77a75b403c48f515b911e148a41b36e1329b6d
Viktoryia <viktoryia.work@gmail.com>
2021-06-22 18:51:31 +0300

    Get rid of sovereignVenueId additions

--- scripts/extend-events-with-venue-ids.ts
+++ scripts/extend-events-with-venue-ids.ts
@@ -25,48 +25,6 @@ initFirebaseAdminApp(projectId, {
     : undefined,
 });
 
-export interface FetchSovereignVenueOptions {
-  previouslyCheckedVenueIds?: readonly string[];
-  maxDepth?: number;
-}
-
-export interface FetchSovereignVenueReturn {
-  sovereignVenue: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>;
-  checkedVenueIds: readonly string[];
-}
-
-const fetchSovereignVenue = async (
-  venueId: string,
-  venues: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[],
-  options?: FetchSovereignVenueOptions
-): Promise<FetchSovereignVenueReturn> => {
-  const { previouslyCheckedVenueIds = [], maxDepth } = options ?? {};
-
-  const venue = venues.find((venue) => venue.id === venueId);
-
-  if (!venue) throw new Error(`The '${venueId}' venue doesn't exist`);
-
-  if (!venue.data().parentId)
-    return {
-      sovereignVenue: venue,
-      checkedVenueIds: previouslyCheckedVenueIds,
-    };
-
-  if (previouslyCheckedVenueIds.includes(venueId))
-    throw new Error(
-      `Circular reference detected. '${venueId}' has already been checked`
-    );
-
-  if (maxDepth && maxDepth <= 0)
-    throw new Error("Maximum depth reached before finding the sovereignVenue.");
-
-  return fetchSovereignVenue(venue.data().parentId, venues, {
-    ...options,
-    previouslyCheckedVenueIds: [...previouslyCheckedVenueIds, venueId],
-    maxDepth: maxDepth ? maxDepth - 1 : undefined,
-  });
-};
-
 (async () => {
   // // @debt This function will currently load all venues in firebase into memory.. not very efficient
   const venuesCollection = admin.firestore().collection("venues");
@@ -74,8 +32,6 @@ const fetchSovereignVenue = async (
 
   venueDocs.forEach(async (doc) => {
     const venueId = doc.id;
-    const { sovereignVenue } = await fetchSovereignVenue(venueId, venueDocs);
-    const sovereignVenueId = sovereignVenue.id;
 
     const events = await venuesCollection
       .doc(venueId)
@@ -85,7 +41,6 @@ const fetchSovereignVenue = async (
     events.forEach((eventDoc) => {
       const venueData = {
         venueId,
-        sovereignVenueId,
       };
 
       console.log(`updating event "${eventDoc.data().name}"`, venueData);
