3f6e0a52b9367436bf2734966f68363cd0afc60d
Malaika <malaika.aiyar@gmail.com>
2021-06-25 22:11:27 +0100

    fix registration page

--- src/pages/Account/Login.tsx
+++ src/pages/Account/Login.tsx
@@ -93,7 +93,7 @@ export const Login: React.FC<LoginProps> = ({
       <div className="auth-form-container">
         {hasAlternativeLogins && (
           <div className="Login__login-box">
-            <span>Quick log in with</span>
+            <span>Quick log in with OKTA</span>
 
             <div className="Login__alternative-logins">
               {hasCustomAuthConnect && (
