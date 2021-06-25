ba44c6a8a12f7838e8403e4fdff18df19ac86854
Goran Peoski <79229621+goran-peoski-work@users.noreply.github.com>
2021-06-25 14:11:28 +0200

    Create ButtonNG

---
+++ src/components/atoms/ButtonNG/ButtonNG/ButtonNG.tsx
@@ -0,0 +1,110 @@
+import React, { CSSProperties } from "react";
+import { Link } from "react-router-dom";
+import classNames from "classnames";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons/faCircleNotch";
+
+import "./ButtonNG.scss";
+
+export type ButtonGradientType =
+  | "gradient"
+  | "admin-gradient"
+  | "danger-gradient";
+
+export type ButtonType = "button" | "reset" | "submit";
+export type ButtonVariant = "primary" | "secondary" | "white" | "dark";
+export type ButtonIconSize = "1x" | "2x" | "3x";
+
+export interface ButtonProps {
+  className?: string;
+  style?: CSSProperties;
+  disabled?: boolean;
+  onClick?: () => void;
+  isLink?: boolean;
+  linkTo?: string;
+  newTab?: boolean;
+  loading?: boolean;
+  gradient?: ButtonGradientType;
+  type?: ButtonType;
+  variant?: ButtonVariant;
+  iconOnly?: boolean;
+  iconName?: IconProp;
+  iconSize?: ButtonIconSize;
+}
+
+export const ButtonNG: React.FC<ButtonProps> = ({
+  children,
+  className = "",
+  style,
+  loading,
+  onClick,
+  type = "button",
+  gradient = "",
+  isLink = false,
+  linkTo = "",
+  disabled = false,
+  newTab = false,
+  variant = "",
+  iconOnly = false,
+  iconName,
+  iconSize = "1x",
+}) => {
+  const cn = classNames({
+    "ButtonNG ButtonNG__link": isLink,
+    "ButtonNG ButtonNG__button": !isLink,
+    "ButtonNG--disabled": disabled,
+    "ButtonNG--enabled": !disabled,
+    "ButtonNG--loading": loading,
+    [`ButtonNG--icon-only ButtonNG--${iconSize}`]: iconOnly,
+    [`ButtonNG--${gradient}`]: gradient && !disabled,
+    [`ButtonNG--${variant}`]: variant && !disabled,
+  });
+
+  if (loading) {
+    return (
+      <button className={cn} style={style} type={type}>
+        <FontAwesomeIcon
+          icon={faCircleNotch}
+          spin
+          size={iconSize}
+          className="ButtonNG__icon"
+        />
+      </button>
+    );
+  }
+
+  if (isLink) {
+    return (
+      <Link
+        className={cn}
+        style={style}
+        to={disabled ? "#" : linkTo}
+        target={newTab && !disabled ? "_blank" : undefined}
+        rel={newTab && !disabled ? "noopener noreferer" : undefined}
+      >
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button
+      className={cn}
+      style={style}
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {iconName && (
+        <FontAwesomeIcon
+          icon={iconName}
+          size={iconSize}
+          className="ButtonNG__icon"
+        />
+      )}
+      {children}
+    </button>
+  );
+};
