469f30bfb9c1809876ea4660f176de38ebfaa663
Pearl Pospiech <pearl@sparkle.space>
2021-06-25 07:20:02 -0700

    Allow camera and microphone for embeddable (v2) (#1643)
    * Allow advanced iframe permissions for embeddable
    
    * Update src/settings.ts
    
    Co-authored-by: Glenn 'devalias' Grant <glenn@devalias.net>

--- src/settings.ts
+++ src/settings.ts
@@ -157,6 +157,7 @@ export const VALID_URL_PROTOCOLS = ["http:", "https:"];
 
 export const IFRAME_ALLOW =
   "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen";
+export const IFRAME_ALLOW_ADVANCED = `${IFRAME_ALLOW} camera; microphone`;
 
 export const ENABLE_SUSPECTED_LOCATION = false;
 export const ENABLE_PLAYA_ADDRESS = false;
