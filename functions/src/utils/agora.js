4368e63439c0089b38a96103ec389485c338fde0
Glenn 'devalias' Grant <glenn@devalias.net>
2021-06-27 22:52:05 +1000

    remove unused generateAgoraTokenForUid

--- functions/src/utils/agora.js
+++ functions/src/utils/agora.js
@@ -16,29 +16,6 @@ const getExpirationTime = () => {
   return currentTimestamp + expirationTimeInSeconds;
 };
 
-/**
- * @deprecated use generateAgoraTokenForAccount where possible so that we can use strings as account, rather than a numeric uid
- */
-const generateAgoraTokenForUid = ({ channelName, uid, role }) => {
-  // @debt we should enforce a stricter security requirement on channelName. Maybe use UUIDs?
-  if (typeof channelName !== "string" && channelName.length <= 0)
-    throw new Error("channelName must be a string");
-
-  if (typeof uid !== "number") throw new Error("uid must be a number");
-
-  if (![RtcRole.PUBLISHER, RtcRole.SUBSCRIBER].includes(role))
-    throw new Error("role must be a valid value from the RtcRole enum");
-
-  return RtcTokenBuilder.buildTokenWithUid(
-    appId,
-    appCertificate,
-    channelName,
-    uid,
-    role,
-    getExpirationTime()
-  );
-};
-
 const generateAgoraTokenForAccount = ({ channelName, account, role }) => {
   // @debt we should enforce a stricter security requirement on channelName. Maybe use UUIDs?
   if (typeof channelName !== "string" && channelName.length <= 0)
@@ -60,12 +37,5 @@ const generateAgoraTokenForAccount = ({ channelName, account, role }) => {
 };
 
 exports.getExpirationTime = getExpirationTime;
-
-/**
- * @deprecated use generateAgoraTokenForAccount where possible so that we can use strings as account, rather than a numeric uid
- */
-exports.generateAgoraTokenForUid = generateAgoraTokenForUid;
-
 exports.generateAgoraTokenForAccount = generateAgoraTokenForAccount;
-
 exports.RtcRole = RtcRole;
