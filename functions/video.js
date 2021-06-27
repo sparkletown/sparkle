e70915920feddb06905e87d590e01f7298aa1631
Glenn 'devalias' Grant <glenn@devalias.net>
2021-06-27 21:38:52 +1000

    refactor getTwilioToken to use assertValidAuth

--- functions/video.js
+++ functions/video.js
@@ -1,17 +1,10 @@
 const functions = require("firebase-functions");
 
+const { assertValidAuth } = require("./src/utils/assert");
 const { twilioVideoToken } = require("./src/utils/twilio");
 
-const PROJECT_ID = functions.config().project.id;
-
 exports.getTwilioToken = functions.https.onCall((data, context) => {
-  if (!context.auth || !context.auth.token) {
-    throw new functions.https.HttpsError("unauthenticated", "Please log in");
-  }
-
-  if (context.auth.token.aud !== PROJECT_ID) {
-    throw new functions.https.HttpsError("permission-denied", "Token invalid");
-  }
+  assertValidAuth(context);
 
   if (data && data.identity && data.room) {
     const token = twilioVideoToken(data.identity, data.room);
