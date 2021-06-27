d6e7899f3543562ddba00fc1741d4683ae84b15f
Glenn 'devalias' Grant <glenn@devalias.net>
2021-06-27 21:41:49 +1000

    refactor getTwilioToken to check guard statement at the start of the func

--- functions/video.js
+++ functions/video.js
@@ -1,4 +1,5 @@
 const functions = require("firebase-functions");
+const { HttpsError } = require("firebase-functions/lib/providers/https");
 
 const { assertValidAuth } = require("./src/utils/assert");
 const { twilioVideoToken } = require("./src/utils/twilio");
@@ -6,15 +7,13 @@ const { twilioVideoToken } = require("./src/utils/twilio");
 exports.getTwilioToken = functions.https.onCall((data, context) => {
   assertValidAuth(context);
 
-  if (data && data.identity && data.room) {
-    const token = twilioVideoToken(data.identity, data.room);
-    return {
-      token: token.toJwt(),
-    };
+  if (!data || !data.identity || !data.room) {
+    throw new HttpsError("invalid-argument", "identity or room data missing");
   }
 
-  throw new functions.https.HttpsError(
-    "invalid-argument",
-    "identity or room data missing"
-  );
+  const token = twilioVideoToken(data.identity, data.room);
+
+  return {
+    token: token.toJwt(),
+  };
 });
