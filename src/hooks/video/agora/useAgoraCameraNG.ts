22c55876c759eded86087be481fc07ba63da526a
Glenn 'devalias' Grant <glenn@devalias.net>
2021-06-29 06:36:17 +1000

    add new useAgoraCameraNG hook

---
+++ src/hooks/video/agora/useAgoraCameraNG.ts
@@ -0,0 +1,81 @@
+import { useCallback } from "react";
+import AgoraRTC, {
+  IAgoraRTCClient,
+  ILocalVideoTrack,
+  UID,
+} from "agora-rtc-sdk-ng";
+
+import { ReactHook } from "types/utility";
+
+import { useAgoraClient } from "./useAgoraClient";
+
+export interface UseAgoraCameraNGProps {
+  channelName?: string;
+}
+
+export interface UseAgoraCameraNGReturn {
+  localCameraTrack?: ILocalVideoTrack;
+
+  isCameraEnabled: boolean;
+  isMicrophoneEnabled: boolean;
+
+  toggleCamera(): void;
+  toggleMicrophone(): void;
+
+  joinChannel: () => Promise<UID | undefined>;
+  leaveChannel(): Promise<void>;
+
+  client: IAgoraRTCClient;
+}
+
+export const useAgoraCameraNG: ReactHook<
+  UseAgoraCameraNGProps,
+  UseAgoraCameraNGReturn
+> = ({ channelName }) => {
+  const {
+    client,
+    localVideoTrack,
+    isVideoTrackEnabled,
+    isAudioTrackEnabled,
+    toggleVideoTrack,
+    toggleAudioTrack,
+    setLocalVideoTrack,
+    setLocalAudioTrack,
+    joinChannel,
+    leaveChannel,
+  } = useAgoraClient({ channelName });
+
+  const joinChannelWithCameraAndMic = useCallback(async () => {
+    const channelId = await joinChannel();
+
+    // const cameraTrack = await AgoraRTC.createCameraVideoTrack();
+    // const microphoneTrack = await AgoraRTC.createMicrophoneAudioTrack();
+
+    const [
+      microphoneTrack,
+      cameraTrack,
+    ] = await AgoraRTC.createMicrophoneAndCameraTracks();
+
+    setLocalVideoTrack(cameraTrack);
+    setLocalAudioTrack(microphoneTrack);
+
+    await client.publish([microphoneTrack, cameraTrack]);
+
+    return channelId;
+  }, [client, joinChannel, setLocalAudioTrack, setLocalVideoTrack]);
+
+  return {
+    client,
+
+    localCameraTrack: localVideoTrack,
+
+    isCameraEnabled: isVideoTrackEnabled,
+    toggleCamera: toggleVideoTrack,
+
+    isMicrophoneEnabled: isAudioTrackEnabled,
+    toggleMicrophone: toggleAudioTrack,
+
+    joinChannel: joinChannelWithCameraAndMic,
+    leaveChannel,
+  };
+};
