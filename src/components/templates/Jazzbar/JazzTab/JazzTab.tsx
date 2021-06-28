73398cbea6d1449356de998698b8f26d0068f6f1
Oleksii Petrov <oleksii.petrov@altexsoft.com>
2021-06-28 09:46:25 +0300

    replaced back to href btn label with simple back

--- src/components/templates/Jazzbar/JazzTab/JazzTab.tsx
+++ src/components/templates/Jazzbar/JazzTab/JazzTab.tsx
@@ -152,7 +152,7 @@ const Jazz: React.FC<JazzProps> = ({ setUserList, venue }) => {
       {!seatedAtTable && parentVenueId && parentVenue && (
         <div className="back-map-btn" onClick={backToParentVenue}>
           <div className="back-icon" />
-          <span className="back-link">Back to {parentVenue.name}</span>
+          <span className="back-link">Back</span>
         </div>
       )}
 
