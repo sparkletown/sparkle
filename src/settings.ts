32af9812f10da0deb321dbfe42bc0467cc1b8f1a
Pearl Pospiech <pearl@sparkle.space>
2021-06-29 15:57:03 -0700

    Added semicolon after both ALLOWs

--- src/settings.ts
+++ src/settings.ts
@@ -156,8 +156,8 @@ export const ACCEPTED_IMAGE_TYPES =
 export const VALID_URL_PROTOCOLS = ["http:", "https:"];
 
 export const IFRAME_ALLOW =
-  "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen";
-export const IFRAME_ALLOW_ADVANCED = `${IFRAME_ALLOW} camera; microphone`;
+  "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen;";
+export const IFRAME_ALLOW_ADVANCED = `${IFRAME_ALLOW} camera; microphone;`;
 
 export const ENABLE_SUSPECTED_LOCATION = false;
 export const ENABLE_PLAYA_ADDRESS = false;
