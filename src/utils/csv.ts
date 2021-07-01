71fbe64d1f3591d78979411e27e72e5165340a4b
ermiaSparkle <ermia@sparkle.space>
2021-07-01 15:07:55 +0430

    fix csv.ts comments in review+fix rawCSV data type

--- src/utils/csv.ts
+++ src/utils/csv.ts
@@ -1,9 +1,13 @@
-import * as fs from "fs";
+import { createReadStream } from "fs";
 import neatCsv from "neat-csv";
 
+type RawCSVData = {
+  [key: string]: string;
+};
+
 export const getCSVRows = async (filePath: string) => {
-  let results: Array<Object> = [];
-  let file = await fs.createReadStream(filePath);
+  let results: RawCSVData[] = [];
+  const file = await createReadStream(filePath);
   results = await neatCsv(file);
   return results;
 };
