0ca67fdfd6ab5cdf1a1ab2b304400c4a094ade93
Glenn 'devalias' Grant <glenn@devalias.net>
2021-07-02 18:58:56 +1000

    split useRecentVenueUsers into it's own file

--- src/hooks/users/index.ts
+++ src/hooks/users/index.ts
@@ -1,8 +1,3 @@
-import { useConnectCurrentVenueNG } from "hooks/useConnectCurrentVenueNG";
-import { useVenueId } from "hooks/useVenueId";
-
-import { useRecentLocationUsers } from "./useRecentLocationUsers";
-
 export { useConnectWorldUsers } from "./useConnectWorldUsers";
 export { useWorldUsers } from "./useWorldUsers";
 export {
@@ -11,19 +6,4 @@ export {
 } from "./useWorldUsersById";
 export { useRecentWorldUsers } from "./useRecentWorldUsers";
 export { useRecentLocationUsers } from "./useRecentLocationUsers";
-
-export const useRecentVenueUsers = () => {
-  const venueId = useVenueId();
-
-  const { currentVenue } = useConnectCurrentVenueNG(venueId);
-
-  const {
-    recentLocationUsers,
-    isRecentLocationUsersLoaded,
-  } = useRecentLocationUsers(currentVenue?.name);
-
-  return {
-    recentVenueUsers: recentLocationUsers,
-    isRecentVenueUsersLoaded: isRecentLocationUsersLoaded,
-  };
-};
+export { useRecentVenueUsers } from "./useRecentVenueUsers";
