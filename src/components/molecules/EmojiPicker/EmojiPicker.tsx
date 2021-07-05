a80c2ba58e360c7e32bdf3c626047bc69b0081e8
Glenn 'devalias' Grant <glenn@devalias.net>
2021-07-05 19:06:02 +1000

    refactor EmojiPicker to use React.lazy / Suspense for lazy loading / bundle chunk splitting

--- src/components/molecules/EmojiPicker/EmojiPicker.tsx
+++ src/components/molecules/EmojiPicker/EmojiPicker.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 
-import { EmojiData, Picker } from "emoji-mart";
+import { EmojiData } from "emoji-mart";
+
+import { tracePromise } from "utils/performance";
+
+import { Loading } from "components/molecules/Loading";
 
 import "emoji-mart/css/emoji-mart.css";
+
+const Picker = lazy(() =>
+  tracePromise("EmojiPicker::lazy-import::emoji-mart", () =>
+    import("emoji-mart").then(({ Picker }) => ({
+      default: Picker,
+    }))
+  )
+);
 
 export interface EmojiPickerProps {
   onSelect: (emoji: EmojiData) => void;
 }
 
-export const EmojiPicker: React.FC<EmojiPickerProps> = ({ onSelect }) => {
-  return <Picker theme={"dark"} onSelect={onSelect} native />;
-};
+export const EmojiPicker: React.FC<EmojiPickerProps> = ({ onSelect }) => (
+  <Suspense fallback={<Loading />}>
+    <Picker theme="dark" onSelect={onSelect} native />
+  </Suspense>
+);
