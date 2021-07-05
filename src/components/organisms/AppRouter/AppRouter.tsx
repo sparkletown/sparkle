398f74783e5c200ea52d4d6be7c5f571bbe2d482
Glenn 'devalias' Grant <glenn@devalias.net>
2021-07-05 11:51:46 +1000

    refactor /admin/* routes from AppRouter -> AdminV1Subrouter

--- src/components/organisms/AppRouter/AppRouter.tsx
+++ src/components/organisms/AppRouter/AppRouter.tsx
@@ -7,12 +7,12 @@ import {
 } from "react-router-dom";
 
 import { LoginWithCustomToken } from "pages/Account/LoginWithCustomToken";
-import Admin from "pages/Admin/Admin";
+
 import Admin_v2 from "pages/Admin/Admin_v2";
 
 import { VenueLandingPage } from "pages/VenueLandingPage";
 import { VenueEntrancePage } from "pages/VenueEntrancePage";
-import { VenueWizard } from "pages/Admin/Venue/VenueWizard";
+
 import VenueWizard_v2 from "pages/Admin/Venue/VenueWizard/VenueWizard";
 
 import { VersionPage } from "pages/VersionPage/VersionPage";
@@ -20,12 +20,13 @@ import { DEFAULT_REDIRECT_URL, SPARKLEVERSE_HOMEPAGE_URL } from "settings";
 
 import VenuePage from "pages/VenuePage";
 import { venueLandingUrl } from "utils/url";
-import { RoomsForm } from "pages/Admin/Venue/Rooms/RoomsForm";
+
 import { VenueAdminPage } from "pages/Admin/Venue/VenueAdminPage";
 import { AdminAdvancedSettings } from "pages/AdminAdvancedSettings";
 import { AdminVenueView } from "components/organisms/AdminVenueView";
 
 import { AccountSubrouter } from "./AccountSubrouter";
+import { AdminV1Subrouter } from "./AdminV1Subrouter";
 import { EnterSubrouter } from "./EnterSubrouter";
 
 const AppRouter: React.FC = () => {
@@ -38,16 +39,14 @@ const AppRouter: React.FC = () => {
         />
         <Route path="/enter" component={EnterSubrouter} />
         <Route path="/account" component={AccountSubrouter} />
+        <Route path="/admin" component={AdminV1Subrouter} />
+
         <Route
           path="/login/:venueId/:customToken"
           component={LoginWithCustomToken}
         />
         {/* @debt The /login route doesn't work since we added non-defaulted props to the Login component */}
         {/*<Route path="/login" component={Login} />*/}
-        <Route path="/admin/venue/rooms/:venueId" component={RoomsForm} />
-
-        <Route path="/admin/venue/creation" component={VenueWizard} />
-        <Route path="/admin/venue/edit/:venueId" component={VenueWizard} />
 
         <Route path="/admin-ng/venue/:venueId?" component={AdminVenueView} />
         <Route
@@ -58,9 +57,6 @@ const AppRouter: React.FC = () => {
         <Route path="/admin-ng/venue/creation" component={VenueWizard_v2} />
         <Route path="/admin-ng/edit/:venueId" component={VenueWizard_v2} />
 
-        <Route path="/admin/:venueId" component={Admin} />
-
-        <Route path="/admin" component={Admin} />
         <Route path="/admin-ng" component={Admin_v2} />
 
         <Route path="/v/:venueId" component={VenueLandingPage} />
