6c1098d4fbf60cce9b8c671cd5447e036f83a604
Glenn 'devalias' Grant <glenn@devalias.net>
2021-07-05 11:42:00 +1000

    refactor /enter/* routes from AppRouter -> EnterSubrouter

--- src/components/organisms/AppRouter/AppRouter.tsx
+++ src/components/organisms/AppRouter/AppRouter.tsx
@@ -6,14 +6,6 @@ import {
   Redirect,
 } from "react-router-dom";
 
-// import { SplashPage } from "pages/Account/SplashPage";
-import Step1 from "pages/Account/Step1";
-import Step2 from "pages/Account/Step2";
-import Step3 from "pages/Account/Step3";
-import Step4 from "pages/Account/Step4";
-import Step5 from "pages/Account/Step5";
-import Step6a from "pages/Account/Step6a";
-import Step6b from "pages/Account/Step6b";
 import Profile from "pages/Account/Profile";
 import Questions from "pages/Account/Questions";
 import CodeOfConduct from "pages/Account/CodeOfConduct";
@@ -36,6 +28,8 @@ import { VenueAdminPage } from "pages/Admin/Venue/VenueAdminPage";
 import { AdminAdvancedSettings } from "pages/AdminAdvancedSettings";
 import { AdminVenueView } from "components/organisms/AdminVenueView";
 
+import { EnterSubrouter } from "./EnterSubrouter";
+
 const AppRouter: React.FC = () => {
   return (
     <Router basename="/">
@@ -44,15 +38,7 @@ const AppRouter: React.FC = () => {
           path="/SparkleVerse"
           component={() => <Redirect to={SPARKLEVERSE_HOMEPAGE_URL} />}
         />
-        <Route path="/enter/step1" component={Step1} />
-        <Route path="/enter/step2" component={Step2} />
-        <Route path="/enter/step3" component={Step3} />
-        <Route path="/enter/step4" component={Step4} />
-        <Route path="/enter/step5" component={Step5} />
-        <Route path="/enter/step6a" component={Step6a} />
-        <Route path="/enter/step6b" component={Step6b} />
-        {/* @debt This appears to be a legacy route related to the old Playa template. Likely to be removed in future. */}
-        {/*<Route path="/enter" component={SplashPage} />*/}
+        <Route path="/enter" component={EnterSubrouter} />
         <Route path="/account/profile" component={Profile} />
         <Route path="/account/questions" component={Questions} />
         <Route path="/account/code-of-conduct" component={CodeOfConduct} />
