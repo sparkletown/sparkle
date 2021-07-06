c7940a96d1efc0796a839856b15545104a474f6d
Glenn 'devalias' Grant <glenn@devalias.net>
2021-07-06 22:51:40 +1000

    lazy load NavBar to create bundle chunk split

--- src/components/organisms/WithNavigationBar/WithNavigationBar.tsx
+++ src/components/organisms/WithNavigationBar/WithNavigationBar.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
+
+import { tracePromise } from "utils/performance";
 
 import { useVenueId } from "hooks/useVenueId";
 import { RelatedVenuesProvider } from "hooks/useRelatedVenues";
 
-import { NavBar } from "components/molecules/NavBar";
 import { Footer } from "components/molecules/Footer";
+import { Loading } from "components/molecules/Loading";
 
 import "./WithNavigationBar.scss";
+
+const NavBar = lazy(() =>
+  tracePromise("WithNavigationBar::lazy-import::NavBar", () =>
+    import("components/molecules/NavBar").then(({ NavBar }) => ({
+      default: NavBar,
+    }))
+  )
+);
 
 export interface WithNavigationBarProps {
   redirectionUrl?: string;
@@ -32,7 +42,12 @@ export const WithNavigationBar: React.FC<WithNavigationBarProps> = ({
        *    works on the user side of things.
        */}
       <RelatedVenuesProvider venueId={venueId}>
-        <NavBar redirectionUrl={redirectionUrl} hasBackButton={hasBackButton} />
+        <Suspense fallback={<Loading />}>
+          <NavBar
+            redirectionUrl={redirectionUrl}
+            hasBackButton={hasBackButton}
+          />
+        </Suspense>
       </RelatedVenuesProvider>
 
       <div className={`navbar-margin ${fullscreen ? "fullscreen" : ""}`}>
