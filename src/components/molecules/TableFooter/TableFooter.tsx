216ac82ee4d2437397c642821eb1931b6c73ee8f
Glenn 'devalias' Grant <glenn@devalias.net>
2021-07-11 01:01:56 +1000

    extract useCallback'd toggleVideoFocused in TableFooter component

--- src/components/molecules/TableFooter/TableFooter.tsx
+++ src/components/molecules/TableFooter/TableFooter.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import { Toggler } from "components/atoms/Toggler";
 
@@ -7,24 +7,27 @@ export interface TableFooterProps {
   setIsVideoFocused: (val: boolean) => void;
 }
 
-const TableFooter: React.FC<TableFooterProps> = ({
+// @debt this doesn't appear to be used anywhere, can we remove it?
+export const TableFooter: React.FC<TableFooterProps> = ({
   isVideoFocused,
   setIsVideoFocused,
-}) => (
-  <div className="table-footer">
-    <div className="actions">
-      <div className="action">
-        <div className="focus">Focus on:</div>
-        <div className="focus-option">Jazz</div>
-        {/* @debt pass the header into Toggler's 'label' prop instead of being external like this */}
-        <Toggler
-          toggled={!isVideoFocused}
-          onChange={() => setIsVideoFocused(!isVideoFocused)}
-        />
-        <div className="focus-option">Friends</div>
+}) => {
+  const toggleVideoFocused = useCallback(
+    () => setIsVideoFocused(!isVideoFocused),
+    [isVideoFocused, setIsVideoFocused]
+  );
+
+  return (
+    <div className="table-footer">
+      <div className="actions">
+        <div className="action">
+          <div className="focus">Focus on:</div>
+          <div className="focus-option">Jazz</div>
+          {/* @debt pass the header into Toggler's 'label' prop instead of being external like this */}
+          <Toggler toggled={!isVideoFocused} onChange={toggleVideoFocused} />
+          <div className="focus-option">Friends</div>
+        </div>
       </div>
     </div>
-  </div>
-);
-
-export default TableFooter;
+  );
+};
