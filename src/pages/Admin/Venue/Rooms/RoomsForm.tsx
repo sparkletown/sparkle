5fd45f908391d151925472e3e49442a0b94f1f82
Glenn 'devalias' Grant <glenn@devalias.net>
2021-07-11 00:33:15 +1000

    prettier

--- src/pages/Admin/Venue/Rooms/RoomsForm.tsx
+++ src/pages/Admin/Venue/Rooms/RoomsForm.tsx
@@ -332,7 +332,11 @@ const RoomInnerForm: React.FC<RoomInnerFormProps> = (props) => {
                   <div className="toggle-room">
                     {/* @debt pass the header into Toggler's 'label' prop instead of being external like this*/}
                     <div className="input-title">Is label hidden?</div>
-                    <Toggler name="isLabeled" forwardedRef={register} disabled={disable} />
+                    <Toggler
+                      name="isLabeled"
+                      forwardedRef={register}
+                      disabled={disable}
+                    />
                   </div>
                 </div>
                 <div className="page-container-left-bottombar">
