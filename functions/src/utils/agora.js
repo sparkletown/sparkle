d04d7e172f7098a47695874b6744ebe0818a410d
Glenn 'devalias' Grant <glenn@devalias.net>
2021-07-11 00:13:32 +1000

    fix logic in agora checks

--- functions/src/utils/agora.js
+++ functions/src/utils/agora.js
@@ -10,18 +10,17 @@ const AGORA_APP_CERTIFICATE = AGORA_CONFIG.app_certificate;
 const expirationTimeInSeconds = 3600;
 
 const assertValidAgoraConfig = () => {
-  if (typeof AGORA_APP_ID !== "string" && AGORA_APP_ID.length <= 0)
-    throw new Error(
-      "required configuration missing, AGORA_APP_ID must be a string"
-    );
+  if (typeof AGORA_APP_ID !== "string")
+    throw new Error("AGORA_APP_ID must be a string");
 
-  if (
-    typeof AGORA_APP_CERTIFICATE !== "string" &&
-    AGORA_APP_CERTIFICATE.length <= 0
-  )
-    throw new Error(
-      "required configuration missing, AGORA_APP_CERTIFICATE must be a string"
-    );
+  if (AGORA_APP_ID.length <= 0)
+    throw new Error("AGORA_APP_ID must not be empty");
+
+  if (typeof AGORA_APP_CERTIFICATE !== "string")
+    throw new Error("AGORA_APP_CERTIFICATE must be a string");
+
+  if (AGORA_APP_CERTIFICATE.length <= 0)
+    throw new Error("AGORA_APP_CERTIFICATE must not be empty");
 };
 
 const getExpirationTime = () => {
@@ -34,11 +33,12 @@ const generateAgoraTokenForAccount = ({ channelName, account, role }) => {
   assertValidAgoraConfig();
 
   // @debt we should enforce a stricter security requirement on channelName. Maybe use UUIDs?
-  if (typeof channelName !== "string" && channelName.length <= 0)
+  if (typeof channelName !== "string")
     throw new Error("channelName must be a string");
+  if (channelName.length <= 0) throw new Error("channelName must not be empty");
 
-  if (typeof account !== "string" && account.length <= 0)
-    throw new Error("account must be a string");
+  if (typeof account !== "string") throw new Error("account must be a string");
+  if (account.length <= 0) throw new Error("account must not be empty");
 
   if (![RtcRole.PUBLISHER, RtcRole.SUBSCRIBER].includes(role))
     throw new Error("role must be a valid value from the RtcRole enum");
