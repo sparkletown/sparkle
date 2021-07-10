1febce9845d4552ac700bf675fae9821bab53f5f
Glenn 'devalias' Grant <glenn@devalias.net>
2021-07-10 23:43:48 +1000

    add assertValidAgoraConfig + fix reading from config when not defined

--- functions/src/utils/agora.js
+++ functions/src/utils/agora.js
@@ -2,13 +2,27 @@ const functions = require("firebase-functions");
 
 const { RtcTokenBuilder, RtcRole } = require("agora-access-token");
 
-const AGORA_CONFIG = functions.config().agora;
-
-const appId = AGORA_CONFIG.app_id;
-const appCertificate = AGORA_CONFIG.app_certificate;
+const AGORA_CONFIG = functions.config().agora || {};
+const AGORA_APP_ID = AGORA_CONFIG.app_id;
+const AGORA_APP_CERTIFICATE = AGORA_CONFIG.app_certificate;
 
 // @debt should we move this into AGORA_CONFIG, or maybe venue config in firestore or similar?
 const expirationTimeInSeconds = 3600;
+
+const assertValidAgoraConfig = () => {
+  if (typeof AGORA_APP_ID !== "string" && AGORA_APP_ID.length <= 0)
+    throw new Error(
+      "required configuration missing, AGORA_APP_ID must be a string"
+    );
+
+  if (
+    typeof AGORA_APP_CERTIFICATE !== "string" &&
+    AGORA_APP_CERTIFICATE.length <= 0
+  )
+    throw new Error(
+      "required configuration missing, AGORA_APP_CERTIFICATE must be a string"
+    );
+};
 
 const getExpirationTime = () => {
   const currentTimestamp = Math.floor(Date.now() / 1000);
@@ -17,6 +31,8 @@ const getExpirationTime = () => {
 };
 
 const generateAgoraTokenForAccount = ({ channelName, account, role }) => {
+  assertValidAgoraConfig();
+
   // @debt we should enforce a stricter security requirement on channelName. Maybe use UUIDs?
   if (typeof channelName !== "string" && channelName.length <= 0)
     throw new Error("channelName must be a string");
@@ -28,8 +44,8 @@ const generateAgoraTokenForAccount = ({ channelName, account, role }) => {
     throw new Error("role must be a valid value from the RtcRole enum");
 
   return RtcTokenBuilder.buildTokenWithAccount(
-    appId,
-    appCertificate,
+    AGORA_APP_ID,
+    AGORA_APP_CERTIFICATE,
     channelName,
     account,
     role,
@@ -37,6 +53,7 @@ const generateAgoraTokenForAccount = ({ channelName, account, role }) => {
   );
 };
 
+exports.assertValidAgoraConfig = assertValidAgoraConfig;
 exports.getExpirationTime = getExpirationTime;
 exports.generateAgoraTokenForAccount = generateAgoraTokenForAccount;
 exports.RtcRole = RtcRole;
