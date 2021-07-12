98b203f28c1da99bc3b471ab00e6001b31646d68
Oleksii Petrov <oleksii.petrov@altexsoft.com>
2021-07-12 21:37:44 +0300

    conduct fixes

--- src/pages/Account/Questions.tsx
+++ src/pages/Account/Questions.tsx
@@ -64,7 +64,7 @@ export const Questions: React.FC = () => {
     if (!sovereignVenue) return;
 
     // Skip this screen if there are no profile questions for the venue
-    if (!sovereignVenue.profile_questions.length) {
+    if (!sovereignVenue.profile_questions?.length) {
       proceed();
     }
   }, [proceed, sovereignVenue]);
@@ -115,7 +115,7 @@ export const Questions: React.FC = () => {
         </p>
 
         <form onSubmit={handleSubmit(onSubmit)} className="form">
-          {sovereignVenue?.profile_questions.map((question: QuestionType) => (
+          {sovereignVenue?.profile_questions?.map((question: QuestionType) => (
             <div key={question.name} className="Questions__question form-group">
               <label className="input-block input-centered">
                 <strong>{question.name}</strong>
