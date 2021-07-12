398fe4ec4ed7f8a38ccc95e970a991f6f63b19b4
work <goran.peoski.work@gmail.com>
2021-07-12 14:00:13 +0200

    Use EmptyProvider instead of React.Fragment in Provided (#1797)

--- src/components/organisms/AppRouter/Provided.tsx
+++ src/components/organisms/AppRouter/Provided.tsx
@@ -7,6 +7,14 @@ import {
   RelatedVenuesProviderProps,
 } from "hooks/useRelatedVenues";
 
+export interface EmptyProviderProps {
+  venueId?: string;
+}
+
+const EmptyProvider: React.FC<EmptyProviderProps> = ({ children }) => {
+  return <>{children}</>;
+};
+
 export interface ProvidedProps {
   withWorldUsers?: boolean;
   withRelatedVenues?: boolean;
@@ -21,11 +29,11 @@ export const Provided: React.FC<ProvidedProps> = ({
 
   const MaybeWorldUsersProvider: React.FC<WorldUsersProviderProps> = withWorldUsers
     ? WorldUsersProvider
-    : React.Fragment;
+    : EmptyProvider;
 
   const MaybeRelatedVenuesProvider: React.FC<RelatedVenuesProviderProps> = withRelatedVenues
     ? RelatedVenuesProvider
-    : React.Fragment;
+    : EmptyProvider;
 
   return (
     <MaybeWorldUsersProvider venueId={venueId}>
