01622d9cf6ddde7d76eadbc3c5ba9148745ef9e5
Oleksii Petrov <oleksii.petrov@altexsoft.com>
2021-07-24 08:59:55 +0300

    space added

--- src/components/organisms/EventModal/EventModal.tsx
+++ src/components/organisms/EventModal/EventModal.tsx
@@ -63,6 +63,7 @@ export const EventModal: React.FC<EventModalProps> = ({
 
   const eventLocationToDisplay =
     (event.room || eventVenue?.name) ?? event.venueId;
+
   const goToEventLocation = () => {
     onHide();
 
