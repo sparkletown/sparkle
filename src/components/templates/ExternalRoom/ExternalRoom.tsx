251906e96ea376c1d90845045ab274a008e3ec4c
malaika <51083763+malaikaaiyar@users.noreply.github.com>
2021-07-28 20:29:45 +0100

    make the external room text look 'sparklier' (#1925)
    * make the external room text look sparklier
    
    * minor url text fix + styles
    
    Co-authored-by: Oleksii Petrov <oleksii.petrov@altexsoft.com>

--- src/components/templates/ExternalRoom/ExternalRoom.tsx
+++ src/components/templates/ExternalRoom/ExternalRoom.tsx
@@ -27,15 +27,29 @@ export const ExternalRoom: React.FC<ExternalRoomProps> = ({ venue }) => {
       <div className="ExternalRoom__message">
         <SparkleLogo />
         <div className="ExternalRoom__content">
-          <h4 className="ExternalRoom__header">Thank you for visiting</h4>
-          <a
-            rel="noreferrer"
-            href={venue.zoomUrl}
-            target="_blank"
-            className="ExternalRoom__link"
-          >
-            {venue.zoomUrl}
-          </a>
+          <h4 className="ExternalRoom__header">
+            You will now be redirected to some incredible content! Please ensure
+            you{" "}
+            <a
+              rel="noreferrer"
+              href="https://support.google.com/chrome/answer/95472?hl=en&co=GENIE.Platform%3DDesktop"
+              target="_blank"
+            >
+              allow popups in your browser
+            </a>{" "}
+            and keep this tab open while you explore, so you can find your way
+            back easily. If you do not see a new tab open, please feel free to
+            click through to:{" "}
+            <a
+              rel="noreferrer"
+              href={venue.zoomUrl}
+              target="_blank"
+              className="ExternalRoom__link"
+            >
+              {venue.zoomUrl}
+            </a>
+            .
+          </h4>
         </div>
       </div>
     </div>
