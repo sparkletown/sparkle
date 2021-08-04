4b9e31ce7d641882f5247a8e4604ab00d7555231
Yurii Lubynets <yurichix1@gmail.com>
2021-08-04 15:39:12 +0300

    updated ScheduleNGItem view; fixed its styles

--- src/components/molecules/ScheduleItemNG/ScheduleItemNG.tsx
+++ src/components/molecules/ScheduleItemNG/ScheduleItemNG.tsx
@@ -6,6 +6,8 @@ import { PersonalizedVenueEvent } from "types/venues";
 import { eventEndTime, eventStartTime } from "utils/event";
 import { formatDateRelativeToNow, formatTimeLocalised } from "utils/time";
 
+import { useShowHide } from "hooks/useShowHide";
+
 import "./ScheduleItemNG.scss";
 
 export interface ScheduleItemNGProps {
@@ -15,32 +17,42 @@ export interface ScheduleItemNGProps {
 const formatDateOptions = { formatToday: () => "" };
 
 export const ScheduleItemNG: React.FC<ScheduleItemNGProps> = ({ event }) => {
+  const { isShown: isEventExpanded, toggle: toggleEventExpand } = useShowHide();
   const showDate = Boolean(
     differenceInCalendarDays(eventEndTime(event), eventStartTime(event))
   );
 
   return (
-    <div className="ScheduleItemNG">
-      <div className="ScheduleItemNG--info">
-        <span className="ScheduleItemNG--date">
+    <div className="ScheduleItemNG" onClick={toggleEventExpand}>
+      <div className="ScheduleItemNG__info">
+        <span className="ScheduleItemNG__date">
           {showDate &&
             formatDateRelativeToNow(eventStartTime(event), formatDateOptions)}
         </span>
 
-        <span className="ScheduleItemNG--time">
+        <span className="ScheduleItemNG__time">
           {formatTimeLocalised(eventStartTime(event))}
         </span>
 
-        <span className="ScheduleItemNG--date">
+        <span className="ScheduleItemNG__date ScheduleItemNG__time--end">
           {showDate &&
             formatDateRelativeToNow(eventEndTime(event), formatDateOptions)}
         </span>
 
-        <span className="ScheduleItemNG--time">
+        <span className="ScheduleItemNG__time ScheduleItemNG__time--end">
           {formatTimeLocalised(eventEndTime(event))}
         </span>
       </div>
-      <div className="ScheduleItemNG--name">{event.name}</div>
+      <div className="ScheduleItemNG__details">
+        <div className="ScheduleItemNG__name">{event.name}</div>
+        <div className="ScheduleItemNG__place">
+          <span className="ScheduleItemNG__place--location">in</span>{" "}
+          {event.venueId}
+        </div>
+        {isEventExpanded && (
+          <div className="ScheduleItemNG__description">{event.description}</div>
+        )}
+      </div>
     </div>
   );
 };
