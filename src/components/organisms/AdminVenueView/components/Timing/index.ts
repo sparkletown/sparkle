32944e72b1a0a4b0831beb5afedcc4fb8b20cfa2
malaika <51083763+malaikaaiyar@users.noreply.github.com>
2021-08-04 10:22:31 +0100

    add timing to admin ng (#1521)
    * add inital skeleton for admin v3 final design
    
    * remove unused interface
    
    * naming refactor
    
    * naming and misc refactoring
    
    * start for timing av3 page
    
    * Update AdminVenueView.tsx
    
    * Create index.ts
    
    * Create AdminTimingView.tsx
    
    * stuff
    
    * add timing page + rearrange the components better
    
    * fix small time error
    
    * Apply suggestions from code review
    
    Co-authored-by: Denis Dimitrov <dedimitrov@melontech.com>
    
    * fix duplicate imports
    
    * fix the styling names/order
    
    * use useconnectvenueevents hook instead of usefirestoreconnect in the component
    
    * add a selector for venueevents
    
    * extract has venue events into a const
    
    * fix more comments :)
    
    * use showhide for delete modal, extract complex values into consts in delete modal
    
    * fix remaining issues
    
    * use correct names
    
    * small fix for resetting form
    
    * use constants in eventsview.scss
    
    * Apply suggestions from code review
    
    Co-authored-by: Denis Dimitrov <dedimitrov@melontech.com>
    
    * syntax fixes!
    
    * fix scss and button styles in Timing
    
    * fix timing delete event modal comments
    
    * last comment bits :)
    
    * small scss variable thing
    
    * make the content actually centered
    
    * fix edit button and create events button styling
    
    * fix bug
    
    * fix bugs!
    
    Co-authored-by: Denis Dimitrov <dedimitrov@melontech.com>

---
+++ src/components/organisms/AdminVenueView/components/Timing/index.ts
@@ -0,0 +1,1 @@
+export { Timing } from "./Timing";
---
+++ src/components/organisms/TimingEvent/TimingEvent.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { format } from "date-fns";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+
+import { WithId } from "utils/id";
+import { formatTimeLocalised } from "utils/time";
+import { eventStartTime, eventEndTime } from "utils/event";
+
+import { VenueEvent } from "types/venues";
+
+export type TimingEventProps = {
+  event: WithId<VenueEvent>;
+  setShowCreateEventModal: () => void;
+  setEditedEvent: (event: WithId<VenueEvent>) => void;
+};
+
+export const TimingEvent: React.FC<TimingEventProps> = ({
+  event,
+  setShowCreateEventModal,
+  setEditedEvent,
+}) => {
+  const [display, setDisplay] = useState(false);
+
+  const showButton = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setDisplay(true);
+  };
+  const hideButton = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setDisplay(false);
+  };
+
+  return (
+    <div
+      key={event.id}
+      className="TimingEvent"
+      onMouseEnter={showButton}
+      onMouseLeave={hideButton}
+    >
+      <div className="TimingEvent__time">
+        <p>{format(eventStartTime(event), "do MMM")}</p>
+        <p className="TimingEvent__time-start">
+          {formatTimeLocalised(eventStartTime(event))}
+        </p>
+        <p>{formatTimeLocalised(eventEndTime(event))}</p>
+      </div>
+      <div className="TimingEvent__details">
+        <p>
+          <span className="TimingEvent__details-name">{event.name}</span> by{" "}
+          <span className="TimingEvent__details-host">{event.host}</span>
+        </p>
+        <p className="TimingEvent__details-description">{event.description}</p>
+        <p className="TimingEvent__details-room">
+          in <span className="event-details-room-name">{event.room}</span>
+        </p>
+      </div>
+
+      {display && (
+        <button
+          className="TimingEvent__edit-button"
+          onClick={() => {
+            setShowCreateEventModal();
+            setEditedEvent(event);
+          }}
+        >
+          <FontAwesomeIcon
+            icon={faPen}
+            className="TimingEvent__edit-button__icon"
+            size="lg"
+          />
+        </button>
+      )}
+    </div>
+  );
+};
