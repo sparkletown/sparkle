56815ce95cc3d860e6104a5e170d59facef18458
YarRainbow <yari.sgw@gmail.com>
2021-08-08 16:46:14 +0300

    squash

--- src/components/templates/AnimateMap/vendors/playerio/PlayerIO.d.ts
+++ src/components/templates/AnimateMap/vendors/playerio/PlayerIO.d.ts
@@ -1,3 +1,4 @@
+// @formatter:off
 export type ErrorCallback = (PlayerIOError: Error) => void;
 export type AuthenticateSuccessCallback = (client: client) => void;
 
@@ -12,7 +13,6 @@ export interface PlayerIO {
     successCallback: AuthenticateSuccessCallback,
     errorCallback: ErrorCallback
   ): void;
-
   // gameFS;
 }
 
@@ -81,53 +81,35 @@ export type MemberOfTuple<T, N, U> = T[N] extends U ? ClearifyType<U> : never;
 
 export interface Message<T> {
   addBoolean<N>(value: MemberOfTuple<T, N, boolean>): void;
-
   addString<N>(value: MemberOfTuple<T, N, string>): void;
-
   // addByteArray<N>(value: MemberOfTuple<T, N, number[]>) : void;
   addDouble<N>(value: MemberOfTuple<T, N, Double>): void;
-
   addFloat<N>(value: MemberOfTuple<T, N, Float>): void;
-
   addULong<N>(value: MemberOfTuple<T, N, ULong>): void;
-
   addLong<N>(value: MemberOfTuple<T, N, Long>): void;
-
   addUInt<N>(value: MemberOfTuple<T, N, UInt>): void;
-
   addInt<N>(value: MemberOfTuple<T, N, Int>): void;
 
   getBoolean<N extends number>(n: N): MemberOfTuple<T, N, boolean>;
-
   getString<N extends number>(n: N): MemberOfTuple<T, N, string>;
-
   // getByteArray<N extends number>(n:N) : MemberOfTuple<T,N,number[]>;
   getDouble<N extends number>(n: N): MemberOfTuple<T, N, Double>;
-
   getFloat<N extends number>(n: N): MemberOfTuple<T, N, Float>;
-
   getULong<N extends number>(n: N): MemberOfTuple<T, N, ULong>;
-
   getLong<N extends number>(n: N): MemberOfTuple<T, N, Long>;
-
   getUInt<N extends number>(n: N): MemberOfTuple<T, N, UInt>;
-
   getInt<N extends number>(n: N): MemberOfTuple<T, N, Int>;
 }
 
 export interface connection {
   connected: boolean;
-
   // addDisconnectCallback;
   addMessageCallback<T>(type: string, callback: (m: Message<T>) => void): void;
-
   // createMessage;
   disconnect(): void;
-
   // removeDisconnectCallback;
   // removeMessageCallback;
   send(type: string, ...args): void;
-
   // sendMessage;
 }
 
@@ -141,14 +123,12 @@ export interface bigDB {
   deleteRange;
   load;
   loadKeys;
-
   loadKeysOrCreate<T>(
     table: string,
     key: string,
     successCallback: (dbObj: T) => void,
     errorCallback: (error: Error) => {}
   ): void;
-
   loadMyPlayerObject;
   loadOrCreate;
   loadRange;
@@ -158,3 +138,4 @@ export interface bigDB {
 
 declare const playerIO: PlayerIO;
 export default playerIO;
+// @formatter:on
