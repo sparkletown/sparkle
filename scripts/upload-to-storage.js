ed9b08e19531d224af348ad21f96c58442c4087b
Dmitry Marchuk <code@dmarchuk.com>
2021-08-09 15:38:07 +0300

    fixes in *.js files

--- scripts/upload-to-storage.js
+++ scripts/upload-to-storage.js
@@ -5,7 +5,7 @@ var fs = require("fs");
 var path = require("path");
 require("firebase/storage");
 
-function usage() {
+const usage = () => {
   console.log(`
   ${process.argv[1]}: Upload file to Firebase storage
 
@@ -15,16 +15,16 @@ function usage() {
   Example: node ${process.argv[1]} co-reality-map aaazzz111222333 images image.jpg user@name.com password
 `);
   process.exit(1);
-}
+};
 
-function uploadFile(
+const uploadFile = (
   username,
   password,
   projectId,
   apiKey,
   storagePath,
   filename
-) {
+) => {
   if (!fs.existsSync(filename)) {
     console.error(`File ${filename} does not exist`);
     process.exit(1);
@@ -44,30 +44,30 @@ function uploadFile(
   firebase
     .auth()
     .signInWithEmailAndPassword(username, password)
-    .then(function (result) {
+    .then((result) => {
       const userFilePath = `users/${result.user.uid}/${filePath}`;
       console.log(`Uploading to ${userFilePath}`);
       const storageRef = firebase.storage().ref();
       storageRef
         .child(userFilePath)
         .put(fileContent)
-        .then(function (snapshot) {
+        .then((snapshot) => {
           console.log("Upload succeeded.");
-          snapshot.ref.getDownloadURL().then(function (downloadURL) {
+          snapshot.ref.getDownloadURL().then((downloadURL) => {
             console.log(`URL: ${downloadURL}`);
             process.exit(0);
           });
         })
-        .catch(function (err) {
+        .catch((err) => {
           console.error("Error occurred:", err);
           process.exit(1);
         });
     })
-    .catch(function (err) {
+    .catch((err) => {
       console.error("Error occurred:", err);
       process.exit(1);
     });
-}
+};
 
 const argv = process.argv.slice(2);
 if (argv.length < 4) {
@@ -89,12 +89,12 @@ if (username && password) {
   uploadFile(username, password, projectId, apiKey, storagePath, filename);
 } else {
   console.log(`log in to upload.`);
-  read({ prompt: "Username:" }, function (err, username) {
+  read({ prompt: "Username:" }, (err, username) => {
     if (err) {
       console.error("Error obtaining username:", err);
       process.exit(1);
     }
-    read({ prompt: "Password:", silent: true }, function (err, password) {
+    read({ prompt: "Password:", silent: true }, (err, password) => {
       if (err) {
         console.error("Error obtaining password:", err);
         process.exit(1);
