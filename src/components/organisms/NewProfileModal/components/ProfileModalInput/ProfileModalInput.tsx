c453385ff1d1acb207bce97c09ddcc885d6e1196
Dmitry Marchuk <code@dmarchuk.com>
2021-08-12 09:01:28 +0300

    InputField review fix

--- src/components/organisms/NewProfileModal/components/ProfileModalInput/ProfileModalInput.tsx
+++ src/components/organisms/NewProfileModal/components/ProfileModalInput/ProfileModalInput.tsx
@@ -18,21 +18,29 @@ interface Props extends ContainerClassName, React.HTMLProps<HTMLInputElement> {
 export const ProfileModalInput = React.forwardRef<HTMLInputElement, Props>(
   ({ error, containerClassName, notCondensed, iconEnd, ...rest }, ref) => {
     return (
-      <InputField
-        error={error}
-        containerClassName={containerClassName}
-        iconEnd={iconEnd}
-        inputClassName={classNames("ProfileModalInput__input", {
-          "ProfileModalInput__input--condensed": !notCondensed,
-        })}
-        errorTextClassName={classNames({
-          "ProfileModalInput__error-message--condensed": !notCondensed,
-        })}
-        iconEndClassName="ProfileModalInput__icon-end"
-        //eslint-disable-next-line @typescript-eslint/no-explicit-any
-        ref={ref as any}
-        {...rest}
-      />
+      <div>
+        <InputField
+          containerClassName={containerClassName}
+          iconEnd={iconEnd}
+          inputClassName={classNames("ProfileModalInput__input", {
+            "ProfileModalInput__input--condensed": !notCondensed,
+          })}
+          iconEndClassName="ProfileModalInput__icon-end"
+          //eslint-disable-next-line @typescript-eslint/no-explicit-any
+          ref={ref as any}
+          {...rest}
+        />
+        {
+          <span
+            className={classNames("ProfileModalInput__error-message", {
+              "ProfileModalInput__error-message--hidden": !error,
+              "ProfileModalInput__error-message--condensed": !notCondensed,
+            })}
+          >
+            {error?.message ?? "empty error"}
+          </span>
+        }
+      </div>
     );
   }
 );
