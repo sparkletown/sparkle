ca5e1692852a6676dbf2e0d86ad6948865670208
Yurii Lubynets <yurichix1@gmail.com>
2021-08-19 13:59:39 +0300

    updated radiomodal

--- src/components/molecules/NavBar/NavBar.tsx
+++ src/components/molecules/NavBar/NavBar.tsx
@@ -49,7 +49,6 @@ import { VenuePartygoers } from "components/molecules/VenuePartygoers";
 import { BackButton } from "components/atoms/BackButton";
 import { UserAvatar } from "components/atoms/UserAvatar";
 
-import * as S from "./Navbar.styles";
 import { NavBarLogin } from "./NavBarLogin";
 
 import "./NavBar.scss";
@@ -218,13 +217,6 @@ export const NavBar: React.FC<NavBarPropsType> = ({ hasBackButton = true }) => {
 
   const handleRadioEnable = useCallback(() => setIsRadioPlaying(true), []);
 
-  const [showRadioPopover, setShowRadioPopover] = useState(false);
-
-  const toggleShowRadioPopover = useCallback(
-    () => setShowRadioPopover((prevState) => !prevState),
-    []
-  );
-
   if (!venueId || !currentVenue) return null;
 
   // TODO: ideally this would find the top most parent of parents and use those details
@@ -298,56 +290,51 @@ export const NavBar: React.FC<NavBarPropsType> = ({ hasBackButton = true }) => {
                   </OverlayTrigger>
                 )}
 
-                {showNormalRadio && (
+                {(showNormalRadio || showSoundCloudRadio) && (
                   <OverlayTrigger
                     trigger="click"
                     placement="bottom-end"
                     overlay={
                       <Popover id="radio-popover">
-                        <Popover.Content>
-                          <RadioModal
-                            {...{
-                              volume,
-                              setVolume,
-                              title: currentVenue?.radioTitle,
-                            }}
-                            onEnableHandler={handleRadioEnable}
-                            isRadioPlaying={isRadioPlaying}
-                          />
+                        <Popover.Content className="NavBar__radio--container">
+                          <div className="NavBar__radio--title">
+                            {currentVenue?.radioTitle ?? "Playa Radio"}
+                          </div>
+                          {showNormalRadio && (
+                            <RadioModal
+                              {...{
+                                volume,
+                                setVolume,
+                              }}
+                              onEnableHandler={handleRadioEnable}
+                              isRadioPlaying={isRadioPlaying}
+                            />
+                          )}
+                          {showSoundCloudRadio && (
+                            <iframe
+                              title="venueRadio"
+                              id="sound-cloud-player"
+                              scrolling="no"
+                              allow="autoplay"
+                              src={`https://w.soundcloud.com/player/?url=${radioStation}&amp;start_track=0&amp;single_active=true&amp;show_artwork=false`}
+                            />
+                          )}
                         </Popover.Content>
                       </Popover>
                     }
                     rootClose={true}
                     defaultShow={showRadioOverlay}
                   >
-                    <button
-                      className={`profile-icon navbar-link-radio ${
-                        volume === 0 && "off"
-                      }`}
-                    />
+                    <div className="NavBar__menu--icon NavBar__menu--radio">
+                      <FontAwesomeIcon
+                        // TODO: fix with a new icon
+                        icon={faBars}
+                        size="sm"
+                      />
+                    </div>
                   </OverlayTrigger>
                 )}
 
-                {showSoundCloudRadio && (
-                  <S.RadioTrigger>
-                    <button
-                      className={`profile-icon navbar-link-radio ${
-                        volume === 0 && "off"
-                      }`}
-                      onClick={toggleShowRadioPopover}
-                    />
-
-                    <S.RadioWrapper showRadioPopover={showRadioPopover}>
-                      <iframe
-                        title="venueRadio"
-                        id="sound-cloud-player"
-                        scrolling="no"
-                        allow="autoplay"
-                        src={`https://w.soundcloud.com/player/?url=${radioStation}&amp;start_track=0&amp;single_active=true&amp;show_artwork=false`}
-                      />
-                    </S.RadioWrapper>
-                  </S.RadioTrigger>
-                )}
                 {(showNormalRadio || showSoundCloudRadio) && (
                   <div className={volumeControlClassname} onClick={toggleMute}>
                     <FontAwesomeIcon
