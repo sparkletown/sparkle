8cbca85bd74ab91a2e43bad3a33a4c7c9f05797e
Dmitry Marchuk <github@dmarchuk.com>
2021-08-25 12:27:37 +0300

    Default venue banner and logo (#2025)
    * make proper use of default banner and logo
    
    * replace default assets + updated adminV1 and adminV3
    
    * review fixes
    
    * added @debt comment
    
    Co-authored-by: Dmitry Marchuk <code@dmarchuk.com>

--- src/hooks/useCheckImage.ts
+++ src/hooks/useCheckImage.ts
@@ -1,7 +1,8 @@
+import { useMemo } from "react";
 import { useAsync } from "react-use";
 
 export const useCheckImage = (
-  src: string
+  src: string | undefined
 ): {
   isValid: boolean;
   isLoading: boolean;
@@ -9,14 +10,30 @@ export const useCheckImage = (
 } => {
   const { loading, error } = useAsync(async () => {
     const checkImage = new Image();
+    if (!src) throw Error("src is not defined.");
     checkImage.src = src;
 
     await checkImage.decode();
   }, [src]);
 
-  return {
-    isValid: !error,
-    isLoading: loading,
-    error: error?.message,
-  };
+  return useMemo(
+    () => ({
+      //here we consider the image valid while it's still loading
+      isValid: !error,
+      isLoading: loading,
+      error: error?.message,
+    }),
+    [error, loading]
+  );
+};
+
+export const useValidImage = (
+  src: string | undefined,
+  fallbackIfInvalid: string
+): [string | undefined, ReturnType<typeof useCheckImage>] => {
+  const checkImageState = useCheckImage(src);
+
+  const processedSrc = checkImageState.isValid ? src : fallbackIfInvalid;
+
+  return [processedSrc, checkImageState];
 };
