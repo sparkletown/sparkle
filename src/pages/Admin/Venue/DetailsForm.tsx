f18512fa717774e7823627ff0f4ec9338bb01db3
Viktoryia <viktoryia@sparkle.space>
2021-08-26 14:12:08 +0300

    BM: show parentId, hide user statuses

--- src/pages/Admin/Venue/DetailsForm.tsx
+++ src/pages/Admin/Venue/DetailsForm.tsx
@@ -131,7 +131,7 @@ export const DetailsForm: React.FC<DetailsFormProps> = ({
       bannerImageUrl: defaultValues?.bannerImageUrl ?? DEFAULT_VENUE_BANNER,
       mapBackgroundImageUrl:
         defaultValues?.mapBackgroundImageUrl ?? DEFAULT_MAP_BACKGROUND,
-      parentId: defaultValues?.parentId ?? "",
+      parentId: defaultValues?.parentId ?? BM_PARENT_ID,
     },
   });
   const { user } = useUser();
@@ -629,7 +629,7 @@ const DetailsFormLeft: React.FC<DetailsFormLeftProps> = ({
   );
 
   const renderParentIdInput = () => (
-    <div className="input-container DetailsForm--hidden">
+    <div className="input-container">
       <h4 className="italic input-header">
         Enter the parent venue ID, for the &quot;back&quot; button to go to, and
         for sharing events in the schedule
@@ -646,7 +646,6 @@ const DetailsFormLeft: React.FC<DetailsFormLeftProps> = ({
         below
       </div>
       <input
-        defaultValue={BM_PARENT_ID}
         type="text"
         disabled={disable}
         name="parentId"
@@ -874,7 +873,7 @@ const DetailsFormLeft: React.FC<DetailsFormLeftProps> = ({
         {renderRadioToggle()}
 
         <UserStatusManager
-          className={"DetailsForm__hidden-component"}
+          className="DetailsForm--hidden"
           venueId={venueId}
           checked={hasUserStatuses}
           userStatuses={userStatuses}
