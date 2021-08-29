5f39863dc2c734e743d26c51117172de6fbe3a47
Malaika <malaika@sparkle.space>
2021-08-29 16:47:38 +0100

    fix the small changes

--- src/pages/RegistrationFlow/InitialFormRF/InitialFormRF.tsx
+++ src/pages/RegistrationFlow/InitialFormRF/InitialFormRF.tsx
@@ -2,7 +2,8 @@ import React, { FC } from "react";
 
 import { ButtonRF } from "pages/RegistrationFlow/ButtonRF";
 import { PaneRF } from "pages/RegistrationFlow/PaneRF";
-import { SpanRF } from "pages/RegistrationFlow/SpanRF";
+
+import { LinkButton } from "components/atoms/LinkButton";
 
 import "./InitialFormRF.scss";
 
@@ -17,15 +18,24 @@ export const InitialFormRF: FC<InitialFormRFProps> = ({
 }) => {
   return (
     <PaneRF className="InitialFormRF">
-      <ButtonRF onClick={onRegister} variant="primary">
-        Create account
+      <LinkButton
+        className="ButtonNG__button btn-primary"
+        href="https://www.eventbrite.com/e/sparkleverse-2021-online-burn-tickets-167724347037"
+      >
+        {" "}
+        Buy Tickets
+      </LinkButton>
+      <ButtonRF onClick={onRegister} variant="seethrough">
+        I&apos;ve got a ticket already
       </ButtonRF>
-      <SpanRF variant="dim" className="InitialFormRF__separator">
-        or
-      </SpanRF>
-      <ButtonRF onClick={onLogin} variant="seethrough">
+      Already have an account?
+      <ButtonRF onClick={onLogin} isLink={true}>
         Log In
       </ButtonRF>
+      <div>
+        To access the digital playa, you need a{" "}
+        <a href="https://sparklever.se/tickets">ticket</a>
+      </div>
     </PaneRF>
   );
 };
