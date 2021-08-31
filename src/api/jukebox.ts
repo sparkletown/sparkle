4fa8176e8eb21d23d1771d6f03317f322f5a60de
Oleksii Petrov <oleksii.petrov@altexsoft.com>
2021-08-31 11:06:26 +0300

    juke box add api + types

---
+++ src/api/jukebox.ts
@@ -0,0 +1,30 @@
+import Bugsnag from "@bugsnag/js";
+import { noop } from "lodash";
+
+import { JukeboxMessage } from "types/jukebox";
+
+import { getVenueRef } from "./venue";
+
+export interface SendVenueMessageProps {
+  venueId: string;
+  message: JukeboxMessage;
+}
+
+export const sendJukeboxMessage = async ({
+  venueId,
+  message,
+}: SendVenueMessageProps): Promise<void> =>
+  getVenueRef(venueId)
+    .collection("jukeboxMessages")
+    .add(message)
+    .then(noop)
+    .catch((err) => {
+      Bugsnag.notify(err, (event) => {
+        event.addMetadata("context", {
+          location: "api/chat::sendJukeboxMessage",
+          venueId,
+          message,
+        });
+      });
+      // @debt rethrow error, when we can handle it to show UI error
+    });
