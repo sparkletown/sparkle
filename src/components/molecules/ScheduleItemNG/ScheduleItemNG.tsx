f5885a728b8b64168aea6e0561bbab2f4026cdd4
Viktoryia <viktoryia@sparkle.space>
2021-09-01 16:28:34 +0300

    Make event venue clickable

--- src/components/molecules/ScheduleItemNG/ScheduleItemNG.tsx
+++ src/components/molecules/ScheduleItemNG/ScheduleItemNG.tsx
@@ -101,6 +101,10 @@ export const ScheduleItemNG: React.FC<ScheduleItemNGProps> = ({
     }
   }, [enterRoom, event, eventRoom]);
 
+  const enterEventVenue = useCallback(() => enterVenue(event.venueId), [
+    event.venueId,
+  ]);
+
   const eventImage = getFirebaseStorageResizedImage(
     eventRoom?.image_url ?? event.venueIcon,
     {
@@ -171,7 +175,9 @@ export const ScheduleItemNG: React.FC<ScheduleItemNGProps> = ({
         <div className="ScheduleItemNG__name">{event.name}</div>
         <div className="ScheduleItemNG__place">
           <span className="ScheduleItemNG__place--location">in</span>{" "}
-          {eventVenue?.name}
+          <span className="button--a" onClick={enterEventVenue}>
+            {eventVenue?.name}
+          </span>
           {eventRoom && (
             <>
               {", "}
