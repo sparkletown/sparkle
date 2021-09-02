2e4b49665019557749283506c29fa17608916352
meiciuc <meiciuc@gmail.com>
2021-09-02 23:37:10 +0300

    turn off firebarrel shouter

--- src/components/templates/AnimateMap/game/map/entities/createFirebarrelEntity.ts
+++ src/components/templates/AnimateMap/game/map/entities/createFirebarrelEntity.ts
@@ -21,7 +21,7 @@ import { FirebarrelCamIcon } from "../graphics/FirebarrelCamIcon";
 import { FirebarrelHalo } from "../graphics/FirebarrelHalo";
 import { FirebarrelHaloAnimated } from "../graphics/FirebarrelHaloAnimated";
 import { FirebarrelHaloEmpty } from "../graphics/FirebarrelHaloEmpty";
-import { FirebarrelShouter } from "../graphics/FirebarrelShouter";
+// import { FirebarrelShouter } from "../graphics/FirebarrelShouter";
 import { FirebarrelTooltip } from "../graphics/FirebarrelTooltip";
 import { HoverIn } from "../graphics/HoverIn";
 import { HoverOut } from "../graphics/HoverOut";
@@ -119,9 +119,9 @@ export const createFirebarrelEntity = (
   const entity: Entity = new Entity();
   const fsm: FSMBase = new FSMBase(entity);
 
-  const shouterTimeOut = 250 * 6 + Math.random() * 120;
-  const shouterCurrentTime = shouterTimeOut * Math.random();
-  const shouter = new FirebarrelShouter(shouterTimeOut, shouterCurrentTime);
+  // const shouterTimeOut = 250 * 6 + Math.random() * 120;
+  // const shouterCurrentTime = shouterTimeOut * Math.random();
+  // const shouter = new FirebarrelShouter(shouterTimeOut, shouterCurrentTime);
   const barrelComponent = new FirebarrelComponent(barrel, fsm);
   const positionComponent = new PositionComponent(barrel.x, barrel.y);
   const spriteComponent = new SpriteComponent();
@@ -167,7 +167,7 @@ export const createFirebarrelEntity = (
     .add(positionComponent)
     .add(spriteComponent)
     .add(new FirebarrelCamIcon(spriteComponent.view))
-    .add(shouter)
+    // .add(shouter)
     .add(
       new HoverableSpriteComponent(
         () => {
