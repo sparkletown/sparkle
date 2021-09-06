481c1df6d605841a96963ce3f6e73c9f01961154
Viktoryia <viktoryia@sparkle.space>
2021-09-06 18:43:09 +0300

    Move settingsSelector to utils/selectors

--- src/hooks/useSettings.ts
+++ src/hooks/useSettings.ts
@@ -3,15 +3,11 @@ import { useMemo } from "react";
 import { DEFAULT_SHOW_CHAT } from "settings";
 
 import { Settings } from "types/Firestore";
-import { SparkleSelector } from "types/SparkleSelector";
+
+import { settingsSelector } from "utils/selectors";
 
 import { isLoaded, useFirestoreConnect } from "hooks/useFirestoreConnect";
 import { useSelector } from "hooks/useSelector";
-
-export const settingsSelector: SparkleSelector<Settings | undefined> = (
-  state
-) => state.firestore.data.settings;
-
 export interface UseSetingsReturnType {
   isLoaded: boolean;
   settings: Required<Settings>;
