1278c27b97175dbb97bfbcccd836fdf433b09eba
alexey-petrov <56254806+alexey-petrov@users.noreply.github.com>
2021-09-15 20:58:51 +0300

    Added environment venue owners script (#2051)
    * added env venue owners scripts
    
    * minor change
    
    * minor fixes + csv formatting
    
    * minor fixes + added exclusion logic
    
    * info fixed
    
    * improved filtering logic
    
    * added type for uid
    
    * minor fix venue owners
    
    * minor fixes + file rename
    
    * mr fixes
    
    * test deploy commit
    
    * test deploy change
    
    Co-authored-by: Oleksii Petrov <oleksii.petrov@altexsoft.com>
    Co-authored-by: sunny-viktoryia <63313732+sunny-viktoryia@users.noreply.github.com>

---
+++ scripts/get-env-venue-owners.ts
@@ -0,0 +1,99 @@
+#!/usr/bin/env node -r esm -r ts-node/register
+
+import fs from "fs";
+import { resolve } from "path";
+
+import admin from "firebase-admin";
+
+import {
+  initFirebaseAdminApp,
+  makeScriptUsage,
+  parseCredentialFile,
+} from "./lib/helpers";
+
+const usage = makeScriptUsage({
+  description:
+    "Print venue owners' email addresses ignoring specified users if needed",
+  usageParams: "[CREDENTIAL_PATH] emailSubstrToIgnore",
+  exampleParams: "[theMatchingAccountServiceKey.json] @gmail.com",
+});
+
+const [credentialPath, ...emailSubstrToIgnore] = process.argv.slice(2);
+
+if (!credentialPath) {
+  usage();
+}
+
+const { project_id: projectId } = parseCredentialFile(credentialPath);
+
+if (!projectId) {
+  console.error("Credential file has no project_id:", credentialPath);
+  process.exit(1);
+}
+
+const outputFileName = `${projectId}-venue-owners.csv`;
+
+initFirebaseAdminApp(projectId, {
+  credentialPath: credentialPath
+    ? resolve(__dirname, credentialPath)
+    : undefined,
+});
+
+(async () => {
+  const { users, pageToken } = await admin.auth().listUsers(1000);
+
+  const allUsers = [...users];
+  let nextPageToken = pageToken;
+
+  while (nextPageToken) {
+    const { users, pageToken } = await admin
+      .auth()
+      .listUsers(1000, nextPageToken);
+    allUsers.push(...users);
+    nextPageToken = pageToken;
+  }
+
+  // @debt This function will currently load all venues in firebase into memory.. not very efficient
+  const firestoreVenues = await admin.firestore().collection("venues").get();
+  const venues = firestoreVenues.docs;
+
+  // clear file
+  fs.writeFileSync(outputFileName, "");
+
+  const headingLine = ["Venue Id", "Venue Name", "Venue Owners"]
+    .map((heading) => `"${heading}"`)
+    .join(",");
+
+  fs.writeFileSync(outputFileName, `${headingLine}\n`, { flag: "a" });
+
+  const existingUsers = allUsers.filter((user) => user.email?.includes("@"));
+  const filteredUsers = emailSubstrToIgnore?.length
+    ? existingUsers.filter(
+        (user) =>
+          !emailSubstrToIgnore.some((substrToExclude) =>
+            user?.email?.includes(substrToExclude)
+          )
+      )
+    : existingUsers;
+
+  venues.forEach((doc) => {
+    if (!doc.exists) return;
+
+    const venueOwners = doc
+      .data()
+      .owners?.map(
+        (uid: string) => filteredUsers.find((u) => u.uid === uid)?.email ?? ""
+      )
+      .filter((owner: string) => owner);
+    const venueData = [doc.id, doc.data().name, venueOwners ?? ""];
+
+    const csvFormattedLine = venueData.map((s) => `"${s}"`).join(",");
+
+    fs.writeFileSync(outputFileName, `${csvFormattedLine}\n`, {
+      flag: "a",
+    });
+  });
+
+  console.log("Venue owners are successfully extracted to", outputFileName);
+  process.exit(0);
+})();
