ab0afbed983975054e3dea0f78619a35e5629d8c
dmitrii-zolotukhin <dmitrii@sparkle.space>
2021-09-16 10:54:28 +0000

    added useCallback to handleBackClicked

--- src/components/organisms/HelpCenter/HelpCenter.tsx
+++ src/components/organisms/HelpCenter/HelpCenter.tsx
@@ -27,9 +27,9 @@ export const HelpCenter: React.FC = () => {
     [startLoading]
   );
 
-  const handleBackClicked = () => {
+  const handleBackClicked = useCallback(() => {
     setUrl("");
-  };
+  }, []);
 
   return (
     <>
