67e0ed398847a662f7583aa14e0597b3cb9688d5
goran-peoski-work <goran.peoski.work@gmail.com>
2021-09-20 11:19:53 +0200

    Make worldId mandatory for BaseVenue (#2316)

--- src/types/venues.ts
+++ src/types/venues.ts
@@ -193,7 +193,7 @@ export interface BaseVenue {
   showUserStatus?: boolean;
   createdAt?: number;
   updatedAt?: number;
-  worldId?: string;
+  worldId: string;
 }
 
 export interface GenericVenue extends BaseVenue {
