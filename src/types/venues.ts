77a4b13019f0ba4b87f6488aa617897421d9e629
Yurii Lubynets <yurichix1@gmail.com>
2021-09-23 15:26:24 +0300

    Feature/Admin V3 - Toggle to force hide room label per portal (#2306)
    * toggle to force hide room label per portal
    
    * updated Label options for "Label appearance" dropdown
    
    * updated Label appearance dropdown with new items
    
    * renamed isLabelHidden into visibility prop of portal
    
    * fixed admin_v1 support
    
    * fixed Room visibility priority and fixed its management
    
    * updated support for v1
    
    * added PortalVisibility component; fixed styles for Edit room; removed noop handler; fixed portal-settings
    
    * fixed PortalVisibility usage for RoomsForm
    
    * moved styles for PortalVisibility into component; updated its usage
    
    * Apply suggestions from code review
    
    Co-authored-by: goran-peoski-work <goran.peoski.work@gmail.com>
    
    * Update PortalVisibility.scss
    
    Co-authored-by: goran-peoski-work <goran.peoski.work@gmail.com>

--- src/types/venues.ts
+++ src/types/venues.ts
@@ -298,6 +298,8 @@ export enum RoomVisibility {
   hover = "hover",
   count = "count",
   nameCount = "count/name",
+  none = "none",
+  unclickable = "unclickable",
 }
 
 export interface VenueConfig {
