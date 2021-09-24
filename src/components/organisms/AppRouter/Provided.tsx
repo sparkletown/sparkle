22d6fc6803de23f8191f4416c6f738d61315aff8
mike-lvov <mike.lvov.soft@gmail.com>
2021-09-24 13:30:34 +0300

    Removes world users provider

--- src/components/organisms/AppRouter/Provided.tsx
+++ src/components/organisms/AppRouter/Provided.tsx
@@ -1,31 +1,18 @@
 import React from "react";
 
 import { useConnectCurrentVenueNG } from "hooks/useConnectCurrentVenueNG";
-import {
-  RelatedVenuesProvider,
-  RelatedVenuesProviderProps,
-} from "hooks/useRelatedVenues";
-import { WorldUsersProvider, WorldUsersProviderProps } from "hooks/users";
+import { RelatedVenuesProvider } from "hooks/useRelatedVenues";
 import { useVenueId } from "hooks/useVenueId";
 
 import { LoadingPage } from "components/molecules/LoadingPage";
 
-export type EmptyProviderProps = RelatedVenuesProviderProps &
-  WorldUsersProviderProps;
-
-const EmptyProvider: React.FC<EmptyProviderProps> = ({ children }) => {
-  return <>{children}</>;
-};
-
 export interface ProvidedProps {
   withRelatedVenues?: boolean;
-  withWorldUsers?: boolean;
 }
 
 export const Provided: React.FC<ProvidedProps> = ({
   children,
   withRelatedVenues = false,
-  withWorldUsers = false,
 }) => {
   const venueId = useVenueId();
 
@@ -34,13 +21,7 @@ export const Provided: React.FC<ProvidedProps> = ({
     isCurrentVenueLoaded,
   } = useConnectCurrentVenueNG(venueId);
 
-  const MaybeRelatedVenuesProvider: React.FC<RelatedVenuesProviderProps> = withRelatedVenues
-    ? RelatedVenuesProvider
-    : EmptyProvider;
-
-  const MaybeWorldUsersProvider: React.FC<WorldUsersProviderProps> = withWorldUsers
-    ? WorldUsersProvider
-    : EmptyProvider;
+  if (!withRelatedVenues) return <>{children}</>;
 
   if (!isCurrentVenueLoaded) return <LoadingPage />;
 
@@ -50,10 +31,8 @@ export const Provided: React.FC<ProvidedProps> = ({
   }
 
   return (
-    <MaybeRelatedVenuesProvider venueId={venueId} worldId={venue.worldId}>
-      <MaybeWorldUsersProvider venueId={venueId} worldId={venue.worldId}>
-        {children}
-      </MaybeWorldUsersProvider>
-    </MaybeRelatedVenuesProvider>
+    <RelatedVenuesProvider venueId={venueId} worldId={venue.worldId}>
+      {children}
+    </RelatedVenuesProvider>
   );
 };
