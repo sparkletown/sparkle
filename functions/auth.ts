c7939bd1deb661caa7901d595d97170e86336b89
Dmitry Marchuk <code@dmarchuk.com>
2021-09-27 14:05:09 +0300

    TS in cloud functions: fix for npm run scripts

--- functions/auth.ts
+++ functions/auth.ts
@@ -41,7 +41,7 @@ type ConnecxtI4AOAuthQueryType = {
  *
  * @see https://github.com/lelylan/simple-oauth2/blob/3.x/API.md#authorizeurlauthorizeoptions--string
  */
-exports.connectI4AOAuth = functions.https.onRequest(
+export const connectI4AOAuth = functions.https.onRequest(
   async (req: Request, res: express.Response) => {
     const { venueId, returnOrigin } = req.query as ConnecxtI4AOAuthQueryType;
 
@@ -98,146 +98,148 @@ exports.connectI4AOAuth = functions.https.onRequest(
  * looks up the associated I4A user's details, creates/fetches a firebase account, and then
  * finally returns a custom Firebase auth token that the frontend can use to login as this user.
  */
-exports.connectI4AOAuthHandler = functions.https.onRequest(async (req, res) => {
-  const {
-    venueId,
-    returnOrigin,
-    code: authCode,
-  } = req.query as ConnecxtI4AOAuthQueryType;
+export const connectI4AOAuthHandler = functions.https.onRequest(
+  async (req, res) => {
+    const {
+      venueId,
+      returnOrigin,
+      code: authCode,
+    } = req.query as ConnecxtI4AOAuthQueryType;
 
-  assertValidVenueId(venueId, "venueId");
-  assertValidUrl(returnOrigin, "returnOrigin");
+    assertValidVenueId(venueId, "venueId");
+    assertValidUrl(returnOrigin, "returnOrigin");
 
-  if (!authCode) {
-    throw new HttpsError("invalid-argument", "code is required");
-  }
+    if (!authCode) {
+      throw new HttpsError("invalid-argument", "code is required");
+    }
 
-  const authConfig = await fetchAuthConfig(venueId);
+    const authConfig = await fetchAuthConfig(venueId);
 
-  const {
-    validReturnOrigins,
-    i4aApiKey,
-    i4aOAuthUserInfoUrl,
-    i4aGetUserMeetingInfoUrl,
-    i4aMeetingIdsToCheck,
-    i4aEventIdsToCheck,
-  } = authConfig;
+    const {
+      validReturnOrigins,
+      i4aApiKey,
+      i4aOAuthUserInfoUrl,
+      i4aGetUserMeetingInfoUrl,
+      i4aMeetingIdsToCheck,
+      i4aEventIdsToCheck,
+    } = authConfig;
 
-  if (!validReturnOrigins.includes(returnOrigin)) {
-    throw new HttpsError(
-      "invalid-argument",
-      "returnOrigin is not an allowed origin"
-    );
-  }
+    if (!validReturnOrigins.includes(returnOrigin)) {
+      throw new HttpsError(
+        "invalid-argument",
+        "returnOrigin is not an allowed origin"
+      );
+    }
 
-  const authClient = createOAuth2Client(authConfig);
+    const authClient = createOAuth2Client(authConfig);
 
-  functions.logger.log("Received auth code:", authCode);
-  const results = await authClient.getToken({
-    code: authCode,
-    redirect_uri: "http://localhost:3000/in/foo", // TODO: what do we want to use here?
-  });
-  functions.logger.log("Auth code exchange result received:", results);
+    functions.logger.log("Received auth code:", authCode);
+    const results = await authClient.getToken({
+      code: authCode,
+      redirect_uri: "http://localhost:3000/in/foo", // TODO: what do we want to use here?
+    });
+    functions.logger.log("Auth code exchange result received:", results);
 
-  const { access_token: accessToken } = results.token;
+    const { access_token: accessToken } = results.token;
 
-  // Retrieve the user's I4A User ID
-  const { id: i4aUserId } = await getJson(i4aOAuthUserInfoUrl, {
-    Authorization: `Bearer ${accessToken}`,
-  });
-
-  functions.logger.log("I4A User ID:", i4aUserId);
-
-  if (!i4aUserId) {
-    throw new HttpsError("internal", "failed to retrieve i4aUserId");
-  }
-
-  const checkedMeetingResult = await postJson(i4aGetUserMeetingInfoUrl, {
-    apiKey: i4aApiKey,
-    ams_id: i4aUserId,
-  });
-
-  functions.logger.log("Checked Meeting Result:", checkedMeetingResult);
-
-  const {
-    email: emailRaw,
-    meeting_ids: meetingIds = [],
-    event_ids: eventIds = [],
-  } = checkedMeetingResult;
-
-  const registeredMeetings = meetingIds.filter((meetingId) =>
-    i4aMeetingIdsToCheck.includes(meetingId)
-  );
-
-  const registeredEvents = eventIds.filter((eventId) =>
-    i4aEventIdsToCheck.includes(eventId)
-  );
-
-  const isRegistered =
-    registeredMeetings.length > 0 && registeredEvents.length > 0;
-
-  functions.logger.log("Registered:", {
-    isRegistered,
-    registeredMeetings,
-    registeredEvents,
-  });
-
-  if (!isRegistered || !emailRaw) {
-    // TODO: redirect to some kind of 'not allowed' page
-    res.redirect("/in/TODO/not-registered");
-    return;
-  }
-
-  const email = emailRaw.toLowerCase().trim();
-
-  // Lookup the existing user by their email, or create them if they don't already exist
-  const userRecord = await admin
-    .auth()
-    .getUserByEmail(email)
-    .catch((error) => {
-      // If user doesn't exist then create them
-      if (error.code === "auth/user-not-found") {
-        functions.logger.log(
-          "Existing user not found, creating new user:",
-          email
-        );
-
-        // We explicitly don't set a password here, which should prevent signing in that way (until the user resets their password to create one)
-        // We also explicitly aren't creating the user's profile here, which will let them configure it in the normal way when they first sign in
-        return admin.auth().createUser({ email });
-      }
-      throw error;
+    // Retrieve the user's I4A User ID
+    const { id: i4aUserId } = await getJson(i4aOAuthUserInfoUrl, {
+      Authorization: `Bearer ${accessToken}`,
     });
 
-  functions.logger.log("User:", {
-    i4aUserId,
-    userId: userRecord.uid,
-    email: userRecord.email,
-  });
+    functions.logger.log("I4A User ID:", i4aUserId);
 
-  // Set custom claims on the user based on their registered meetings/events
-  await admin.auth().setCustomUserClaims(userRecord.uid, {
-    i4aUserId,
-    registeredMeetings,
-    registeredEvents,
-  });
+    if (!i4aUserId) {
+      throw new HttpsError("internal", "failed to retrieve i4aUserId");
+    }
 
-  // Create a custom token for the frontend to use to sign into firebase auth as this user
-  const customToken = await admin.auth().createCustomToken(userRecord.uid);
+    const checkedMeetingResult = await postJson(i4aGetUserMeetingInfoUrl, {
+      apiKey: i4aApiKey,
+      ams_id: i4aUserId,
+    });
 
-  // Construct the platform URL that the custom token will be returned to
-  const customTokenReturnUrl = new URL(
-    `/login/${venueId}/${customToken}`,
-    returnOrigin
-  );
+    functions.logger.log("Checked Meeting Result:", checkedMeetingResult);
 
-  functions.logger.log(
-    "Redirecting back to platform with custom token:",
-    customTokenReturnUrl
-  );
+    const {
+      email: emailRaw,
+      meeting_ids: meetingIds = [],
+      event_ids: eventIds = [],
+    } = checkedMeetingResult;
 
-  res.redirect(customTokenReturnUrl.toString());
-});
+    const registeredMeetings = meetingIds.filter((meetingId) =>
+      i4aMeetingIdsToCheck.includes(meetingId)
+    );
+
+    const registeredEvents = eventIds.filter((eventId) =>
+      i4aEventIdsToCheck.includes(eventId)
+    );
+
+    const isRegistered =
+      registeredMeetings.length > 0 && registeredEvents.length > 0;
+
+    functions.logger.log("Registered:", {
+      isRegistered,
+      registeredMeetings,
+      registeredEvents,
+    });
+
+    if (!isRegistered || !emailRaw) {
+      // TODO: redirect to some kind of 'not allowed' page
+      res.redirect("/in/TODO/not-registered");
+      return;
+    }
+
+    const email = emailRaw.toLowerCase().trim();
+
+    // Lookup the existing user by their email, or create them if they don't already exist
+    const userRecord = await admin
+      .auth()
+      .getUserByEmail(email)
+      .catch((error) => {
+        // If user doesn't exist then create them
+        if (error.code === "auth/user-not-found") {
+          functions.logger.log(
+            "Existing user not found, creating new user:",
+            email
+          );
+
+          // We explicitly don't set a password here, which should prevent signing in that way (until the user resets their password to create one)
+          // We also explicitly aren't creating the user's profile here, which will let them configure it in the normal way when they first sign in
+          return admin.auth().createUser({ email });
+        }
+        throw error;
+      });
+
+    functions.logger.log("User:", {
+      i4aUserId,
+      userId: userRecord.uid,
+      email: userRecord.email,
+    });
+
+    // Set custom claims on the user based on their registered meetings/events
+    await admin.auth().setCustomUserClaims(userRecord.uid, {
+      i4aUserId,
+      registeredMeetings,
+      registeredEvents,
+    });
+
+    // Create a custom token for the frontend to use to sign into firebase auth as this user
+    const customToken = await admin.auth().createCustomToken(userRecord.uid);
+
+    // Construct the platform URL that the custom token will be returned to
+    const customTokenReturnUrl = new URL(
+      `/login/${venueId}/${customToken}`,
+      returnOrigin
+    );
+
+    functions.logger.log(
+      "Redirecting back to platform with custom token:",
+      customTokenReturnUrl
+    );
+
+    res.redirect(customTokenReturnUrl.toString());
+  }
+);
 
 /** Automatically make user admin upon register.
  *
@@ -253,20 +255,22 @@ exports.connectI4AOAuthHandler = functions.https.onRequest(async (req, res) => {
  *
  *  @see https://firebase.google.com/docs/functions/auth-events
  */
-exports.autoAdminOnRegister = functions.auth.user().onCreate(async (user) => {
-  const flag = functions.config().flag || {};
+export const autoAdminOnRegister = functions.auth
+  .user()
+  .onCreate(async (user) => {
+    const flag = functions.config().flag || {};
 
-  if (flag.autoadmin) {
-    functions.logger.log(
-      "flag.autoadmin is",
-      flag.autoadmin,
-      "adding user.uid",
-      user.uid,
-      "with email",
-      user.email,
-      "to the admin role"
-    );
+    if (flag.autoadmin) {
+      functions.logger.log(
+        "flag.autoadmin is",
+        flag.autoadmin,
+        "adding user.uid",
+        user.uid,
+        "with email",
+        user.email,
+        "to the admin role"
+      );
 
-    await addAdmin(user.uid);
-  }
-});
+      await addAdmin(user.uid);
+    }
+  });
