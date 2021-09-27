6aa45c1c0dc93f7258b7e1541f2e1db2d1a3c541
Denis Dimitrov <dedimitrov@melontech.com>
2021-09-27 10:35:59 +0300

    minor refactoring

--- src/components/atoms/Toggler/Toggler.tsx
+++ src/components/atoms/Toggler/Toggler.tsx
@@ -7,19 +7,17 @@ export interface TogglerProps
   extends Omit<CheckboxProps, "toggler" | "checked" | "defaultChecked"> {
   toggled?: boolean;
   title?: string;
-  titleClassName?: string;
   defaultToggled?: boolean;
 }
 
 export const Toggler: React.FC<TogglerProps> = ({
   toggled,
   title,
-  titleClassName,
   defaultToggled,
   ...extraProps
 }) => (
   <>
-    {title && <Form.Label className={titleClassName}>{title}</Form.Label>}
+    {title && <Form.Label>{title}</Form.Label>}
     <Checkbox
       {...extraProps}
       toggler
