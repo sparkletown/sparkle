7ee03219269eeca40766620dc31ba7dafa2051d8
Yurii Lubynets <yurichix1@gmail.com>
2021-09-28 14:44:42 +0300

    fixed space modal styles

--- src/components/templates/PartyMap/components/RoomModal/RoomModal.tsx
+++ src/components/templates/PartyMap/components/RoomModal/RoomModal.tsx
@@ -57,7 +57,12 @@ export const RoomModal: React.FC<RoomModalProps> = ({
   }
 
   return (
-    <Modal show={show} onHide={onHide} centered>
+    <Modal
+      show={show}
+      onHide={onHide}
+      centered
+      contentClassName="RoomModal__wrapper"
+    >
       <div className="RoomModal">
         <RoomModalContent room={room} venueEvents={venueEvents} venue={venue} />
       </div>
