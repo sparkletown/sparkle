fcbe1f5215d610fce6f291c2f8b2b80906901285
goran-peoski-work <goran.peoski.work@gmail.com>
2021-09-29 11:26:07 +0200

    Add WorldsDashboard (#2368)
    * Add WorldsDashboard
    
    * Move WorldsDashboard to pages

--- src/components/organisms/AppRouter/AdminV3Subrouter.tsx
+++ src/components/organisms/AppRouter/AdminV3Subrouter.tsx
@@ -7,16 +7,19 @@ import {
   ADMIN_V3_EDIT_PARAM_URL,
   ADMIN_V3_ROOT_URL,
   ADMIN_V3_VENUE_PARAM_URL,
+  ADMIN_V3_WORLDS_URL,
 } from "settings";
 
 import VenueWizardV2 from "pages/Admin/Venue/VenueWizard/VenueWizard";
 import { AdminAdvancedSettings } from "pages/AdminAdvancedSettings";
+import { WorldsDashboard } from "pages/WorldsDashboard";
 
 import { AdminDashboard } from "components/organisms/AdminDashboard";
 import { AdminVenueView } from "components/organisms/AdminVenueView";
 
 export const AdminV3Subrouter: React.FC = () => (
   <Switch>
+    <Route path={ADMIN_V3_WORLDS_URL} component={WorldsDashboard} />
     <Route path={ADMIN_V3_VENUE_PARAM_URL} component={AdminVenueView} />
     <Route
       path={ADMIN_V3_ADVANCED_PARAM_URL}
---
+++ src/pages/WorldsDashboard/index.ts
@@ -0,0 +1,1 @@
+export { WorldsDashboard } from "./WorldsDashboard";
