3b34439cdcbafc0016775e1a483437892ce23ca2
Oleksii Petrov <oleksii.petrov@altexsoft.com>
2021-09-30 20:03:50 +0300

    removed unnecessary images + refactor img load

--- src/pages/RegistrationFlow/LogoRF/LogoRF.tsx
+++ src/pages/RegistrationFlow/LogoRF/LogoRF.tsx
@@ -1,30 +1,11 @@
 import "./LogoRF.scss";
 
-export const LogoRF = () => {
-  const logoClasses = "LogoRF__logo";
-
-  return (
-    <div className="LogoRF">
-      <img
-        className={`${logoClasses}-small LogoRF__logo-small--left-top`}
-        src="/sparkle-icon-wide-sm.svg"
-        alt="sparkle logo"
-      />
-      <img
-        className={`${logoClasses}-small LogoRF__logo-small--left-bottom`}
-        src="/sparkle-icon-wide-sm.svg"
-        alt="sparkle logo"
-      />
-      <img
-        className={logoClasses}
-        src="/sparkle-icon-wide.svg"
-        alt="sparkle logo"
-      />
-      <img
-        className={`${logoClasses}-small LogoRF__logo-small--right-top`}
-        src="/sparkle-icon-wide-sm.svg"
-        alt="sparkle logo"
-      />
-    </div>
-  );
-};
+export const LogoRF = () => (
+  <div className="LogoRF">
+    <img
+      className="LogoRF__logo"
+      src="/sparkle-logo-rf.svg"
+      alt="sparkle logo"
+    />
+  </div>
+);
