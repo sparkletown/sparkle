64b5890f4805c270b6e3a0fb98d07d8a4f6a3087
dmitrii-zolotukhin <dmitrii@sparkle.space>
2021-10-05 11:34:24 +0000

    added guard pattern for setMixpanelGroup

--- src/utils/mixpanel.ts
+++ src/utils/mixpanel.ts
@@ -26,6 +26,8 @@ export const trackMixpanelEvent = (eventName: string, data: Object) => {
 };
 
 export const setMixpanelGroup = (groupKey: string, groupIds: string[]) => {
+  if (!MIXPANEL_PROJECT_TOKEN) return;
+
   mixpanel.set_group(groupKey, groupIds);
   groupIds.forEach((id) => {
     mixpanel.get_group(groupKey, id).set({ $name: id });
