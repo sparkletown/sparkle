3b7e19c6c5d571a8eed048f0d68a5f5771e56a7b
Viktoryia <viktoryia@sparkle.space>
2021-11-08 19:17:59 +0300

    Reorganize migration script: add-room-templates

--- scripts/migration/v0.0.4__add-room-templates.ts
+++ scripts/migration/v0.0.4__add-room-templates.ts
@@ -6,6 +6,113 @@ import { Room } from "../../src/types/rooms";
 
 const isVenueRegex = /\/in\/(\w+)$/;
 
+/**
+ * Transformation of rooms in partymaps. The script:
+ * - changes template of all the 'themecamp' venues to 'partymap' (themecamp is an outdated venue template)
+ * - assignes the room template based on the room URL
+ * - replaces "audience" room template with "auditorium" (audience is an outdated template)
+ */
+export const migrate = async ({ firestore }: MigrateOptions) => {
+  // Replace "themecamp"s -> "partymap"s
+  const { docs: thememaps } = await firestore
+    .collection("venues")
+    .where("template", "==", "themecamp")
+    .get();
+
+  console.log(
+    "thememaps: ",
+    thememaps.map((doc) => doc.id)
+  );
+
+  await Promise.all(
+    thememaps.map(
+      async (doc) =>
+        await doc.ref.update({
+          template: "partymap",
+        })
+    )
+  );
+
+  // Fetch all the "partymap"s
+  const { docs: venueDocs } = await firestore
+    .collection("venues")
+    .where("template", "==", "partymap")
+    .get();
+
+  // Update the rooms of the partymaps
+  await Promise.all(
+    venueDocs.map(async (venueDoc) => {
+      const rooms: Room[] = venueDoc.data().rooms ?? [];
+      await Promise.all(
+        rooms?.map((room) =>
+          transformRoomAndCreateVenues({
+            firestore,
+            venueDoc,
+            room,
+          })
+        )
+      );
+
+      const wasRooms = venueDoc.data().rooms?.map((room) => ({
+        title: room.title,
+        url: room.url,
+        template: room.template,
+      }));
+
+      const nowRooms = rooms?.map((room) => ({
+        title: room.title,
+        url: room.url,
+        template: room.template,
+      }));
+
+      if (!isEqual(wasRooms, nowRooms)) {
+        // console.log("-------------------------------");
+        // console.log("venudId: ", venueDoc.id);
+        // console.log("now", wasRooms);
+        // console.log("will be", nowRooms);
+        await firestore.collection("venues").doc(venueDoc.id).update({ rooms });
+      }
+    })
+  );
+};
+
+const transformRoomAndCreateVenues = async ({ firestore, venueDoc, room }) => {
+  if (room.template === "audience") {
+    room.template = "auditorium";
+  }
+
+  if (room.template === "themecamp") {
+    room.template = "partymap";
+  }
+
+  if (room.template) return;
+
+  if (isVenueRegex.test(room.url)) {
+    const [, targetVenueId] = room.url.match(isVenueRegex);
+
+    const targetVenueDoc = await firestore
+      .collection("venues")
+      .doc(targetVenueId)
+      .get();
+
+    if (targetVenueDoc.exists) {
+      room.template = targetVenueDoc.data().template;
+    }
+  }
+
+  if (!room.template) {
+    const externalExperienceVenueId = await createVenueFromPortal({
+      firestore,
+      portal: room,
+      parentVenueId: venueDoc.id,
+      parentWorldId: venueDoc.data().worldId,
+    });
+
+    room.template = "zoomroom";
+    room.url = `/in/${externalExperienceVenueId}`;
+  }
+};
+
 interface CreateVenueFromPortalProps {
   firestore: FirebaseFirestore.Firestore;
   portal: Room;
@@ -41,105 +148,5 @@ const createVenueFromPortal = async ({
 
   await firestore.collection("venues").doc(venueId).set(venueData);
 
-  return `/in/${venueId}`;
-};
-
-export const migrate = async ({ firestore }: MigrateOptions) => {
-  // Replace "themecamps" -> "partymap"
-  const { docs: thememaps } = await firestore
-    .collection("venues")
-    .where("template", "==", "themecamp")
-    .get();
-
-  console.log(
-    "thememaps: ",
-    thememaps.map((doc) => doc.id)
-  );
-
-  await Promise.all(
-    thememaps.map(
-      async (doc) =>
-        await doc.ref.update({
-          template: "partymap",
-        })
-    )
-  );
-
-  // Fetch all the "partymap"s
-  const { docs: venueDocs } = await firestore
-    .collection("venues")
-    .where("template", "==", "partymap")
-    .get();
-
-  // Update the rooms of the partymaps
-  await Promise.all(
-    venueDocs.map(async (venueDoc) => {
-      const rooms: Room[] = venueDoc.data().rooms ?? [];
-      await Promise.all(
-        rooms?.map(async (room) => {
-          if (room.template === "audience") {
-            room.template = "auditorium";
-          }
-
-          if (room.template === "themecamp") {
-            room.template = "partymap";
-          }
-
-          if (room.template) return;
-
-          if (isVenueRegex.test(room.url)) {
-            const [, targetVenueId] = room.url.match(isVenueRegex);
-
-            const targetVenueDoc = await firestore
-              .collection("venues")
-              .doc(targetVenueId)
-              .get();
-
-            if (targetVenueDoc.exists) {
-              room.template = targetVenueDoc.data().template;
-            } else {
-              const externalExperienceUrl = await createVenueFromPortal({
-                firestore,
-                portal: room,
-                parentVenueId: venueDoc.id,
-                parentWorldId: venueDoc.data().worldId,
-              });
-              room.template = "zoomroom";
-              room.url = externalExperienceUrl;
-            }
-          } else {
-            const externalExperienceUrl = await createVenueFromPortal({
-              firestore,
-              portal: room,
-              parentVenueId: venueDoc.id,
-              parentWorldId: venueDoc.data().worldId,
-            });
-
-            room.template = "zoomroom";
-            room.url = externalExperienceUrl;
-          }
-        })
-      );
-
-      const wasRooms = venueDoc.data().rooms?.map((room) => ({
-        title: room.title,
-        url: room.url,
-        template: room.template,
-      }));
-
-      const nowRooms = rooms?.map((room) => ({
-        title: room.title,
-        url: room.url,
-        template: room.template,
-      }));
-
-      if (!isEqual(wasRooms, nowRooms)) {
-        // console.log("-------------------------------");
-        // console.log("venudId: ", venueDoc.id);
-        // console.log("now", wasRooms);
-        // console.log("will be", nowRooms);
-        await firestore.collection("venues").doc(venueDoc.id).update({ rooms });
-      }
-    })
-  );
+  return venueId;
 };
