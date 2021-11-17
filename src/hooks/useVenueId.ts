f92e4b6e5c0e16f009dd430ffc274895729d17f7
Denis Dimitrov <dedimitrov@melontech.com>
2021-11-17 18:53:45 +0200

    additional migration from venueId to spaceSlug

--- src/hooks/useVenueId.ts
+++ src/hooks/useVenueId.ts
@@ -6,16 +6,16 @@ export interface SpaceParams {
 }
 
 /**
- * Retrieve the venueId from the URL path (/:venueId/) or the search query string (?venueId=).
+ * Retrieve the venueId from the URL path (/:spaceSlug/) or the search query string (?venueId=).
  *
  * @see https://reactrouter.com/web/api/Hooks/useparams
  */
 export const useSpaceParams: () => string | undefined = () => {
   const { spaceSlug: urlParamVenueId } = useParams() as SpaceParams;
 
-  // @debt this is a fallback for our legacy ?venueId=foo pattern. Once we eradicate that
-  //   from the codebase in favour of using /:venueId/ paths in the URL, we can remove this.
-  const searchParamVenueId = useSearchParam("venueId");
+  // @debt this is a fallback for our legacy ?spaceSlug=foo pattern. Once we eradicate that
+  //   from the codebase in favour of using /:spaceSlug/ paths in the URL, we can remove this.
+  const searchParamVenueId = useSearchParam("spaceSlug");
 
   return urlParamVenueId ?? searchParamVenueId ?? undefined;
 };
--- src/settings/urlSettings.ts
+++ src/settings/urlSettings.ts
@@ -47,7 +47,7 @@ export const LOGIN_CUSTOM_TOKEN_PARAM_URL = `${LOGIN_BASE_URL}/:spaceSlug/:custo
 // Account URLs
 export const ACCOUNT_CODE_QUESTIONS_URL = `${ACCOUNT_ROOT_URL}/code-of-conduct`;
 export const ACCOUNT_PROFILE_BASE_URL = `${ACCOUNT_ROOT_URL}/profile`;
-export const ACCOUNT_PROFILE_VENUE_PARAM_URL = `${ACCOUNT_PROFILE_BASE_URL}?venueId=:venueId`;
+export const ACCOUNT_PROFILE_VENUE_PARAM_URL = `${ACCOUNT_PROFILE_BASE_URL}?spaceSlug=:spaceSlug`;
 export const ACCOUNT_PROFILE_QUESTIONS_URL = `${ACCOUNT_ROOT_URL}/questions`;
 
 // Admin v3 URLs
