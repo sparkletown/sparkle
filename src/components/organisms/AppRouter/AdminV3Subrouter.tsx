0d8fc41547784d23a1a16248c62ecb76228cce4a
goran-peoski-work <goran.peoski.work@gmail.com>
2021-11-22 17:00:56 +0100

    Update the SpacesDashboard with a title bar (#2575)
    * Update the SpacesDashboard with a title bar
    
    * Change the default adminWorldSpacesUrl() path to ADMIN_V4_SPACES_1_PARAM_URL

--- src/components/organisms/AppRouter/AdminV3Subrouter.tsx
+++ src/components/organisms/AppRouter/AdminV3Subrouter.tsx
@@ -11,21 +11,29 @@ import {
   ADMIN_V3_SPACE_SETTINGS_PARAM_URL,
   ADMIN_V3_WORLD_SPACES_PARAM_URL,
   ADMIN_V3_WORLDS_BASE_URL,
+  ADMIN_V4_SPACES_1_PARAM_URL,
+  ADMIN_V4_SPACES_2_PARAM_URL,
 } from "settings";
 
 import { SpaceCreateWizard } from "pages/SpaceCreateWizard";
 import { SpaceEditor } from "pages/SpaceEditor";
 import { SpaceEditWizard } from "pages/SpaceEditWizard";
+import { SpacesDashboard } from "pages/SpacesDashboard";
 import { WorldEditor } from "pages/WorldEditor";
 import { WorldsDashboard } from "pages/WorldsDashboard";
 
 import { AdminVenueView } from "components/organisms/AdminVenueView";
 
-import { AdminDashboard } from "../AdminDashboard";
-
 export const AdminV3Subrouter: React.FC = () => (
   <Switch>
-    <Route path={ADMIN_V3_WORLD_SPACES_PARAM_URL} component={AdminDashboard} />
+    <Route
+      path={[
+        ADMIN_V4_SPACES_1_PARAM_URL,
+        ADMIN_V4_SPACES_2_PARAM_URL,
+        ADMIN_V3_WORLD_SPACES_PARAM_URL,
+      ]}
+      component={SpacesDashboard}
+    />
     <Route path={ADMIN_V3_NEW_WORLD_URL} component={WorldEditor} />
     <Route path={ADMIN_V3_OLD_WORLD_PARAM_URL} component={WorldEditor} />
     <Route path={ADMIN_V3_WORLDS_BASE_URL} component={WorldsDashboard} />
