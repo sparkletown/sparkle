9271b497f777db2831871aea6d62e4b28c51c12e
Goran Peoski <79229621+goran-peoski-work@users.noreply.github.com>
2021-11-26 13:19:24 +0100

    Use shouldScheduleBeShown() everywhere to have same check

--- src/components/templates/PartyMap/components/index.ts
+++ src/components/templates/PartyMap/components/index.ts
@@ -1,3 +1,4 @@
+// @debt these components should be exported by index.ts inside their respective directories
 export { Map } from "./Map/Map";
 export { PartyTitle } from "./PartyTitle/PartyTitle";
 export { RoomAttendance } from "./RoomAttendance";
