e0d0d1749e6efeb2878143ef310a633792eed90d
Goran Peoski <79229621+goran-peoski-work@users.noreply.github.com>
2021-12-03 21:26:24 +0100

    Remove some unused API functions

--- src/components/atoms/ChatMessage/ChatMessage.tsx
+++ src/components/atoms/ChatMessage/ChatMessage.tsx
@@ -15,7 +15,7 @@ import { RenderMarkdown } from "components/organisms/RenderMarkdown";
 import { useSelectThisReplyThread } from "components/molecules/Chatbox/components/context/ChatboxContext";
 
 import { ChatMessageInfo } from "components/atoms/ChatMessageInfo";
-import { ChatMessageReplies } from "components/atoms/ChatMessageReplies/ChatMessageReplies";
+import { ChatMessageReplies } from "components/atoms/ChatMessageReplies";
 import { TextButton } from "components/atoms/TextButton";
 
 import "./ChatMessage.scss";
