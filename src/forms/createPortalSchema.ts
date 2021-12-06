89f0c3cbd064b14862502b114439fa674bfc7ef2
Colin Howe <88904+colinhowe@users.noreply.github.com>
2021-12-06 14:40:58 +0000

    Remove createPortalSchema

--- src/forms/createPortalSchema.ts
+++
@@ -1,9 +0,0 @@
-import * as Yup from "yup";
-
-import { PortalSchema } from "types/validation";
-
-import { spaceNameSchema } from "forms/spaceNameSchema";
-
-export const createPortalSchema = Yup.object().shape<PortalSchema>({
-  venueName: spaceNameSchema,
-});
