fc9a5ce3067e424b2feb15ae4a2c0fe46cd2e00e
Colin Howe <88904+colinhowe@users.noreply.github.com>
2021-12-06 19:23:52 +0000

    Add button for creating a portal

--- src/components/molecules/PortalsTable/PortalsTable.tsx
+++ src/components/molecules/PortalsTable/PortalsTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 import { ALWAYS_EMPTY_ARRAY } from "settings";
 
@@ -7,6 +7,10 @@ import { AnyVenue, isVenueWithRooms } from "types/venues";
 import { WithId } from "utils/id";
 
 import { PortalStripForm } from "components/organisms/PortalStripForm";
+
+import { ButtonNG } from "components/atoms/ButtonNG";
+
+import { PortalAddEditModal } from "../PortalAddEditModal";
 
 import "./PortalsTable.scss";
 
@@ -17,17 +21,32 @@ export interface PortalsTableProps {
 export const PortalsTable: React.FC<PortalsTableProps> = ({ space }) => {
   const isSupportingPortals = isVenueWithRooms(space);
   const portals = isSupportingPortals ? space?.rooms : ALWAYS_EMPTY_ARRAY;
+  const [showCreateModal, setShowCreateModal] = useState(false);
+
+  const openModal = useCallback(() => {
+    setShowCreateModal(true);
+  }, [setShowCreateModal]);
+
+  const hideModal = useCallback(() => {
+    setShowCreateModal(false);
+  }, [setShowCreateModal]);
 
   return (
-    <div className="PortalsTable">
-      {portals?.map((portal, index) => (
-        <PortalStripForm
-          key={index}
-          portal={portal}
-          index={index}
-          spaceId={space?.id}
-        />
-      ))}
-    </div>
+    <>
+      <ButtonNG onClick={openModal}>Create portal</ButtonNG>
+      <div className="PortalsTable">
+        {portals?.map((portal, index) => (
+          <PortalStripForm
+            key={index}
+            portal={portal}
+            index={index}
+            spaceId={space?.id}
+          />
+        ))}
+        {showCreateModal && (
+          <PortalAddEditModal show={true} onHide={hideModal} />
+        )}
+      </div>
+    </>
   );
 };
