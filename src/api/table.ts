7bad687036a061c49ef92bb572250cfb0597a69f
mike-lvov <63194656+mike-lvov@users.noreply.github.com>
2021-12-08 16:56:25 +0200

    Porting isolated changes (#2687)
    * Porting table-related commit
    
    * Adds proper reference

--- src/api/table.ts
+++ src/api/table.ts
@@ -1,41 +1,21 @@
 import firebase from "firebase/app";
 
-import { MIN_TABLE_CAPACITY } from "settings";
-
 import { Table } from "types/Table";
 
 export interface UpdateVenueTableProps {
   venueId: string;
-  tableOfUser: Table;
-  tables: Table[];
-  title: string;
-  subtitle?: string;
-  capacity: number | string;
+  newTable: Table;
+  defaultTables: Table[];
 }
 
 export const updateVenueTable = async ({
   venueId,
-  tableOfUser,
-  tables,
-  title,
-  subtitle,
-  capacity,
+  newTable,
+  defaultTables,
 }: UpdateVenueTableProps) => {
-  // @debt: capacity type equals number, but incoming value is string. Needs to be fixed in table types
-  const capacityInteger = parseInt(capacity.toString(), 10);
-  const tableColumns = tableOfUser?.columns ?? MIN_TABLE_CAPACITY;
-  const updatedTable = {
-    ...tableOfUser,
-    title,
-    subtitle,
-    capacity: capacityInteger,
-    // Math.ceil is used so that seats container displays with corrent height with uneven capacity
-    rows: Math.ceil(capacityInteger / tableColumns),
-  };
-
   return await firebase.functions().httpsCallable("venue-updateTables")({
     venueId,
-    tables,
-    updatedTable,
+    newTable,
+    defaultTables,
   });
 };
