8e30b3287593d6df825f1d27f8acdd407cd2b415
mike-lvov <mike.lvov.soft@gmail.com>
2022-01-07 18:15:43 +0200

    Makes the function work

--- functions/src/utils/time.js
+++ functions/src/utils/time.js
@@ -1,4 +1,36 @@
-export const formatSecondsAsHHMMSS = (secondsValue) => {
+/**
+ * @deprecated in favor of using date-fns functions
+ */
+const ONE_MINUTE_IN_SECONDS = 60;
+
+/**
+ * @deprecated in favor of using date-fns functions
+ */
+const ONE_HOUR_IN_SECONDS = ONE_MINUTE_IN_SECONDS * 60;
+
+/**
+ * @deprecated in favor of using date-fns functions
+ */
+const ONE_DAY_IN_SECONDS = ONE_HOUR_IN_SECONDS * 24;
+
+const secondsToDuration = (totalSeconds) => {
+  const days = Math.floor(totalSeconds / ONE_DAY_IN_SECONDS);
+  const remainingSecondsWithoutDays = totalSeconds % ONE_DAY_IN_SECONDS;
+
+  const hours = Math.floor(remainingSecondsWithoutDays / ONE_HOUR_IN_SECONDS);
+  const remainingSecondsWithoutHours =
+    remainingSecondsWithoutDays % ONE_HOUR_IN_SECONDS;
+
+  const minutes = Math.floor(
+    remainingSecondsWithoutHours / ONE_MINUTE_IN_SECONDS
+  );
+  const remainingSecondsWithoutMinutes =
+    remainingSecondsWithoutHours % ONE_MINUTE_IN_SECONDS;
+
+  return { days, hours, minutes, seconds: remainingSecondsWithoutMinutes };
+};
+
+const formatSecondsAsHHMMSS = (secondsValue) => {
   const { hours = 0, minutes = 0, seconds = 0 } = secondsToDuration(
     secondsValue
   );
@@ -8,3 +40,5 @@ export const formatSecondsAsHHMMSS = (secondsValue) => {
 
   return `${hourFormatted}:${minuteFormatted}:${secondsFormatted}`;
 };
+
+exports.formatSecondsAsHHMMSS = formatSecondsAsHHMMSS;
