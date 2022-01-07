19bd90fa3fac32b8b70c2b53a8127f3b482aefeb
Colin Howe <88904+colinhowe@users.noreply.github.com>
2022-01-07 12:23:20 +0000

    Update package-lock

--- src/components/templates/AnimateMap/game/map/systems/FirebarrelSystem.ts
+++ src/components/templates/AnimateMap/game/map/systems/FirebarrelSystem.ts
@@ -21,7 +21,7 @@ export class FirebarrelSystem extends System {
   private _unsubscribeFirebarrelExit!: () => void;
 
   private creator: EntityFactory;
-  private waitingEnterFirebarrelId?: number;
+  private waitingEnterFirebarrelId?: NodeJS.Timeout;
   private WAITING_ENTER_FIREBARREL_TIMEOUT = 15000;
   private SHOUTER_ON = false;
 
@@ -40,7 +40,9 @@ export class FirebarrelSystem extends System {
     this._unsubscribeFirebarrelEnter = subscribeActionAfter(
       AnimateMapActionTypes.ENTER_FIREBARREL,
       () => {
-        clearTimeout(this.waitingEnterFirebarrelId);
+        if (this.waitingEnterFirebarrelId) {
+          clearTimeout(this.waitingEnterFirebarrelId);
+        }
         // this.creator.enterFirebarrel(barrelId);
       }
     );
