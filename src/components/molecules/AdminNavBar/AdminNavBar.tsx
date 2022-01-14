e50f97e4c1bffa7eb5da09e5d20f598f5d4d3a6f
Colin Howe <88904+colinhowe@users.noreply.github.com>
2022-01-14 14:59:56 +0000

    Make navbar look like something

--- src/components/molecules/AdminNavBar/AdminNavBar.tsx
+++ src/components/molecules/AdminNavBar/AdminNavBar.tsx
@@ -1,17 +1,18 @@
 import React, { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { XIcon } from "@heroicons/react/outline";
-
-import "./AdminNavBar.scss";
+import classNames from "classnames";
 
 export interface AdminNavBarPropsType {
   sidebarOpen: boolean;
   setSidebarOpen: (newValue: boolean) => void;
+  hideMenu?: boolean;
 }
 
 export const AdminNavBar: React.FC<AdminNavBarPropsType> = ({
   setSidebarOpen,
   sidebarOpen,
+  hideMenu = false,
 }) => {
   return (
     <>
@@ -82,7 +83,58 @@ export const AdminNavBar: React.FC<AdminNavBarPropsType> = ({
         </Dialog>
       </Transition.Root>
       <div className="hidden md:flex md:w-56 md:flex-col md:fixed md:inset-y-0">
-        This it the desktop nav bar
+        {/* Sidebar component, swap this element with another sidebar if you like */}
+        <div className="flex-1 flex flex-col min-h-0 bg-gray-800">
+          <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
+            <div className="flex items-center flex-shrink-0 px-4">
+              <img
+                className="block h-8 w-auto"
+                src="/images/sparkle-300.png"
+                alt="Sparkle"
+              />
+            </div>
+            {hideMenu === false && (
+              <nav className="mt-5 flex-1 px-2 space-y-1">
+                <h3 className="flex-1 flex flex-col overflow-y-auto px-2 py-1 text-xs text-gray-400 uppercase tracking-wider">
+                  TODO: worldName
+                </h3>
+              </nav>
+            )}
+          </div>
+          <div className="flex-shrink-0">
+            <div className="flex-1 flex flex-col overflow-y-auto px-2 py-2">
+              <a
+                href="switch-world"
+                className={classNames(
+                  hideMenu
+                    ? "text-gray-30 group flex items-center px-2 py-2 text-sm font-medium uppercase tracking-wider rounded-md bg-sparkle text-white"
+                    : "text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-regular uppercase tracking-wider rounded-md"
+                )}
+              >
+                TODO: Switch world
+              </a>
+            </div>
+          </div>
+          <div className="flex-shrink-0 flex bg-gray-700 p-4">
+            <div className="flex-shrink-0 w-full group block">
+              <div className="flex items-center">
+                <div>
+                  <img
+                    className="inline-block h-9 w-9 rounded-full"
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                </div>
+                <div className="ml-3">
+                  <p className="text-sm font-medium text-white">Tom Cook</p>
+                  <p className="text-xs font-medium text-gray-300 group-hover:text-gray-200">
+                    View profile
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
