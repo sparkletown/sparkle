d3eb75756417afe2dd4d327c9bde13498c0cb5be
Colin Howe <88904+colinhowe@users.noreply.github.com>
2022-01-14 15:00:08 +0000

    Set fonts and colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Rubik", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        sparkle: "rgb(99, 42, 199)",
+        "sparkle-darker": "rgb(84, 35, 169)" /* darker by 15% */,
+        "warning-red": "rgb(190, 33, 52)",
+        "sparkle-gradient-blue": "rgb(0, 246, 213)",
+        "sparkle-gradient-purple": "rgb(111, 67, 255)",
+        "sparkle-gradient-pink": "rgb(225, 90, 218)",
+      },
+    },
   },
   variants: {
     extend: {},
