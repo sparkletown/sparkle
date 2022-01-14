1e0b998379cf72436ecd6ce3d07643cde0a36e1a
Colin Howe <88904+colinhowe@users.noreply.github.com>
2022-01-14 14:15:07 +0000

    Create the nav bar

--- src/components/layouts/AdminLayout/AdminLayout.tsx
+++ src/components/layouts/AdminLayout/AdminLayout.tsx
@@ -1,4 +1,5 @@
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useEffect, useMemo, useState } from "react";
+import { MenuIcon } from "@heroicons/react/outline";
 
 import { AdminNavBar } from "components/molecules/AdminNavBar";
 
@@ -18,10 +19,34 @@ export const AdminLayout: React.FC<AdminLayoutPropsType> = ({ children }) => {
     };
   }, []);
 
+  const [pageLayout, setPageLayout] = useState({ sidebarOpen: false });
+
+  const setSidebarOpen = useMemo(
+    () => (newValue: boolean) => setPageLayout({ sidebarOpen: newValue }),
+    [setPageLayout]
+  );
+
   return (
     <div>
-      <AdminNavBar />
-      {children}
+      <AdminNavBar
+        sidebarOpen={pageLayout.sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+      />
+      <div className="md:pl-56 flex flex-col flex-1">
+        <div className="sticky top-0 z-10 md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3 bg-gray-100">
+          <button
+            type="button"
+            className="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+            onClick={() => {
+              setPageLayout({ sidebarOpen: !pageLayout.sidebarOpen });
+            }}
+          >
+            <span className="sr-only">Open sidebar</span>
+            <MenuIcon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        {children}
+      </div>
     </div>
   );
 };
