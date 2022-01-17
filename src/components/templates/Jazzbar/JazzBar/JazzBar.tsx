157da0d0b6468c6085a98a7bdbffa21403256295
Colin Howe <88904+colinhowe@users.noreply.github.com>
2022-01-17 18:05:35 +0000

    Strip out undesired parts for now

--- src/components/templates/Jazzbar/JazzBar/JazzBar.tsx
+++ src/components/templates/Jazzbar/JazzBar/JazzBar.tsx
@@ -103,119 +103,93 @@ export const JazzBar: React.FC<JazzProps> = ({ venue }) => {
     (settings.showReactions ?? DEFAULT_SHOW_REACTIONS) &&
     (venue.showReactions ?? DEFAULT_SHOW_REACTIONS);
 
-  const containerClasses = classNames("music-bar", {
-    "music-bar--tableview": seatedAtTable,
-  });
-
-  const videoContainerClasses = classNames("video-container", {
-    "video-container--seated": seatedAtTable,
-  });
-
   if (!venue) return <>Loading...</>;
 
   return (
     <>
+      { /*
       <VenueWithOverlay
         venue={venue}
         containerClassNames={`music-bar ${containerClasses}`}
       >
-        {!seatedAtTable && parentVenue && (
-          <BackButton variant="simple" space={parentVenue} />
-        )}
+      */}
+      {parentVenue && (
+        <BackButton variant="simple" space={parentVenue} />
+      )}
 
-        {!seatedAtTable && (
-          <UserList
-            usersSample={venue.recentUsersSample ?? ALWAYS_EMPTY_ARRAY}
-            userCount={venue.recentUserCount ?? 0}
-            activity={venue.activity ?? "here"}
-          />
-        )}
-
-        {seatedAtTable && (
-          <TableHeader
-            seatedAtTable={seatedAtTable}
-            setSeatedAtTable={setSeatedAtTable}
-            venueName={venue.name}
-            tables={jazzbarTables}
-            venueId={venue.id}
-            defaultTables={JAZZBAR_TABLES}
-          />
-        )}
-        {venue.description?.text && (
-          <div className="row">
-            <div className="col">
-              <div className="description">
-                <RenderMarkdown text={venue.description?.text} />
+      {!venue.hideVideo && (
+        <div className="component-media-object">
+          <div className="iframe-container">
+            {embedIframeUrl ? (
+              <iframe
+                key="main-event"
+                title="main event"
+                className="iframe-video"
+                src={embedIframeUrl}
+                frameBorder="0"
+                allow={IFRAME_ALLOW}
+              />
+            ) : (
+              <div className="iframe-video">
+                Embedded Video URL not yet set up
               </div>
-            </div>
-          </div>
-        )}
-
-        <div className="music-bar-content">
-          <div className={videoContainerClasses}>
-            {!venue.hideVideo && (
-              <>
-                <div className="iframe-container">
-                  {embedIframeUrl ? (
-                    <iframe
-                      key="main-event"
-                      title="main event"
-                      className="iframe-video"
-                      src={embedIframeUrl}
-                      frameBorder="0"
-                      allow={IFRAME_ALLOW}
-                    />
-                  ) : (
-                    <div className="iframe-video">
-                      Embedded Video URL not yet set up
-                    </div>
-                  )}
-                </div>
-
-                {shouldShowReactions && (
-                  <div className="actions-container">
-                    <ReactionsBar
-                      venueId={venue.id}
-                      isReactionsMuted={isUserAudioMuted}
-                      toggleMute={toggleUserAudio}
-                      isAudioDisabled={isReactionsMuted}
-                      isShoutoutsEnabled={isShoutoutsEnabled}
-                    />
-                  </div>
-                )}
-
-                {!seatedAtTable && (
-                  <TablesControlBar
-                    containerClassName="ControlBar__container"
-                    onToggleAvailableTables={toggleTablesVisibility}
-                    showOnlyAvailableTables={showOnlyAvailableTables}
-                  />
-                )}
-              </>
             )}
           </div>
-          {seatedAtTable && (
-            <JazzBarRoom
-              roomName={`${venue.id}-${seatedAtTable}`}
-              venueId={venue.id}
-              setSeatedAtTable={setSeatedAtTable}
-              isReactionsMuted={isUserAudioMuted}
-            />
-          )}
-          <TablesUserList
-            setSeatedAtTable={setSeatedAtTable}
-            seatedAtTable={seatedAtTable}
-            venueId={venue.id}
-            TableComponent={TableComponent}
-            joinMessage={!venue.hideVideo ?? true}
-            customTables={jazzbarTables}
-            showOnlyAvailableTables={showOnlyAvailableTables}
-            venue={venue}
-            defaultTables={JAZZBAR_TABLES}
-            template={VenueTemplate.jazzbar}
-          />
+          <div className="media-controls"></div>
         </div>
+      )}
+
+      {/*!seatedAtTable && (
+        <UserList
+          usersSample={venue.recentUsersSample ?? ALWAYS_EMPTY_ARRAY}
+          userCount={venue.recentUserCount ?? 0}
+          activity={venue.activity ?? "here"}
+        />
+      )*/}
+
+      {/*seatedAtTable && (
+        <TableHeader
+          seatedAtTable={seatedAtTable}
+          setSeatedAtTable={setSeatedAtTable}
+          venueName={venue.name}
+          tables={jazzbarTables}
+          venueId={venue.id}
+          defaultTables={JAZZBAR_TABLES}
+        />
+      )*/}
+      {venue.description?.text && (
+        <div className="row">
+          <div className="col">
+            <div className="description">
+              <RenderMarkdown text={venue.description?.text} />
+            </div>
+          </div>
+        </div>
+      )}
+
+      {seatedAtTable && (
+        <JazzBarRoom
+          roomName={`${venue.id}-${seatedAtTable}`}
+          venueId={venue.id}
+          setSeatedAtTable={setSeatedAtTable}
+          isReactionsMuted={isUserAudioMuted}
+        />
+      )}
+      <TablesUserList
+        setSeatedAtTable={setSeatedAtTable}
+        seatedAtTable={seatedAtTable}
+        venueId={venue.id}
+        TableComponent={TableComponent}
+        joinMessage={!venue.hideVideo ?? true}
+        customTables={jazzbarTables}
+        showOnlyAvailableTables={showOnlyAvailableTables}
+        venue={venue}
+        defaultTables={JAZZBAR_TABLES}
+        template={VenueTemplate.jazzbar}
+      />
+      {/*
       </VenueWithOverlay>
+        */}
     </>
   );
 };
