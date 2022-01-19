40defd6f3bf616b753e6bc446d6e3b2cd4df536f
mike-lvov <63194656+mike-lvov@users.noreply.github.com>
2022-01-19 13:02:12 +0200

    Make scripts run from admin area (#2747)
    * Adds proper script types
    
    * Transfers badges script into functions/; Adds file upload into storage
    
    * Makes the function work
    
    * Adds analytics call
    
    * Adds basic UI
    
    * Adds functionality to Script Component
    
    * Finished working demo
    
    * Adds UI polishes
    
    * Finishing touches
    
    * Applies PR review changes
    
    * Removes risky fs import; Deletes get-badges script
    
    * PR cleanups

---
+++ src/pages/Admin/ToolsPage/index.ts
@@ -0,0 +1,1 @@
+export { ToolsPage } from "./ToolsPage";
--- src/pages/SpacesDashboard/SpacesDashboard.tsx
+++ src/pages/SpacesDashboard/SpacesDashboard.tsx
@@ -5,6 +5,7 @@ import {
   ADMIN_IA_SPACE_CREATE_PARAM_URL,
   ADMIN_IA_WORLD_BASE_URL,
   ADMIN_IA_WORLD_EDIT_PARAM_URL,
+  ADMIN_IA_WORLD_TOOLS_PARAM_URL,
   SPACE_TAXON,
   SPACES_TAXON,
 } from "settings";
@@ -28,6 +29,7 @@ import { LoadingPage } from "components/molecules/LoadingPage";
 import { AdminRestricted } from "components/atoms/AdminRestricted";
 import { ButtonNG } from "components/atoms/ButtonNG";
 import { SortDropDown } from "components/atoms/SortDropDown";
+import { TesterRestricted } from "components/atoms/TesterRestricted";
 
 import "./SpacesDashboard.scss";
 
@@ -111,6 +113,22 @@ export const SpacesDashboard: React.FC = () => {
               </ButtonNG>
             </div>
           </AdminTitleBar>
+
+          <TesterRestricted>
+            <div className="SpacesDashboard__tools">
+              <ButtonNG
+                variant="secondary"
+                isLink
+                linkTo={generateUrl({
+                  route: ADMIN_IA_WORLD_TOOLS_PARAM_URL,
+                  required: ["worldSlug"],
+                  params: { worldSlug },
+                })}
+              >
+                Tools
+              </ButtonNG>
+            </div>
+          </TesterRestricted>
 
           {hasPartyVenues && <AdminTitle>My map spaces</AdminTitle>}
           <main
