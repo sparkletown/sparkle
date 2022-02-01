dd95e39281bd04752a589cd964ea63c42ece94d7
Oleksii Petrov <oleksii.petrov@altexsoft.com>
2022-02-01 10:21:08 +0200

    remove functions/lib

--- functions/lib/types/venue.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VenueIdSchema = void 0;
-const yup = require("yup");
-const venue_1 = require("../utils/venue");
-exports.VenueIdSchema = yup.string().matches(venue_1.VENUE_ID_REGEX).required();
-//# sourceMappingURL=venue.js.map
