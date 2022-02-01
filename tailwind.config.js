5d1777c0ae753c92656609bd0b78869c2c6e75ae
Colin Howe <88904+colinhowe@users.noreply.github.com>
2022-02-01 09:39:26 +0000

    Fix tailwind and postcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: 'media',
   theme: {
     extend: {
       fontFamily: {
