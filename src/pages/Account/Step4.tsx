0bd5f4526077586b865e85720ac3a6304030161d
Oleksii Petrov <oleksii.petrov@altexsoft.com>
2022-02-07 20:08:11 +0200

    step 4 playa image missing fix

---
+++ src/pages/Account/Step4.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { ReactMic } from "react-mic";
+import { useHistory } from "react-router-dom";
+import Webcam from "react-webcam";
+
+import { DEFAULT_MAP_BACKGROUND, PLAYA_VENUE_NAME } from "settings";
+
+import "firebase/storage";
+
+import "./Account.scss";
+
+const Step4 = () => {
+  const history = useHistory();
+  const [showMicrophone, setShowMicrophone] = useState(false);
+  return (
+    <div className="splash-page-container">
+      <img
+        className="playa-img"
+        src={DEFAULT_MAP_BACKGROUND}
+        alt={`${PLAYA_VENUE_NAME} Background Map`}
+      />
+      <div className="step-container step5-container">
+        <div className="login-container">
+          <h2>Test your camera and mic before entering the venue</h2>
+          <div>
+            <Webcam width={400} />
+          </div>
+          <ReactMic
+            record={showMicrophone} // defaults -> false.  Set to true to begin recording
+            visualSetting="frequencyBars" // defaults -> "sinewave".  Other option is "frequencyBars
+            strokeColor="#937c63" // sinewave or frequency bar color
+            backgroundColor="black"
+            className="microphone"
+          />
+          <button
+            className="microphone-button"
+            onClick={() => setShowMicrophone((prev) => !prev)}
+          >
+            Test microphone
+          </button>
+          <button
+            className={`btn btn-primary btn-block btn-centered`}
+            onClick={() => history.push(`/enter/step5`)}
+          >
+            Continue
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Step4;
