f56a9d914d434f960a53756414af9bb3709762a8
Colin Howe <88904+colinhowe@users.noreply.github.com>
2022-02-09 15:17:49 +0000

    Document how huddles should be used

--- src/components/attendee/VideoHuddle/useVideoHuddle.tsx
+++ src/components/attendee/VideoHuddle/useVideoHuddle.tsx
@@ -23,7 +23,6 @@ export interface ButtonConfig {
 export const HuddleContext = React.createContext<HuddleContextType>({
   inHuddle: false,
   setInHuddle: (cb: (prevInHuddle: boolean) => void) => {},
-  // TODO Make it so classes can be set on the video element
   extraButtons: [],
   setExtraButtons: (buttons: ButtonConfig[]) => [],
 });
@@ -53,6 +52,18 @@ export const HuddleProvider: React.FC<HuddleProviderProps> = ({ children }) => {
   );
 };
 
+/**
+ * Allows a developer to interact with the "huddle" component that persists
+ * across space navigations.
+ *
+ * Extra buttons can be added to the huddle by spaces that want to add
+ * extra behaviour on a per-video-track basis. This is done in a restrictive way
+ * to ensure that the look-and-feel of the huddle component is consistent and
+ * not a free for all (as would happen if arbitrary children could be added).
+ *
+ * For the most part, joinHuddle and leaveHuddle will be all that anyone
+ * needs to use.
+ */
 export const useVideoHuddle = () => {
   const {
     localParticipant,
@@ -66,23 +77,19 @@ export const useVideoHuddle = () => {
     HuddleContext
   );
 
-  // TODO Docs
   const joinHuddle = useMemo(() => {
     return (userId: string, huddleId: string) => {
-      console.log("joining huddle");
       joinChannel(userId, huddleId, false, false);
       setInHuddle(() => true);
     };
   }, [joinChannel, setInHuddle]);
 
-  // TODO Docs
   const leaveHuddle = useMemo(() => {
     return () => {
       // Use the callback version of state setting so that the state is not
       // used to determine the memo wrapping this.
       setInHuddle((prevInHuddle) => {
         if (prevInHuddle) {
-          console.log("leaving huddle");
           disconnect();
           setInHuddle(() => false);
         }
