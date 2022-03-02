dc942d5714cc11f9a7f41649621825dcfa94a5c4
Goran Peoski <79229621+goran-peoski-work@users.noreply.github.com>
2022-03-02 13:46:15 +0100

    Fix useLiveCollection ID problem

--- src/hooks/useSeatedTableUsers.ts
+++ src/hooks/useSeatedTableUsers.ts
@@ -1,3 +1,5 @@
+import { useMemo } from "react";
+
 import { ALWAYS_EMPTY_ARRAY, COLLECTION_SPACES } from "settings";
 
 import { TableSeatedUser } from "types/User";
@@ -7,14 +9,17 @@ import { convertToFirestoreKey } from "utils/id";
 import { useLiveCollection } from "hooks/fire/useLiveCollection";
 
 export const useSeatedTableUsers = (spaceId: string | undefined) => {
-  const {
-    data: seatedTableUsers,
-    isLoaded,
-  } = useLiveCollection<TableSeatedUser>([
+  const result = useLiveCollection<TableSeatedUser>([
     COLLECTION_SPACES,
     convertToFirestoreKey(spaceId),
     "seatedTableUsers",
   ]);
 
-  return [seatedTableUsers ?? ALWAYS_EMPTY_ARRAY, isLoaded];
+  return useMemo(
+    () => ({
+      ...result,
+      seatedTableUsers: result.data ?? ALWAYS_EMPTY_ARRAY,
+    }),
+    [result]
+  );
 };
