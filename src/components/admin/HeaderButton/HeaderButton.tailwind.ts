7a4c49a67db70833f13fe19181940babd7b1e700
Colin Howe <88904+colinhowe@users.noreply.github.com>
2022-03-07 13:55:28 +0000

    Add danger variant for header button

--- src/components/admin/HeaderButton/HeaderButton.tailwind.ts
+++ src/components/admin/HeaderButton/HeaderButton.tailwind.ts
@@ -5,3 +5,5 @@ export const multicolor =
   "text-white bg-gradient-to-r from-sparkle-gradient-blue via-sparkle-gradient-purple to-sparkle-gradient-pink";
 export const general =
   "inline-flex items-center px-4 py-2 border rounded-md shadow-sm text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
+
+export const danger = "border-transparent text-white bg-warning-red";
