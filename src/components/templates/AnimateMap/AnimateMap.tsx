0e7188fca0129ae613bc76550f0b14558a0c115c
Denis Isaev <dengonz48@gmail.com>
2022-03-09 18:36:20 +0300

    add animated map wrapper

--- src/components/templates/AnimateMap/AnimateMap.tsx
+++ src/components/templates/AnimateMap/AnimateMap.tsx
@@ -1,76 +1,41 @@
-import React, { useEffect, useRef, useState } from "react";
-import { useStore } from "react-redux";
+import React, { useEffect, useState } from "react";
 import Bugsnag from "@bugsnag/js";
 
-import {
-  enterAnimateMapFireBarrel,
-  exitAnimateMapFireBarrel,
-  updateAnimateMapFireBarrel,
-} from "store/actions/AnimateMap";
-
+import { User } from "types/User";
 import { AnimateMapVenue } from "types/venues";
 
 import { WithId } from "utils/id";
 
-import { useDispatch } from "hooks/useDispatch";
-
 import { AnimateMapErrorPrompt } from "components/templates/AnimateMap/components/AnimateMapErrorPrompt";
 
-import { CloudDataProviderWrapper } from "./bridges/CloudDataProviderWrapper";
-import { CloudDataProvider } from "./bridges/DataProvider/CloudDataProvider";
-import { GameConfig } from "./configs/GameConfig";
 import { GameInstance } from "./game/GameInstance";
-import { useRelatedPartymapRooms } from "./hooks/useRelatedPartymapRooms";
 import { FirebarrelProvider, UIOverlay, UIOverlayGrid } from "./components";
-import { configs } from "./configs";
 
 import "./AnimateMap.scss";
 
 export interface AnimateMapProps {
   space: WithId<AnimateMapVenue>;
+  updateAnimateMapFireBarrelDispatch: (roomId: string, userList: User[]) => void;
+  onConnectFirebarrelChange: (roomId: string, userList: User[], isConnected: boolean) => void;
+  showFirebarrelFlag: boolean;
+  game: GameInstance | null;
+  containerRef: {
+    current:  HTMLDivElement | null;
+  }
 }
 
 export const AnimateMap: React.FC<AnimateMapProps> = (props) => {
-  const [dataProvider, setDataProvider] = useState<CloudDataProvider | null>(
-    null
-  );
-  const [app, setApp] = useState<GameInstance | null>(null);
   const [appError, setAppError] = useState<Error | null>(null);
-  const containerRef = useRef<HTMLDivElement>(null);
-  const store = useStore();
-  const dispatch = useDispatch();
-  const relatedRooms = useRelatedPartymapRooms({ venue: props.space });
-  const [showFirebarrelFlag, setShowFirebarrelFlag] = useState(false);
 
   const runApp = async () => {
-    if (app) {
-      await app.init();
-      await app.start();
+    if (props.game) {
+      await props.game.init();
+      await props.game.start();
     }
   };
 
   useEffect(() => {
-    if (app || !dataProvider || !containerRef || !containerRef.current) {
-      return;
-    }
-
-    const config = props.space.gameOptions
-      ? new GameConfig(props.space.gameOptions)
-      : configs.animateMap;
-
-    const game = new GameInstance(
-      config,
-      store,
-      dataProvider,
-      containerRef.current as HTMLDivElement
-    );
-
-    setApp(game);
-    //eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [dataProvider]);
-
-  useEffect(() => {
-    if (!app) {
+    if (!props.game) {
       return;
     }
 
@@ -78,7 +43,7 @@ export const AnimateMap: React.FC<AnimateMapProps> = (props) => {
       .then(() => console.log("runApp success"))
       .catch((e) => setAppError(e));
     //eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [app]);
+  }, [props.game]);
 
   // NOTE: this is a good to have check for error inside animatemap (and infinite retries due to it)
   if (appError) {
@@ -108,34 +73,18 @@ export const AnimateMap: React.FC<AnimateMapProps> = (props) => {
           <div
             className={
               "UIOverlay__bottom-panel" +
-              (showFirebarrelFlag ? " UIOverlay__bottom-panel--show" : "")
+              (props.showFirebarrelFlag ? " UIOverlay__bottom-panel--show" : "")
             }
           >
             <FirebarrelProvider
               venue={props.space}
-              setUserList={(roomId, userList) => {
-                dispatch(updateAnimateMapFireBarrel(roomId, userList));
-              }}
-              onConnectChange={(roomId, userList, isConnected) => {
-                if (isConnected) {
-                  dispatch(enterAnimateMapFireBarrel(roomId, userList));
-                } else {
-                  dispatch(exitAnimateMapFireBarrel(roomId));
-                }
-
-                setShowFirebarrelFlag(isConnected);
-              }}
+              setUserList={props.updateAnimateMapFireBarrelDispatch}
+              onConnectChange={props.onConnectFirebarrelChange}
             />
           </div>
         </UIOverlay>
       </div>
-      <div ref={containerRef} className="AnimateMap__app-wrapper" />
-      <CloudDataProviderWrapper
-        venue={props.space}
-        newDataProviderCreate={setDataProvider}
-        relatedRooms={relatedRooms}
-        reInitOnError={void 0}
-      />
+      <div ref={props.containerRef} className="AnimateMap__app-wrapper" />
     </div>
   );
 };
--- src/components/templates/AnimateMapWrapper/AnimateMapWrapper.tsx
+++ src/components/templates/AnimateMapWrapper/AnimateMapWrapper.tsx
@@ -1,17 +1,93 @@
-import React from 'react';
+import React, { Fragment, useEffect, useRef, useState } from "react";
+import { useStore } from "react-redux";
 
+import {
+  enterAnimateMapFireBarrel,
+  exitAnimateMapFireBarrel,
+  updateAnimateMapFireBarrel
+} from "store/actions/AnimateMap";
+
+import { User } from "types/User";
 import { AnimateMapVenue } from "types/venues";
 
 import { WithId } from "utils/id";
 
-import { AnimateMap } from '../AnimateMap';
+import { useDispatch } from "hooks/useDispatch";
+
+import { AnimateMap } from "../AnimateMap";
+import { CloudDataProviderWrapper } from "../AnimateMap/bridges/CloudDataProviderWrapper";
+import { CloudDataProvider } from "../AnimateMap/bridges/DataProvider/CloudDataProvider";
+import { configs } from "../AnimateMap/configs";
+import { GameConfig } from "../AnimateMap/configs/GameConfig";
+import { GameInstance } from "../AnimateMap/game/GameInstance";
+import { useRelatedPartymapRooms } from "../AnimateMap/hooks/useRelatedPartymapRooms";
 
 export type AnimateMapWrapperProps = {
   space: WithId<AnimateMapVenue>;
 };
 
 export const AnimateMapWrapper: React.VFC<AnimateMapWrapperProps> = (props) => {
+  const [game, setGame] = useState<GameInstance | null>(null);
+  const relatedRooms = useRelatedPartymapRooms({ venue: props.space });
+  const [showFirebarrelFlag, setShowFirebarrelFlag] = useState(false);
+  const [dataProvider, setDataProvider] = useState<CloudDataProvider | null>(null);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const store = useStore();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (game || !dataProvider || !containerRef || !containerRef.current) {
+      return;
+    }
+
+    const config = props.space.gameOptions
+      ? new GameConfig(props.space.gameOptions)
+      : configs.animateMap;
+
+    const gameInstance = new GameInstance(
+      config,
+      store,
+      dataProvider,
+      containerRef.current as HTMLDivElement
+    );
+
+    setGame(gameInstance);
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dataProvider]);
+
+  const updateAnimateMapFireBarrelDispatch = (roomId: string, userList: User[]) => {
+    dispatch(updateAnimateMapFireBarrel(roomId, userList));
+  };
+
+  const onConnectFirebarrelChange = (roomId: string, userList: User[], isConnected: boolean) => {
+    if (isConnected) {
+      dispatch(enterAnimateMapFireBarrel(roomId, userList));
+    } else {
+      dispatch(exitAnimateMapFireBarrel(roomId));
+    }
+
+    setShowFirebarrelFlag(isConnected);
+  };
+
   return (
-    <AnimateMap {...props}/>
+    <Fragment>
+      <AnimateMap
+        updateAnimateMapFireBarrelDispatch={updateAnimateMapFireBarrelDispatch}
+        onConnectFirebarrelChange={onConnectFirebarrelChange}
+        showFirebarrelFlag={showFirebarrelFlag}
+        game={game}
+        containerRef={containerRef}
+        {...props}
+      />
+      <CloudDataProviderWrapper
+        venue={props.space}
+        newDataProviderCreate={setDataProvider}
+        relatedRooms={relatedRooms}
+        reInitOnError={void 0}
+      />
+    </Fragment>
   );
 };
