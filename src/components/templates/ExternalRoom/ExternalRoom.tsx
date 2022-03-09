623de46ff6f750afdc42cdc3228ffe9ede2bbb18
mike-lvov <mike.lvov.soft@gmail.com>
2022-03-09 10:06:12 +0200

    Adds new external room

--- src/components/templates/ExternalRoom/ExternalRoom.tsx
+++ src/components/templates/ExternalRoom/ExternalRoom.tsx
@@ -1,22 +1,11 @@
-import React, { useCallback, useEffect } from "react";
-import classNames from "classnames";
-
-import { ALWAYS_EMPTY_ARRAY, ENABLE_POPUPS_URL } from "settings";
+import React from "react";
 
 import { AnyVenue } from "types/venues";
 
 import { WithId } from "utils/id";
-import { getExtraLinkProps, openUrl } from "utils/url";
 
-import { RenderMarkdown } from "components/organisms/RenderMarkdown";
-
-import { UserList } from "components/molecules/UserList";
-
-import { ButtonNG } from "components/atoms/ButtonNG";
-import { SparkleLogoIcon } from "components/atoms/SparkleLogoIcon";
-import { VenueWithOverlay } from "components/atoms/VenueWithOverlay/VenueWithOverlay";
-
-import "./ExternalRoom.scss";
+// import { openUrl } from "utils/url";
+import styles from "./ExternalRoom.module.scss";
 
 export interface ExternalRoomProps {
   venue: WithId<AnyVenue>;
@@ -25,80 +14,34 @@ export interface ExternalRoomProps {
 export const ExternalRoom: React.FC<ExternalRoomProps> = ({ venue }) => {
   const redirectUrl = venue.zoomUrl ?? "";
 
-  const venueLogoClasses = classNames("ExternalRoom__venue-logo");
+  // useEffect(() => {
+  // if (!redirectUrl) return;
+  // openUrl(redirectUrl);
+  // }, [redirectUrl]);
 
-  useEffect(() => {
-    if (!redirectUrl) return;
-    openUrl(redirectUrl);
-  }, [redirectUrl]);
+  // const openRoomUrl = useCallback(() => openUrl(redirectUrl), [redirectUrl]);
 
-  const openRoomUrl = useCallback(() => openUrl(redirectUrl), [redirectUrl]);
+  // venue.name
+  // venue.config?.landingPageConfig.subtitle
+  // venue.config?.landingPageConfig.description
 
   return (
-    <VenueWithOverlay venue={venue} containerClassNames="ExternalRoom">
-      <div className="ExternalRoom__container">
-        <SparkleLogoIcon />
-        {!redirectUrl && (
+    <div className={styles.ExternalRoom}>
+      {/* {!redirectUrl && (
           <div className="ExternalRoom__container">
             <div className="ExternalRoom__message">
               Venue {venue.name} should redirect to a URL, but none was set.
             </div>
           </div>
-        )}
-
-        {redirectUrl && (
-          <>
-            <div className="ExternalRoom__message">
-              <div>This page should automatically open</div>
-              <a
-                href={redirectUrl}
-                className="ExternalRoom__link"
-                {...getExtraLinkProps(true)}
-              >
-                {redirectUrl}
-              </a>
-
-              <div>
-                in a new tab. If you&apos;re not seeing this, try{" "}
-                <a rel="noreferrer" href={ENABLE_POPUPS_URL} target="_blank">
-                  enabling pop ups on your browser.
-                </a>
-              </div>
-            </div>
-            <div className="ExternalRoom__content">
-              <div className="ExternalRoom__venue-container">
-                <div className={venueLogoClasses} />
-
-                <div className="ExternalRoom__venue-details">
-                  <div className="ExternalRoom__venue-title">{venue.name}</div>
-
-                  <div className="ExternalRoom__venue-subtitle">
-                    {venue.config?.landingPageConfig.subtitle}
-                  </div>
-
-                  <ButtonNG variant="primary" onClick={openRoomUrl}>
-                    Enter
-                  </ButtonNG>
-                </div>
-              </div>
-
-              <div className="ExternalRoom__venue-description">
-                <RenderMarkdown
-                  text={venue.config?.landingPageConfig.description}
-                />
-              </div>
-
-              <UserList
-                containerClassName="ExternalRoom__userlist"
-                userCount={venue.recentUserCount ?? 0}
-                usersSample={venue.recentUsersSample ?? ALWAYS_EMPTY_ARRAY}
-                activity="in here"
-                hasClickableAvatars
-              />
-            </div>
-          </>
-        )}
-      </div>
-    </VenueWithOverlay>
+        )} */}
+      <img src={venue.host?.icon} alt="Venue icon" />
+      <h1>{venue.name}</h1>
+      <p className={styles.RedirectText}>
+        This page should automatically open <br />
+        <a href={redirectUrl}>{redirectUrl}</a> <br />
+        in a new tab.If you&apos;re not seeing this, try enabling pop ups on
+        your browser.
+      </p>
+    </div>
   );
 };
