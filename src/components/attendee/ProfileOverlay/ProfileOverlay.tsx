0015ebef02b89b17b1afcdf98f3f76c89ea2d7fe
Goran Peoski <79229621+goran-peoski-work@users.noreply.github.com>
2022-03-09 06:19:32 +0100

    Revert "Add new ProfileOverlayHoc"
    This reverts commit 2c0046ebc5f92a69790be56a474a936097cc4c3b.

--- src/components/attendee/ProfileOverlay/ProfileOverlay.tsx
+++ src/components/attendee/ProfileOverlay/ProfileOverlay.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import {
   FieldErrors,
   useFieldArray,
@@ -13,12 +13,13 @@ import { ProfileModalEditLinks } from "components/attendee/ProfileModalEditLinks
 import firebase from "firebase/compat/app";
 import { omit, pick, uniq } from "lodash";
 
-import { UserWithId } from "types/id";
 import {
   UserProfileModalFormData,
   UserProfileModalFormDataPasswords,
 } from "types/profileModal";
+import { User } from "types/User";
 
+import { WithId } from "utils/id";
 import { isShallowEqual } from "utils/object";
 
 import { useCheckOldPassword } from "hooks/useCheckOldPassword";
@@ -37,13 +38,13 @@ const PASSWORD_FIELDS: (keyof UserProfileModalFormDataPasswords)[] = [
 Object.freeze(PASSWORD_FIELDS);
 
 type ProfileOverlayProps = {
-  user: UserWithId;
+  profile: WithId<User>;
 };
-export const ProfileOverlay: React.FC<ProfileOverlayProps> = ({ user }) => {
+export const ProfileOverlay: React.FC<ProfileOverlayProps> = ({ profile }) => {
   const [isSuccess, setSuccess] = useState(false);
   const checkOldPassword = useCheckOldPassword();
   const firebaseUser = firebase.auth()?.currentUser;
-  const defaultValues = useProfileModalFormDefaultValues(user);
+  const defaultValues = useProfileModalFormDefaultValues(profile);
 
   const {
     register,
@@ -159,7 +160,7 @@ export const ProfileOverlay: React.FC<ProfileOverlayProps> = ({ user }) => {
       <div className={styles.ProfileOverlay__search}></div>
       <div className={styles.ProfileOverlay__content}>
         <ProfileModalEditBasicInfo
-          user={user}
+          user={profile}
           register={register}
           watch={watch}
           setValue={setValue}
