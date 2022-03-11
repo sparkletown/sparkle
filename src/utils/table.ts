d8c823ca7f62a8f092735f9313b0aceb021a2d93
Oleksii Petrov <oleksii.petrov@altexsoft.com>
2022-03-11 14:11:01 +0200

    moved func to utils

--- src/utils/table.ts
+++ src/utils/table.ts
@@ -1,22 +1,22 @@
-import { uuid } from "uuidv4";
-
 import { DEFAULT_TABLE_COLUMNS, DEFAULT_TABLE_ROWS } from "settings";
 
 import { Table } from "types/Table";
 
-const generateUniqueTableReference = (title: string) => `${title}-${uuid()}`;
+import { generateTableReferenceId } from "./string";
 
 export const generateTable: (props: {
-  tableNumber?: number;
+  tableNumber: number;
   columns?: number;
   rows?: number;
+  generateTableReference?: (title: string) => string;
 }) => Table = ({
   tableNumber,
   columns = DEFAULT_TABLE_COLUMNS,
   rows = DEFAULT_TABLE_ROWS,
+  generateTableReference = generateTableReferenceId,
 }) => {
-  const title = tableNumber ? `Table ${tableNumber}` : "New table";
-  const reference = generateUniqueTableReference(title);
+  const title = `Table ${tableNumber}`;
+  const reference = generateTableReference(title);
 
   const capacity = columns * rows;
 
@@ -42,9 +42,21 @@ export const generateTables: (props: {
   rows?: number;
   columns?: number;
   startFrom?: number;
-}) => Table[] = ({ num, rows, columns, startFrom = 1 }) =>
+  generateTableReference?: (title: string) => string;
+}) => Table[] = ({
+  num,
+  rows,
+  columns,
+  generateTableReference = generateTableReferenceId,
+  startFrom = 1,
+}) =>
   Array.from(Array(num)).map((_, idx) => {
     const tableNumber = startFrom + idx;
 
-    return generateTable({ tableNumber, columns, rows });
+    return generateTable({
+      tableNumber,
+      columns,
+      rows,
+      generateTableReference,
+    });
   });
