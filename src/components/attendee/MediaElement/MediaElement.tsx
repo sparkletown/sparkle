d745323b6705b56636fa9d1c6b5543262c313fee
Colin Howe <88904+colinhowe@users.noreply.github.com>
2022-03-22 11:54:54 +0000

    Allow for max-width media elements

--- src/components/attendee/MediaElement/MediaElement.tsx
+++ src/components/attendee/MediaElement/MediaElement.tsx
@@ -12,12 +12,19 @@ import { convertToEmbeddableUrl } from "utils/embeddableUrl";
 
 import styles from "./MediaElement.module.scss";
 
-interface MediaPlayerProps {
+interface MediaElementProps {
   url: string;
   autoPlay: boolean;
+  // Used in various templates to create a "full width" non-resizable media
+  // element
+  fullWidth?: boolean;
 }
 
-export const MediaElement: React.FC<MediaPlayerProps> = ({ url, autoPlay }) => {
+export const MediaElement: React.FC<MediaElementProps> = ({
+  url,
+  autoPlay,
+  fullWidth = false,
+}) => {
   const embedIframeUrl = convertToEmbeddableUrl({
     url,
     autoPlay,
@@ -29,7 +36,8 @@ export const MediaElement: React.FC<MediaPlayerProps> = ({ url, autoPlay }) => {
   }, [setExpandedIframe]);
 
   const videoClassnames = classNames(styles.video, {
-    [styles.video__expanded]: expandedIframe,
+    [styles.video__expanded]: expandedIframe && !fullWidth,
+    [styles.video__fullWidth]: fullWidth,
   });
 
   return (
@@ -47,12 +55,14 @@ export const MediaElement: React.FC<MediaPlayerProps> = ({ url, autoPlay }) => {
           <div>Embedded Video URL not yet set up</div>
         )}
       </div>
-      <div className={styles.mediaControls}>
-        <FontAwesomeIcon
-          icon={expandedIframe ? faCompressArrowsAlt : faExpandArrowsAlt}
-          onClick={toggleExpandedIframe}
-        />
-      </div>
+      {!fullWidth && (
+        <div className={styles.mediaControls}>
+          <FontAwesomeIcon
+            icon={expandedIframe ? faCompressArrowsAlt : faExpandArrowsAlt}
+            onClick={toggleExpandedIframe}
+          />
+        </div>
+      )}
     </div>
   );
 };
