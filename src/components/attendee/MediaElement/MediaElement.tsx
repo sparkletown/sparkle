c85794a6871c61ecb51142e230e65222f0a92c86
Colin Howe <88904+colinhowe@users.noreply.github.com>
2022-03-22 21:24:43 +0000

    Art piece template (#2949)
    * Rename MediaPlayer -> MediaElement
    
    * Initial parts for ArtPiece
    
    * Allow for max-width media elements
    
    * WebcamGrid component

--- src/components/attendee/MediaElement/MediaElement.tsx
+++ src/components/attendee/MediaElement/MediaElement.tsx
@@ -10,14 +10,21 @@ import { IFRAME_ALLOW } from "settings";
 
 import { convertToEmbeddableUrl } from "utils/embeddableUrl";
 
-import styles from "./MediaPlayer.module.scss";
+import styles from "./MediaElement.module.scss";
 
-interface MediaPlayerProps {
+interface MediaElementProps {
   url: string;
   autoPlay: boolean;
+  // Used in various templates to create a "full width" non-resizable media
+  // element
+  fullWidth?: boolean;
 }
 
-export const MediaPlayer: React.FC<MediaPlayerProps> = ({ url, autoPlay }) => {
+export const MediaElement: React.FC<MediaElementProps> = ({
+  url,
+  autoPlay,
+  fullWidth = false,
+}) => {
   const embedIframeUrl = convertToEmbeddableUrl({
     url,
     autoPlay,
@@ -29,11 +36,16 @@ export const MediaPlayer: React.FC<MediaPlayerProps> = ({ url, autoPlay }) => {
   }, [setExpandedIframe]);
 
   const videoClassnames = classNames(styles.video, {
-    [styles.video__expanded]: expandedIframe,
+    [styles.video__expanded]: expandedIframe && !fullWidth,
+    [styles.video__fullWidth]: fullWidth,
+  });
+
+  const containerClassnames = classNames(styles.componentMediaObject, {
+    [styles.componentMediaObject__fullWidth]: fullWidth,
   });
 
   return (
-    <div className={styles.componentMediaObject}>
+    <div className={containerClassnames}>
       <div className={videoClassnames}>
         {embedIframeUrl ? (
           <iframe
@@ -47,12 +59,14 @@ export const MediaPlayer: React.FC<MediaPlayerProps> = ({ url, autoPlay }) => {
           <div>Embedded Video URL not yet set up</div>
         )}
       </div>
-      <div className={styles.mediaControls}>
-        <FontAwesomeIcon
-          icon={expandedIframe ? faCompressArrowsAlt : faExpandArrowsAlt}
-          onClick={toggleExpandedIframe}
-        />
-      </div>
+      {!fullWidth && (
+        <div className={styles.mediaControls}>
+          <FontAwesomeIcon
+            icon={expandedIframe ? faCompressArrowsAlt : faExpandArrowsAlt}
+            onClick={toggleExpandedIframe}
+          />
+        </div>
+      )}
     </div>
   );
 };
