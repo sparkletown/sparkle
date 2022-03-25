f0f5b959ebcb508234485ca84ed637285e5ee1bd
mike-lvov <mike.lvov.soft@gmail.com>
2022-03-25 02:29:34 +0200

    Adds fullscreen banner on atendee side

--- src/components/attendee/AttendeeLayout.tsx
+++ src/components/attendee/AttendeeLayout.tsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import { AttendeeFooter } from "components/attendee/AttendeeFooter";
 import { AttendeeHeader } from "components/attendee/AttendeeHeader";
 import { ChatContainer } from "components/attendee/ChatContainer";
+import { withSlugs } from "components/hocs/context/withSlugs";
 import { withAuth } from "components/hocs/db/withAuth";
+import { withWorldAndSpace } from "components/hocs/db/withWorldAndSpace";
 import { withFallback } from "components/hocs/gate/withFallback";
 import { withRequired } from "components/hocs/gate/withRequired";
 import { compose } from "lodash/fp";
@@ -11,12 +13,15 @@ import { POPOVER_CONTAINER_ID } from "settings";
 
 import { SpaceWithId } from "types/id";
 
+import { useShowHide } from "hooks/useShowHide";
+
 import { VenuePage } from "pages/VenuePage";
 
 import { LoadingPage } from "components/molecules/LoadingPage";
 
 import { NotLoggedInFallback } from "components/atoms/NotLoggedInFallback";
 
+import { Banner } from "./Banner/Banner";
 import { VideoCommsProvider } from "./VideoComms/VideoCommsProvider";
 import { HuddleProvider } from "./VideoHuddle/HuddleProvider";
 import { VideoHuddle } from "./VideoHuddle/VideoHuddle";
@@ -26,10 +31,16 @@ import styles from "./AttendeeLayout.module.scss";
 
 interface _AttendeeLayoutProps {
   userId: string;
+  space: SpaceWithId;
 }
 
-const _AttendeeLayout: React.FC<_AttendeeLayoutProps> = ({ userId }) => {
+const _AttendeeLayout: React.FC<_AttendeeLayoutProps> = ({ userId, space }) => {
   const [backButtonSpace, setBackButtonSpace] = useState<SpaceWithId>();
+  const {
+    isShown: isBlurTurnedOn,
+    show: turnOnBlur,
+    hide: turnOffBlur,
+  } = useShowHide();
 
   useEffect(() => {
     document.documentElement.classList.add(styles.html);
@@ -43,7 +54,9 @@ const _AttendeeLayout: React.FC<_AttendeeLayoutProps> = ({ userId }) => {
       <HuddleProvider>
         <AttendeeHeader backButtonSpace={backButtonSpace} />
         <main>
-          <section className={styles.Space}>
+          <section
+            className={`${styles.Space} ${isBlurTurnedOn && styles.blur}`}
+          >
             <VenuePage setBackButtonSpace={setBackButtonSpace} />
           </section>
           <div className={styles.LayerUi}>
@@ -55,6 +68,13 @@ const _AttendeeLayout: React.FC<_AttendeeLayoutProps> = ({ userId }) => {
         <AttendeeFooter />
         {/* Used by popovers to ensure z-index is handled properly */}
         <div id={POPOVER_CONTAINER_ID} className={styles.popoverContainer} />
+        {space.banner && (
+          <Banner
+            banner={space.banner}
+            turnOnBlur={turnOnBlur}
+            turnOffBlur={turnOffBlur}
+          />
+        )}
       </HuddleProvider>
     </VideoCommsProvider>
   );
@@ -63,6 +83,8 @@ const _AttendeeLayout: React.FC<_AttendeeLayoutProps> = ({ userId }) => {
 export const AttendeeLayout = compose(
   withAuth,
   withFallback(["isAuthLoaded", "userId"], NotLoggedInFallback),
+  withSlugs,
+  withWorldAndSpace,
   withRequired({
     required: ["userId"],
     fallback: LoadingPage,
---
+++ src/components/attendee/Banner/Banner.tsx
@@ -0,0 +1,77 @@
+import React, { useCallback, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { Button } from "components/attendee/Button";
+
+import { Banner as TBanner } from "types/banner";
+
+import { useShowHide } from "hooks/useShowHide";
+
+import { RenderMarkdown } from "components/organisms/RenderMarkdown";
+
+import CN from "./Banner.module.scss";
+
+export type BannerProps = {
+  banner: TBanner;
+  turnOnBlur: () => void;
+  turnOffBlur: () => void;
+};
+
+export const Banner: React.FC<BannerProps> = ({
+  banner,
+  turnOnBlur,
+  turnOffBlur,
+}) => {
+  const { isShown: isBannerShown, hide: closeBanner } = useShowHide(true);
+
+  const isBannerFullScreen = banner.isFullScreen;
+  const isWithButton = banner?.buttonDisplayText && banner?.isActionButton;
+  const isBannerCloaseable = !banner?.isForceFunnel;
+
+  useEffect(() => {
+    if (!isBannerFullScreen) return;
+
+    if (isBannerShown) {
+      turnOnBlur();
+    }
+  }, [isBannerFullScreen, turnOnBlur, isBannerShown]);
+
+  const { push: openUrlUsingRouter } = useHistory();
+
+  const hideBanner = useCallback(() => {
+    turnOffBlur();
+    closeBanner();
+  }, [turnOffBlur, closeBanner]);
+
+  const goToButtonDestination = useCallback(() => {
+    if (!banner.buttonUrl) return;
+
+    openUrlUsingRouter(banner.buttonUrl);
+  }, [openUrlUsingRouter, banner.buttonUrl]);
+
+  if (!isBannerFullScreen || !isBannerShown) return null;
+
+  return (
+    <div className={CN.banner}>
+      <div className={CN.bannerContent}>
+        <RenderMarkdown text={banner.content} />
+        <div className={CN.actionButtons}>
+          {isWithButton && banner.buttonUrl && (
+            <Button onClick={goToButtonDestination}>
+              {banner.buttonDisplayText}
+            </Button>
+          )}
+
+          {isBannerCloaseable && (
+            <Button
+              className={CN.closeButton}
+              onClick={hideBanner}
+              variant="alternative"
+            >
+              Close
+            </Button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
