4bc4fb895d495278a1ee7aa7450ac17fb7795008
Colin Howe <88904+colinhowe@users.noreply.github.com>
2022-03-28 09:30:37 +0100

    Remove old comment;

--- src/api/retunableMediaElement.ts
+++ src/api/retunableMediaElement.ts
@@ -3,10 +3,6 @@ import firebase from "firebase/compat/app";
 
 import { COLLECTION_RETUNABLE_MEDIA_ELEMENTS } from "settings";
 
-// This is a temporary API until we have something better. For now we use
-// a collection "huddles" to store all this temporary data until we have a
-// better place.
-
 export const setRetunableMediaSettings = async (
   spaceId: string,
   settings: RetunableMediaElementSettings
