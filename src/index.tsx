7530e0db47c3ff094da3e096e70c1c87fbe1bc39
Colin Howe <88904+colinhowe@users.noreply.github.com>
2022-04-04 12:26:35 +0100

    Remove unused service worker

--- src/index.tsx
+++ src/index.tsx
@@ -28,8 +28,6 @@ import { LoadingPage } from "components/molecules/LoadingPage/LoadingPage";
 
 import "./wdyr";
 
-import * as serviceWorker from "./serviceWorker";
-
 import "normalize.css";
 import "./index.scss";
 
@@ -73,8 +71,3 @@ traceReactScheduler("initial render", window.performance.now(), () => {
     SPARK.version()
   );
 });
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
