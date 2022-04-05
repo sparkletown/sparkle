f76650329d29db4de4b642d955547dbfa8bc66bb
Denis Dimitrov <dedimitrov@melontech.com>
2022-04-05 15:49:03 +0300

    fix checkbox and toggle colours (#2976)

--- src/components/admin/Checkbox/Checkbox.tailwind.ts
+++ src/components/admin/Checkbox/Checkbox.tailwind.ts
@@ -1,2 +1,4 @@
 export const errorMessage = "mt-2 text-sm text-red-600";
 export const subtext = "mt-2 text-sm text-gray-500";
+export const checkbox =
+  "checked:bg-blue-500 indeterminate:bg-gray-300 focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded accent-sparkle";
