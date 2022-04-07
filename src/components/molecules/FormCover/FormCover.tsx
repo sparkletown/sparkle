b9748ef2f84357a7e0a73160424c96cc4aa53f51
goran-peoski-work <goran.peoski.work@gmail.com>
2022-04-07 14:14:07 +0200

    Add snippets to determine which fn in createVenue_v2 throws an error (#3002)

--- src/components/molecules/FormCover/FormCover.tsx
+++ src/components/molecules/FormCover/FormCover.tsx
@@ -2,17 +2,15 @@ import React from "react";
 
 import LoadingIcon from "assets/icons/icon-loading.svg";
 
-import "./FormCover.scss";
-
 export interface FormCoverProps {
   displayed?: boolean;
 }
 
 export const FormCover: React.FC<FormCoverProps> = ({ displayed, children }) =>
   displayed ? (
-    <div className="FormCover">
-      <div className="FormCover__overlay">
-        <img className="FormCover__spinner" src={LoadingIcon} alt="loading" />
+    <div data-bem="FormCover">
+      <div data-bem="FormCover__overlay">
+        <img data-bem="FormCover__spinner" src={LoadingIcon} alt="loading" />
       </div>
       {children}
     </div>
