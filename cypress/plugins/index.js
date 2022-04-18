3867581220d5e8619cc86079428ff75b8ac55379
Goran Peoski <79229621+goran-peoski-work@users.noreply.github.com>
2022-04-18 15:45:43 +0200

    Add new cypress setup

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,3 @@
-/// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -15,7 +14,8 @@
 /**
  * @type {Cypress.PluginConfig}
  */
+// eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+};
