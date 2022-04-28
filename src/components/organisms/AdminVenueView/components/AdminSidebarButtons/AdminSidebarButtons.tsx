9bbb46735bbaced58cb05620a2c87e7311f4197c
Colin Howe <88904+colinhowe@users.noreply.github.com>
2022-04-28 10:21:20 +0100

    Remove various unused components

--- src/components/organisms/AdminVenueView/components/AdminSidebarButtons/AdminSidebarButtons.tsx
+++ src/components/organisms/AdminVenueView/components/AdminSidebarButtons/AdminSidebarButtons.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import "./AdminSidebarButtons.scss";
-
 export const AdminSidebarButtons: React.FC = ({ children }) => (
   <div className="AdminSidebarButtons mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
     {children}
